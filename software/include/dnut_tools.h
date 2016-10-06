0f3ef21bc042a31e52cb3cb1716139a69d745bfe
Frank Haverkamp <haver@linux.vnet.ibm.com>
2016-10-06 11:57:22 +0200

    Adding simple example for the search demo
    This is direct card access mode, we need to figure out if we
    like to use the other mode (which we fake, since it won't access
    when we plan the demo).
    
    Signed-off-by: Frank Haverkamp <haver@linux.vnet.ibm.com>

--- software/include/dnut_tools.h
+++ software/include/dnut_tools.h
@@ -36,37 +36,6 @@
 
 #define DNUT_TOOL_VERS_STRING	"3.0.25"
 
-#ifndef ARRAY_SIZE
-#  define ARRAY_SIZE(a) ((sizeof((a))/sizeof((a)[0])))
-#endif
-
-
-/* ANSI sequences for terminal IO */
-#define  PR_STD			"\x1b[0m"
-#define  PR_STD_BOLD		"\x1b[1m"
-
-#define  PR_STD_BLINK		"\x1b[5m"
-#define  PR_INVERS		"\x1b[7m"
-
-#define  PR_RED			"\x1b[0;31m"
-#define  PR_RED_BOLD		"\x1b[1;31m"
-
-#define  PR_CYAN		"\x1b[0;36m"
-#define  PR_CYAN_BOLD		"\x1b[1;36m"
-
-#define  PR_BLUE		"\x1b[0;34m"
-#define  PR_BLUE_BOLD		"\x1b[1;34m"
-
-#define  PR_GREEN		"\x1b[0;32m"
-#define  PR_GREEN_BOLD		"\x1b[1;32m"
-
-#define  PR_MAGENTA		"\x1b[0;35m"
-#define  PR_MAGENTA_BOLD	"\x1b[1;35m"
-
-#define	 ANSI_ERASE		"\x1b[2J"
-#define	 ANSI_HOME		"\x1b[1;1H"
-#define	 ANSI_INIT		ANSI_ERASE ANSI_HOME
-
 /*****************************************************************************/
 /** Useful macros in case they are not defined somewhere else		     */
 /*****************************************************************************/
@@ -143,7 +112,5 @@ extern int verbose_flag;
 #ifndef CLOCK_MONOTONIC_RAW
 #define   clock_gettime(clk_id, tp) ({ int val = 0; val; })
 #endif
-
-const char *dnut_regname(uint32_t addr);
 
 #endif		/* __DNUT_TOOLS_H__ */
