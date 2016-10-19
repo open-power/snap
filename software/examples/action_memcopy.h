bb740aa102d25ecafbeee26685668e2ff5b1ff39
Frank Haverkamp <haver@linux.vnet.ibm.com>
2016-10-19 11:35:20 +0200

    Adding memcopy example
    Signed-off-by: Frank Haverkamp <haver@linux.vnet.ibm.com>

---
+++ software/examples/action_memcopy.h
@@ -0,0 +1,32 @@
+#ifndef __ACTION_MEMCOPY_H__
+#define __ACTION_MEMCOPY_H__
+
+/*
+ * Copyright 2016, International Business Machines
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <stdint.h>
+#include <libdonut.h>
+
+#define MEMCOPY_ACTION_TYPE	0x0004
+
+struct memcopy_job {
+	struct dnut_addr in;	/* input data */
+	struct dnut_addr out;   /* offset table */
+	uint64_t mmio_din;	/* private settins for this action */
+	uint64_t mmio_dout;	/* private settins for this action */
+};
+
+#endif	/* __ACTION_MEMCOPY_H__ */
