f7239d61bd8aed33a90185a5de18273aead6b10f
jkayser <jkayser@de.ibm.com>
2016-12-20 16:44:38 +0100

    add export*tcl to separate irun+questa
    Signed-off-by: jkayser <jkayser@de.ibm.com>

---
+++ hardware/setup/export_irun.tcl
@@ -0,0 +1,26 @@
+#-----------------------------------------------------------
+#
+# Copyright 2016, International Business Machines
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#-----------------------------------------------------------
+
+set root_dir    $::env(DONUT_HARDWARE_ROOT)
+set ies_libs    $::env(IES_LIBS)
+puts $root_dir
+puts $ies_libs
+
+open_project $root_dir/viv_project/framework.xpr
+export_simulation  -force -directory "$root_dir/sim" -simulator ies -lib_map_path "$ies_libs" -ip_user_files_dir "$root_dir/viv_project/framework.ip_user_files" -ipstatic_source_dir "$root_dir/viv_project/framework.ip_user_files/ipstatic" -use_ip_compiled_libs
+close_project
