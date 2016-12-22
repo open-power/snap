546a3846f99f65c7d5a63540cac631633312417a
Thomas Fuchs <thomas.fuchs@de.ibm.com>
2016-12-22 16:41:07 +0100

    added donut_syn.xdc and donut_link.xdc in psl_fpga.tcl, to fix timing problems with the axi_clock_converter
    Signed-off-by: Thomas Fuchs <thomas.fuchs@de.ibm.com>

--- hardware/setup/patch_tcl.sh
+++ hardware/setup/patch_tcl.sh
@@ -5,6 +5,8 @@ sed -i '/set netlistDir/ a\
 set rootDir    \$::env(DONUT_HARDWARE_ROOT)\
 set dimmDir    \$::env(DIMMTEST)' $2
 
+sed -i 's/top    synth_options "-flatten_hierarchy rebuilt/top    synth_options "-flatten_hierarchy none/' $2
+
 sed -i '/top    synth_options/ a\
                                            \]' $2
 
@@ -29,9 +31,8 @@ set_attribute module \$top    ip            \[list \\' $2
 
 if [ $DDR3_USED == "TRUE" ]; then
 sed -i '/top    synth_options/ a\
-set_attribute module $top    xdc           \[list \\\
-                                            \$dimmDir/example/dimm_test-admpcieku3-v3_0_0/fpga/src/ddr3sdram_locs_b1_8g_x72ecc.xdc \\\
-                                            \$dimmDir/example/dimm_test-admpcieku3-v3_0_0/fpga/src/ddr3sdram_dm_b1_x72ecc.xdc \\\
+set_attribute module $top    synthXDC      \[list \\\
+                                             \$rootDir/setup/donut_synth.xdc \\\
                                            \]' $2
 fi
 
@@ -44,12 +45,14 @@ if [ $ILA_DEBUG == "TRUE" ]; then
 sed -i '/top      top/ a\
                                              \$rootDir/setup/debug.xdc \\' $2
 fi
+if [ $DDR3_USED == "TRUE" ]; then
+sed -i '/top      top/ a\
+                                             \$dimmDir/example/dimm_test-admpcieku3-v3_0_0/fpga/src/ddr3sdram_locs_b1_8g_x72ecc.xdc \\\
+                                             \$dimmDir/example/dimm_test-admpcieku3-v3_0_0/fpga/src/ddr3sdram_dm_b1_x72ecc.xdc \\' $2
+fi
 
 sed -i '/top      top/ a\
 set_attribute impl \$top      linkXDC       \[list \\\
-                                             \$rootDir/setup/donut.xdc \\' $2
-
-sed -i '/top      impl/ a\
-#set_attribute impl \$top      phys_options  "-force_replication_on_nets \[get_nets -hierarchical -top_net_of_hierarchical_group -filter { NAME =~  "\*action_reset\*" } \]"' $2
+                                             \$rootDir/setup/donut_link.xdc \\' $2
 
 sed -i 's/top      phys_directive Explore/top      phys_directive AggressiveExplore/' $2
