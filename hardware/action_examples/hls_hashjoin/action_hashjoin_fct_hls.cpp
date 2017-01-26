6dbda542df325132b7ada410c5b5e885b2723733
Bruno Mesnet <mesnet@hdclv015.boeblingen.de.ibm.com>
2017-01-26 12:39:18 +0100

    V14 with files to link together in run_hls_script.tcl
    Signed-off-by: Bruno Mesnet <mesnet@hdclv015.boeblingen.de.ibm.com>

---
+++ hardware/action_examples/hls_hashjoin/action_hashjoin_fct_hls.cpp
@@ -0,0 +1,125 @@
+/*
+ * Copyright 2017, International Business Machines
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/* #define NO_SYNTH */
+
+#if defined(NO_SYNTH)
+
+#include <stdlib.h> /* malloc, free, atoi */
+#include <stdio.h>  /* printf */
+#include <limits.h> /* ULONG_MAX = 0xFFFFFFFFUL */
+#include "action_hashjoin_hls.h"
+
+#define __unused __attribute__((unused))
+
+#else
+
+#include <string.h>
+#include "ap_int.h"
+#include "action_hashjoin_hls.h"
+
+#define __unused
+
+/*
+ * Hardware implementation is lacking some libc functions. So let us
+ * replace those.
+ */
+#ifndef ULONG_MAX
+#  define ULONG_MAX 0xFFFFFFFFUL /* gcc compiler but not HLS compiler */
+#endif
+#ifndef NULL
+#  define NULL 0                 /* gcc compiler but not HLS compiler */
+#endif
+
+#endif  /* NO_SYNTH */
+//---------------------------------------------------------------------
+// WRITE DATA TO MEMORY
+short write_burst_of_data_to_mem(ap_uint<MEMDW> *dout_gmem, ap_uint<MEMDW> *d_ddrmem,
+         ap_uint<16> memory_type, ap_uint<64> output_address,
+         ap_uint<MEMDW> *buffer, ap_uint<64> size_in_bytes_to_transfer)
+{
+    short rc;
+    if(memory_type == HOST_DRAM) {
+       memcpy((ap_uint<MEMDW> *) (dout_gmem + output_address), buffer, size_in_bytes_to_transfer);
+       rc =  0;
+    } else if(memory_type == CARD_DRAM) {
+       memcpy((ap_uint<MEMDW> *) (d_ddrmem + output_address), buffer, size_in_bytes_to_transfer);
+       rc =  0;
+    } else // unknown output_type
+       rc =  1;
+    return rc;
+}
+
+// READ DATA FROM MEMORY
+short read_burst_of_data_from_mem(ap_uint<MEMDW> *din_gmem, ap_uint<MEMDW> *d_ddrmem,
+         ap_uint<16> memory_type, ap_uint<64> input_address,
+         ap_uint<MEMDW> *buffer, ap_uint<64> size_in_bytes_to_transfer)
+{
+     short rc;
+     if(memory_type == HOST_DRAM) {
+        memcpy(buffer, (ap_uint<MEMDW> *) (din_gmem + input_address), size_in_bytes_to_transfer);
+       rc = 0;
+     } else if(memory_type == CARD_DRAM) {
+        memcpy(buffer, (ap_uint<MEMDW> *) (d_ddrmem + input_address), size_in_bytes_to_transfer);
+       rc = 0;
+    } else // unknown input_type
+       rc = 1;
+    return rc;
+}
+// READ DATA FROM MEMORY
+short read_single_word_of_data_from_mem(ap_uint<MEMDW> *din_gmem, ap_uint<MEMDW> *d_ddrmem,
+         ap_uint<16> memory_type, ap_uint<64> input_address, ap_uint<MEMDW>
+*buffer)
+{
+     short rc;
+     if(memory_type == HOST_DRAM) {
+        buffer[0] = (din_gmem + input_address)[0];
+       rc = 0;
+     } else if(memory_type == CARD_DRAM) {
+        buffer[0] = (d_ddrmem + input_address)[0];
+       rc = 0;
+    } else // unknown input_type
+       rc = 1;
+    return rc;
+}
+// Larger than DWword
+// // convert_64charTable_to_DWTable(buf_gmem, t3->name);
+// // t3->name defined as char[64]
+void convert_64charTable_to_DWTable(ap_uint<MEMDW> *buffer, char *SixtyFourBytesWordToWrite)
+{
+    int i, j;
+
+     for ( i = 0; i < WPERDW; i++ )  {          //if MEMDW = 512 => WPERDW = 1
+     #pragma HLS UNROLL
+        for ( j = 0; j < BPERDW; j++ ) {        //if MEMDW = 512 => BPERDW = 64
+         #pragma HLS UNROLL
+                buffer[i]( (j+1)*8-1, j*8) = SixtyFourBytesWordToWrite[(i*BPERDW)+j];
+        }
+     }
+}
+void convert_DWTable_to_64charTable(ap_uint<MEMDW> *buffer, char *SixtyFourBytesWordRead)
+{
+    int i, j;
+
+     for ( i = 0; i < WPERDW; i++ )  {          //if MEMDW = 512 => WPERDW = 1
+     #pragma HLS UNROLL
+        for ( j = 0; j < BPERDW; j++ ) {        //if MEMDW = 512 => BPERDW = 64
+        #pragma HLS UNROLL
+                SixtyFourBytesWordRead[(i*BPERDW)+j] = buffer[i]( (j+1)*8-1, j*8);
+        }
+     }
+}
+
--- hardware/action_examples/hls_hashjoin/kernel.cpp
+++ hardware/action_examples/hls_hashjoin/kernel.cpp
@@ -6,7 +6,7 @@
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,28 +20,50 @@
 #include "ap_int.h"
 #include "action_hashjoin_hls.h"
 
+#define HASHJOIN_ACTION_TYPE 0x0022
 
-//--------------------------------------------------------------------------------------------
-#include "action_hashjoin_hls.c"
+#define RELEASE_VERSION 0xFEEDA02200000014 //contains Action and Release numbers
+// ----------------------------------------------------------------------------
+// Known Limitations => Issue #39 & #45
+//      => Transfers must be 64 byte aligned and a size of multiples of 64 bytes
+// ----------------------------------------------------------------------------
+// v1.4 : 01/20/2017 :  cleaning code             : split files and link them
+// v1.3 : 01/17/2017 :  simplifying code          : read tables sent by application
+//                      HLS_SYN_MEM=142,HLS_SYN_DSP=26,HLS_SYN_FF=20836,HLS_SYN_LUT=32321
+// v1.2 : 01/10/2017 :  adapting to master branch : change interfaces names and MEMDW=512
+// v1.1 : 12/08/2016 :  adding 2nd C code         : HJ2 for real hash table creation
+// v1.0 : 12/05/2016 :  creation from search V1.8 : HJ1 used for RD/WR database value
+//                                                : + test string+int conversion
 
+
+short action_hashjoin_hls(ap_uint<MEMDW> *din_gmem, ap_uint<MEMDW> *dout_gmem,
+        ap_uint<MEMDW> *d_ddrmem,
+        action_input_reg *Action_Input,
+        ap_uint<64> T1_address, ap_uint<64> T2_address, ap_uint<64> T3_address,
+        ap_uint<64> *T3_produced);
 
 
 // WRITE RESULTS IN MMIO REGS
-void write_results_in_HJ_regs(action_output_reg *Action_Output, action_input_reg *Action_Input, 
-                   ap_uint<32> ReturnCode, ap_uint<64> field1, ap_uint<64> field2, 
+void write_results_in_HJ_regs(action_output_reg *Action_Output, action_input_reg *Action_Input,
+                   ap_uint<32> ReturnCode, ap_uint<64> field1, ap_uint<64> field2,
                    ap_uint<64> field3, ap_uint<64> field4)
 {
-// Always check that ALL Outputs are tied to a value or HLS will generate a Action_Output_i and a Action_Output_o registers and address to read results will be shifted ...and wrong
+// Always check that ALL Outputs are tied to a value or HLS will generate a
+// Action_Output_i and a Action_Output_o registers and address to read results
+// will be shifted ...and wrong
+// => easy checking in generated files : grep 0x184 action_wrapper_ctrl_reg_s_axi.vhd
+// this grep should return nothing if no duplication of registers (which is expected)
 //
   Action_Output->Retc = (ap_uint<32>) ReturnCode;
   Action_Output->Reserved = (ap_uint<64>) 0x0;
- 
+
   Action_Output->Data.t1_processed   = field1;
   Action_Output->Data.t2_processed   = field2;
   Action_Output->Data.t3_produced    = field3;
   Action_Output->Data.checkpoint     = field4;
-  Action_Output->Data.rc             = 0;
-  Action_Output->Data.action_version =  RELEASE_VERSION; 
+  Action_Output->Data.rc(31,0)       = (ap_uint<32>) ReturnCode;
+  Action_Output->Data.rc(63,32)      = 0;
+  Action_Output->Data.action_version =  RELEASE_VERSION;
 
   // Registers unchanged
   Action_Output->Data.t1 = Action_Input->Data.t1;
@@ -50,12 +72,11 @@ void write_results_in_HJ_regs(action_output_reg *Action_Output, action_input_reg
   Action_Output->Data.hash_table = Action_Input->Data.hash_table;
 }
 
-
 //--------------------------------------------------------------------------------------------
 //--- MAIN PROGRAM ---------------------------------------------------------------------------
 //--------------------------------------------------------------------------------------------
-void action_wrapper(ap_uint<memDW> *din_gmem, ap_uint<memDW> *dout_gmem, 
-           ap_uint<memDW> *d_ddrmem, 
+void action_wrapper(ap_uint<MEMDW> *din_gmem, ap_uint<MEMDW> *dout_gmem,
+           ap_uint<MEMDW> *d_ddrmem,
            action_input_reg *Action_Input, action_output_reg *Action_Output)
 {
 
@@ -67,7 +88,7 @@ void action_wrapper(ap_uint<memDW> *din_gmem, ap_uint<memDW> *dout_gmem,
 
 //DDR memory Interface
 #pragma HLS INTERFACE m_axi port=d_ddrmem    bundle=card_mem0 offset=slave
-#pragma HLS INTERFACE s_axilite port=d_ddrmem    bundle=ctrl_reg 
+#pragma HLS INTERFACE s_axilite port=d_ddrmem    bundle=ctrl_reg
 
 // Host Memory AXI Lite Master Interface
 #pragma HLS DATA_PACK variable=Action_Input
@@ -94,32 +115,33 @@ void action_wrapper(ap_uint<memDW> *din_gmem, ap_uint<memDW> *dout_gmem,
 
 
   // byte address received need to be aligned with port width
-  T1_address = (Action_Input->Data.t1.address)>>ADDR_RIGHT_SHIFT;
-  T2_address = (Action_Input->Data.t2.address)>>ADDR_RIGHT_SHIFT;
-  T3_address = (Action_Input->Data.t3.address)>>ADDR_RIGHT_SHIFT;
+  T1_address = (Action_Input->Data.t1.address) >> ADDR_RIGHT_SHIFT;
+  T2_address = (Action_Input->Data.t2.address) >> ADDR_RIGHT_SHIFT;
+  T3_address = (Action_Input->Data.t3.address) >> ADDR_RIGHT_SHIFT;
 
   ReturnCode = RET_CODE_OK;
 
-
   if(Action_Input->Control.action == HASHJOIN_ACTION_TYPE) {
 
-    	/* Iterations are needed to get the profiler working right
-           	... memory leaks? */
+        /* Iterations are needed to get the profiler working right
+                ... memory leaks? */
 
       for (i = 0; i < 1; i++) {
-    		rc = action_hashjoin_hls(din_gmem, dout_gmem, d_ddrmem, Action_Input, 
-			T1_address, T2_address, T3_address, &T3_produced);
-       		if(rc!=0) ReturnCode = RET_CODE_FAILURE;
+                rc = action_hashjoin_hls(din_gmem, dout_gmem, d_ddrmem, Action_Input,
+                        T1_address, T2_address, T3_address, &T3_produced);
+                if(rc!=0) ReturnCode = RET_CODE_FAILURE;
       }
 
   }
 
   else  // unknown action
-  	ReturnCode = RET_CODE_FAILURE;
- 
-  write_results_in_HJ_regs(Action_Output, Action_Input, ReturnCode, 0, 0, T3_produced, 0); 
+        ReturnCode = RET_CODE_FAILURE;
+
+  write_results_in_HJ_regs(Action_Output, Action_Input, ReturnCode, 0, 0, T3_produced, 0);
 
   return;
 }
+
+
 
 
