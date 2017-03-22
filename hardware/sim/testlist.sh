0bc0ed9913f00ec638e740e0d3ef196f5db8fde3
jkayser <jkayser@de.ibm.com>
2017-03-22 19:42:23 +0100

    adding more tests, disable donut trace
    Signed-off-by: jkayser <jkayser@de.ibm.com>

--- hardware/sim/testlist.sh
+++ hardware/sim/testlist.sh
@@ -3,11 +3,11 @@
   set -e                                                # exit on error
   n=0                                                   # count amount of tests executed (exception for subsecond calls)
   loops=1;
-  export DNUT_TRACE=0xFF
+# export DNUT_TRACE=0xFF
   stimfile=$(basename "$0"); logfile="${stimfile%.*}.log"; ts0=$(date +%s); echo "executing $stimfile, logging $logfile maxloop=$loops";
   for((i=1;i<=loops;i++)) do l="loop=$i of $loops"; ts1=$(date +%s);                                                                                    #  sec
 #   t="$DONUT_ROOT/software/tools/dnut_peek -h"                                                 ;echo -e "$t $l";                   $t;echo -e "RC=$?$del" #  1
-    t="$DONUT_ROOT/software/tools/dnut_peek 0x0         "                                       ;echo -e "$t $l";                   $t;echo -e "RC=$?$del" #  1
+#   t="$DONUT_ROOT/software/tools/dnut_peek 0x0         "                                       ;echo -e "$t $l";                   $t;echo -e "RC=$?$del" #  1
 #   t="$DONUT_ROOT/software/tools/dnut_peek 0x10000 -w32"                                       ;echo -e "$t $l";                   $t;echo -e "RC=$?$del" #  1
     t="$DONUT_ROOT/software/tools/dnut_peek 0x0         ";     r=$($t|grep ']'|awk '{print $2}');echo -e "$t result=$r # release maj.int.min.dist.4Bsha"
     t="$DONUT_ROOT/software/tools/dnut_peek 0x8         ";     r=$($t|grep ']'|awk '{print $2}');echo -e "$t result=$r # build date 0000YYYY.MM.DD.hh.mm"
@@ -22,19 +22,19 @@
     if [[ $done == "0" ]];then echo "exploration not done yet"
       action=$(echo $ACTION_ROOT|sed -e "s/action_examples\// /g"|awk '{print $2}');echo "ENV_action=$action"
       if [[ $action == *"memcopy"* ]];then echo "testing memcopy in master mode"
-#       t="$DONUT_ROOT/software/tools/stage2 -a1 -m -s1 -e2 -i1 -t100 -vv"                      ;echo -e "$t $l";date;((n+=1));time $t;echo -e "RC=$?$del" #  2..34
-#       t="$DONUT_ROOT/software/tools/stage2 -a2 -m -A4096 -S0 -B1 -t30"                        ;echo -e "$t $l";date;((n+=1));time $t;echo -e "RC=$?$del" #
-#       t="$DONUT_ROOT/software/tools/stage2 -a2 -m -A4096 -S1 -B0 -t30"                        ;echo -e "$t $l";date;((n+=1));time $t;echo -e "RC=$?$del" #
-#       t="$DONUT_ROOT/software/tools/stage2 -a6 -m -A4096 -S0 -B1 -t30"                        ;echo -e "$t $l";date;((n+=1));time $t;echo -e "RC=$?$del" #
-#       t="$DONUT_ROOT/software/tools/stage2 -a6 -m -A4096 -S1 -B0 -t30"                        ;echo -e "$t $l";date;((n+=1));time $t;echo -e "RC=$?$del" #
-#       t="$DONUT_ROOT/software/tools/stage2_ddr -m -s0x1000 -e0x1100 -b0x100 -i1 -t200"        ;echo -e "$t $l";date;((n+=1));time $t;echo -e "RC=$?$del" #
-#       t="$DONUT_ROOT/software/tools/stage2_set -m -H -b1 -s10 -p10 -t200"                     ;echo -e "$t $l";date;((n+=1));time $t;echo -e "RC=$?$del" #
+        t="$DONUT_ROOT/software/tools/stage2 -a1 -m -s1 -e2 -i1 -t100 -vv"                      ;echo -e "$t $l";date;((n+=1));time $t;echo -e "RC=$?$del" #  2..34
+        t="$DONUT_ROOT/software/tools/stage2 -a2 -m -A4096 -S0 -B1 -t30"                        ;echo -e "$t $l";date;((n+=1));time $t;echo -e "RC=$?$del" #
+        t="$DONUT_ROOT/software/tools/stage2 -a2 -m -A4096 -S1 -B0 -t30"                        ;echo -e "$t $l";date;((n+=1));time $t;echo -e "RC=$?$del" #
+        t="$DONUT_ROOT/software/tools/stage2 -a6 -m -A4096 -S0 -B1 -t30"                        ;echo -e "$t $l";date;((n+=1));time $t;echo -e "RC=$?$del" #
+        t="$DONUT_ROOT/software/tools/stage2 -a6 -m -A4096 -S1 -B0 -t30"                        ;echo -e "$t $l";date;((n+=1));time $t;echo -e "RC=$?$del" #
+        t="$DONUT_ROOT/software/tools/stage2_ddr -m -s0x1000 -e0x1100 -b0x100 -i1 -t200"        ;echo -e "$t $l";date;((n+=1));time $t;echo -e "RC=$?$del" #
+        t="$DONUT_ROOT/software/tools/stage2_set -m -H -b1 -s10 -p10 -t200"                     ;echo -e "$t $l";date;((n+=1));time $t;echo -e "RC=$?$del" #
       fi
       echo "start exploration"
 #     t="$DONUT_ROOT/software/tools/snap_maint -h -V"                                           ;echo -e "$t $l";                   $t;echo -e "RC=$?$del" #
 #     t="$DONUT_ROOT/software/tools/snap_maint"                                                 ;echo -e "$t $l";date;((n+=1));time $t;echo -e "RC=$?$del" #
-#     t="$DONUT_ROOT/software/tools/snap_maint -m1 -c1"                                         ;echo -e "$t $l";date;((n+=1));time $t;echo -e "RC=$?$del" #
-      t="$DONUT_ROOT/software/tools/snap_maint -m1 -c1 -vvv"                                    ;echo -e "$t $l";date;((n+=1));time $t;echo -e "RC=$?$del" #
+      t="$DONUT_ROOT/software/tools/snap_maint -m1 -c1"                                         ;echo -e "$t $l";date;((n+=1));time $t;echo -e "RC=$?$del" #
+#     t="$DONUT_ROOT/software/tools/snap_maint -m1 -c1 -vvv"                                    ;echo -e "$t $l";date;((n+=1));time $t;echo -e "RC=$?$del" #
 #     t="$DONUT_ROOT/software/tools/snap_maint -m2 -c1 -vvv"                                    ;echo -e "$t $l";date;((n+=1));time $t;echo -e "RC=$?$del" #
       t="$DONUT_ROOT/software/tools/dnut_peek 0x10        ";   r=$($t|grep ']'|awk '{print $2}');echo -e "$t result=$r # cmdreg 0x10=exploration done"
       t="$DONUT_ROOT/software/tools/dnut_peek 0x18        ";   r=$($t|grep ']'|awk '{print $2}');echo -e "$t result=$r # statusreg 0x100=exploration done 1action, 0x111=2action"
@@ -46,18 +46,18 @@
         t="$DONUT_ROOT/software/tools/dnut_peek 0x18        "; r=$($t|grep ']'|awk '{print $2}');echo -e "$t result=$r # statusreg 0x100=exploration done 1action, 0x111=2action"
       fi
     fi
-#   t="$DONUT_ROOT/software/tools/dnut_peek 0x20        ";     r=$($t|grep ']'|awk '{print $2}');echo -e "$t result=$r # Lockreg 0x1=locked"
-#   t="$DONUT_ROOT/software/tools/dnut_peek 0x80        ";     r=$($t|grep ']'|awk '{print $2}');echo -e "$t result=$r # freerunning timer"
-#   t="$DONUT_ROOT/software/tools/dnut_peek 0x88        ";     r=$($t|grep ']'|awk '{print $2}');echo -e "$t result=$r # Job timeout reg"
-#   t="$DONUT_ROOT/software/tools/dnut_peek 0x90        ";     r=$($t|grep ']'|awk '{print $2}');echo -e "$t result=$r # action active counter"
-#   t="$DONUT_ROOT/software/tools/dnut_peek 0x98        ";     r=$($t|grep ']'|awk '{print $2}');echo -e "$t result=$r # job execution counter"
-#   t="$DONUT_ROOT/software/tools/dnut_peek 0xA0        ";     r=$($t|grep ']'|awk '{print $2}');echo -e "$t result=$r # job IDreg 8=master"
-#   t="$DONUT_ROOT/software/tools/dnut_peek 0xE000      ";     r=$($t|grep ']'|awk '{print $2}');echo -e "$t result=$r # Jobmgr FIR"
-#   t="$DONUT_ROOT/software/tools/dnut_peek 0xE008      ";     r=$($t|grep ']'|awk '{print $2}');echo -e "$t result=$r # MMIO   FIR"
-#   t="$DONUT_ROOT/software/tools/dnut_peek 0xE010      ";     r=$($t|grep ']'|awk '{print $2}');echo -e "$t result=$r # DMA    FIR"
-#   t="$DONUT_ROOT/software/tools/dnut_peek 0xE800      ";     r=$($t|grep ']'|awk '{print $2}');echo -e "$t result=$r # Jobmgr ErrInj"
-#   t="$DONUT_ROOT/software/tools/dnut_peek 0xE800      ";     r=$($t|grep ']'|awk '{print $2}');echo -e "$t result=$r # MMIO   ErrInj"
-#   t="$DONUT_ROOT/software/tools/dnut_peek 0xE800      ";     r=$($t|grep ']'|awk '{print $2}');echo -e "$t result=$r # DMA    ErrInj"
+    t="$DONUT_ROOT/software/tools/dnut_peek 0x20        ";     r=$($t|grep ']'|awk '{print $2}');echo -e "$t result=$r # Lockreg 0x1=locked"
+    t="$DONUT_ROOT/software/tools/dnut_peek 0x80        ";     r=$($t|grep ']'|awk '{print $2}');echo -e "$t result=$r # freerunning timer"
+    t="$DONUT_ROOT/software/tools/dnut_peek 0x88        ";     r=$($t|grep ']'|awk '{print $2}');echo -e "$t result=$r # Job timeout reg"
+    t="$DONUT_ROOT/software/tools/dnut_peek 0x90        ";     r=$($t|grep ']'|awk '{print $2}');echo -e "$t result=$r # action active counter"
+    t="$DONUT_ROOT/software/tools/dnut_peek 0x98        ";     r=$($t|grep ']'|awk '{print $2}');echo -e "$t result=$r # job execution counter"
+    t="$DONUT_ROOT/software/tools/dnut_peek 0xA0        ";     r=$($t|grep ']'|awk '{print $2}');echo -e "$t result=$r # job IDreg 8=master"
+    t="$DONUT_ROOT/software/tools/dnut_peek 0xE000      ";     r=$($t|grep ']'|awk '{print $2}');echo -e "$t result=$r # Jobmgr FIR"
+    t="$DONUT_ROOT/software/tools/dnut_peek 0xE008      ";     r=$($t|grep ']'|awk '{print $2}');echo -e "$t result=$r # MMIO   FIR"
+    t="$DONUT_ROOT/software/tools/dnut_peek 0xE010      ";     r=$($t|grep ']'|awk '{print $2}');echo -e "$t result=$r # DMA    FIR"
+    t="$DONUT_ROOT/software/tools/dnut_peek 0xE800      ";     r=$($t|grep ']'|awk '{print $2}');echo -e "$t result=$r # Jobmgr ErrInj"
+    t="$DONUT_ROOT/software/tools/dnut_peek 0xE800      ";     r=$($t|grep ']'|awk '{print $2}');echo -e "$t result=$r # MMIO   ErrInj"
+    t="$DONUT_ROOT/software/tools/dnut_peek 0xE800      ";     r=$($t|grep ']'|awk '{print $2}');echo -e "$t result=$r # DMA    ErrInj"
     if (( numact > 0 ));then
       t="$DONUT_ROOT/software/tools/dnut_peek 0x100       ";   r=$($t|grep ']'|awk '{print $2}');echo -e "$t result=$r # action0 type 0.0.0.shrt.4B_long"
          t0s=${r:7:1};t0l=${r:8:8};if [[ $t0l == "00000001" ]];then a0="memcopy";else a0="unknown";fi; echo "action0 type0s=$t0s type0l=$t0l $a0"
@@ -84,8 +84,8 @@
 #     t="$DONUT_ROOT/software/tools/stage2                 -t100      "                         ;echo -e "$t $l";date;((n+=1));time $t;echo -e "RC=$?$del" #  4..105
 #     t="$DONUT_ROOT/software/tools/stage2     -s2 -e4 -i1 -t40       "                         ;echo -e "$t $l";date;((n+=1));time $t;echo -e "RC=$?$del" #  2..33
 #     t="$DONUT_ROOT/software/tools/stage2 -a1             -t200"                               ;echo -e "$t $l";date;((n+=1));time $t;echo -e "RC=$?$del" #t 4..112..33min
-      t="$DONUT_ROOT/software/tools/stage2 -a1 -s1 -e2 -i1 -t100 -I -vv "                       ;echo -e "$t $l";date;((n+=1));time $t;echo -e "RC=$?$del" #  2..34
-#     t="$DONUT_ROOT/software/tools/stage2 -a1 -s1 -e1 -i1 -t100  -vv "                         ;echo -e "$t $l";date;((n+=1));time $t;echo -e "RC=$?$del" #  2..34
+#     t="$DONUT_ROOT/software/tools/stage2 -a1 -s1 -e2 -i1 -t100 -I -vv "                       ;echo -e "$t $l";date;((n+=1));time $t;echo -e "RC=$?$del" #  2..34
+      t="$DONUT_ROOT/software/tools/stage2 -a1 -s1 -e2 -i1 -t100  -vv "                         ;echo -e "$t $l";date;((n+=1));time $t;echo -e "RC=$?$del" #  2..34
 #     t="$DONUT_ROOT/software/tools/stage2 -a1 -s2 -e4 -i1 -t10"                                ;echo -e "$t $l";date;((n+=1));time $t;echo -e "RC=$?$del" #  2..34
 #     t="$DONUT_ROOT/software/tools/stage2 -a1 -s2 -e8 -i1 -t100"                               ;echo -e "$t $l";date;((n+=1));time $t;echo -e "RC=$?$del" #  5..76..12min
 #     t="$DONUT_ROOT/software/tools/stage2 -a2                    -vvv"                         ;echo -e "$t $l";date;((n+=1));time $t;echo -e "RC=$?$del" #e memcmp failed
@@ -97,30 +97,30 @@
 #     t="$DONUT_ROOT/software/tools/stage2 -a5             -t10       "                         ;echo -e "$t $l";date;((n+=1));time $t;echo -e "RC=$?$del" #  1..3
 #     t="$DONUT_ROOT/software/tools/stage2 -a6                    -vvv"                         ;echo -e "$t $l";date;((n+=1));time $t;echo -e "RC=$?$del" #e memcmp error
 #     t="$DONUT_ROOT/software/tools/stage2 -a6 -z1         -t100      "                         ;echo -e "$t $l";date;((n+=1));time $t;echo -e "RC=$?$del" #  6..10
-#     for num4k in 0 1; do
-#     for num64 in 1 2; do
-#     for align in 4096 1024 256 64; do
-#       t="$DONUT_ROOT/software/tools/stage2 -a2 -A${align} -S${num4k} -B${num64} -t200"        ;echo -e "$t $l";date;((n+=1));time $t;echo -e "RC=$?$del" #
-#     done
-#     done
-#     done
-#     for num64 in 1 5 63 64;do         # 1..64
-#     for align in 4096 1024 256 64; do # must be mult of 64
-#     for num4k in 0 1 3 7; do          # 1=6sec, 7=20sec
-#       t="$DONUT_ROOT/software/tools/stage2 -a6 -A${align} -S${num4k} -B${num64} -t200"        ;echo -e "$t $l";date;((n+=1));time $t;echo -e "RC=$?$del" #
-#     done
-#     done
-#     done
-#     #### check DDR3 memory in KU3
-#     t="$DONUT_ROOT/software/tools/stage2_ddr -h"                                              ;echo -e "$t $l";                   $t;echo -e "RC=$?$del" #
-#     for strt in 0x1000 0x2000;do      # start adr
-#     for iter in 1 2 3;do              # number of blocks
-#     for bsize in 64 0x1000; do        # block size
-#       let end=${strt}+${iter}*${bsize}
-#       t="$DONUT_ROOT/software/tools/stage2_ddr -s${strt} -e${end} -b${bsize} -i${iter} -t200" ;echo -e "$t $l";date;((n+=1));time $t;echo -e "RC=$?$del" #
-#     done
-#     done
-#     done
+      for num4k in 0 1; do
+      for num64 in 1 2; do
+      for align in 4096 1024 256 64; do
+        t="$DONUT_ROOT/software/tools/stage2 -a2 -A${align} -S${num4k} -B${num64} -t200"        ;echo -e "$t $l";date;((n+=1));time $t;echo -e "RC=$?$del" #
+      done
+      done
+      done
+      for num64 in 1 5 63 64;do         # 1..64
+      for align in 4096 1024 256 64; do # must be mult of 64
+      for num4k in 0 1 3 7; do          # 1=6sec, 7=20sec
+        t="$DONUT_ROOT/software/tools/stage2 -a6 -A${align} -S${num4k} -B${num64} -t200"        ;echo -e "$t $l";date;((n+=1));time $t;echo -e "RC=$?$del" #
+      done
+      done
+      done
+      #### check DDR3 memory in KU3
+      t="$DONUT_ROOT/software/tools/stage2_ddr -h"                                              ;echo -e "$t $l";                   $t;echo -e "RC=$?$del" #
+      for strt in 0x1000 0x2000;do      # start adr
+      for iter in 1 2 3;do              # number of blocks
+      for bsize in 64 0x1000; do        # block size
+        let end=${strt}+${iter}*${bsize}
+        t="$DONUT_ROOT/software/tools/stage2_ddr -s${strt} -e${end} -b${bsize} -i${iter} -t200" ;echo -e "$t $l";date;((n+=1));time $t;echo -e "RC=$?$del" #
+      done
+      done
+      done
 #     #### use memset in host or in fpga memory
 #     t="$DONUT_ROOT/software/tools/stage2_set -h"                                              ;echo -e "$t $l";                   $t;echo -e "RC=$?$del" #
 #     for beg in 0 1 11 63 64;do         # start adr
