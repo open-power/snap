7cb81157a8c715955bade5c5e60872a239783b86
Reiner Rieke <rieke@de.ibm.com>
2017-04-06 17:18:30 +0200

    nvme changes
    Signed-off-by: Reiner Rieke <rieke@de.ibm.com>

--- hardware/setup/patch_NVMe.sh
+++ hardware/setup/patch_NVMe.sh
@@ -1,4 +1,23 @@
-echo "patch NVMe was called"
+#!/bin/bash
+############################################################################
+############################################################################
+##
+## Copyright 2016,2017 International Business Machines
+##
+## Licensed under the Apache License, Version 2.0 (the "License");
+## you may not use this file except in compliance with the License.
+## You may obtain a copy of the License at
+##
+##     http://www.apache.org/licenses/LICENSE#2.0
+##
+## Unless required by applicable law or agreed to in writing, software
+## distributed under the License is distributed on an "AS IS" BASIS,
+## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+## See the License for the specific language governing permissions AND
+## limitations under the License.
+##
+############################################################################
+############################################################################
 cd ..
 cp ./viv_project_tmp/nvme.srcs/sources_1/bd/nvme_top/ip/nvme_top_axi_pcie3_0_0/ip_2/sim/nvme_top_axi_pcie3_0_0_pcie3_ip.v ./viv_project_tmp/nvme.srcs/sources_1/bd/nvme_top/ip/nvme_top_axi_pcie3_0_0/ip_2/sim/nvme_top_axi_pcie3_0_0_pcie3_ip.patch
 grep -v "sigs(pipe_rx" ./viv_project_tmp/nvme.srcs/sources_1/bd/nvme_top/ip/nvme_top_axi_pcie3_0_0/ip_2/sim/nvme_top_axi_pcie3_0_0_pcie3_ip.patch >./viv_project_tmp/nvme.srcs/sources_1/bd/nvme_top/ip/nvme_top_axi_pcie3_0_0/ip_2/sim/nvme_top_axi_pcie3_0_0_pcie3_ip.v
