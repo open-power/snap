4eca92f196197bf1c064532c2f60dc2b0a8ddeb9
luyong6 <luyong@cn.ibm.com>
2017-04-06 11:10:44 +0200

    HLS intersect ID change

--- software/examples/action_intersect.h
+++ software/examples/action_intersect.h
@@ -19,9 +19,8 @@
 
 #include <stdint.h>
 #include <libdonut.h>
-#define INTERSECT_ACTION_TYPE	0x0008
-#define INTERSECT_RELEASE       0xFEEDA00800000010
 
+#define HLS_INTERSECT_ID 0x10141005
 
 #define END_SIGN 0xFFFFFFFF
 #define NUM_TABLES  2
@@ -29,17 +28,15 @@
 
 struct intersect_job {
 	struct dnut_addr src_tables[NUM_TABLES];	 /* input tables */
-	struct dnut_addr intsect_result;             /* output table */
+	struct dnut_addr result_table;             /* output table */
     uint64_t step;    //step = 1: copy to DDR; step = 2: do intersection
-    uint64_t rc;
-    uint64_t action_version;
 };
 
 typedef char value_t[64];
 typedef unsigned int ptr_t; 
 
-inline void copyvalue(value_t dst, value_t src);
-inline int cmpvalue(const value_t src1, const value_t src2);
+void copyvalue(value_t dst, value_t src);
+int cmpvalue(const value_t src1, const value_t src2);
 uint32_t intersect_method;
 
 long access_bytes;
