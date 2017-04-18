a15774bb2f5968f636f9ed64d3435a5da96fb32f
luyong6 <luyong@cn.ibm.com>
2017-04-18 08:07:43 +0200

    old version to be merged

--- hardware/action_examples/hls_bfs/kernel.cpp
+++ hardware/action_examples/hls_bfs/kernel.cpp
@@ -18,230 +18,204 @@
 #include <string.h>
 #include "ap_int.h"
 #include <hls_stream.h>
-#include "action_bfs.H"
+#include "bfs.h"
 
 typedef ap_uint<VEX_WIDTH> Q_t;
 //--------------------------------------------------------------------------------------------
 
-// WRITE DATA TO MEMORY
-short write_burst_of_data_to_mem(snap_membus_t *dout_gmem, snap_membus_t *d_ddrmem,
-         snapu16_t memory_type, snapu64_t output_address,
-         snap_membus_t *buffer, snapu64_t size_in_bytes_to_transfer)
-{
-    short rc;
-    switch (memory_type) {
-	case HOST_DRAM:
-       		memcpy((snap_membus_t  *) (dout_gmem + output_address), 
-				buffer, size_in_bytes_to_transfer);
-       		rc =  0;
-		break;
-	case CARD_DRAM:
-       		memcpy((snap_membus_t  *) (d_ddrmem + output_address), 
-				buffer, size_in_bytes_to_transfer);
-       		rc =  0;
-		break;
-	default:
-		rc = 1;
-    }
-    return rc;
-}
 
-// READ DATA FROM MEMORY
-short read_burst_of_data_from_mem(snap_membus_t *din_gmem, snap_membus_t *d_ddrmem,
-         snapu16_t memory_type, snapu64_t input_address,
-         snap_membus_t *buffer, snapu64_t size_in_bytes_to_transfer)
+// WRITE RESULTS IN MMIO REGS
+void write_results_in_BFS_regs(action_output_reg *Action_Output, action_input_reg *Action_Input, 
+                   ap_uint<32>ReturnCode, ap_uint<VEX_WIDTH> current_vex, ap_uint<32> current_pos)
 {
-     short rc;
-    switch (memory_type) {
-	case HOST_DRAM:
-        	memcpy(buffer, (snap_membus_t  *) (din_gmem + input_address), 
-				size_in_bytes_to_transfer);
-       		rc =  0;
-		break;
-	case CARD_DRAM:
-        	memcpy(buffer, (snap_membus_t  *) (d_ddrmem + input_address), 
-				size_in_bytes_to_transfer);
-       		rc =  0;
-		break;
-	default:
-		rc = 1;
-    }
-    return rc;
-}
+// Always check that ALL Outputs are tied to a value or HLS will generate a 
+// Action_Output_i and a Action_Output_o registers and address to read results 
+// will be shifted ...and wrong
+// => easy checking in generated files : grep 0x184 hls_action_ctrl_reg_s_axi.vhd
+//
+  Action_Output->Retc = (ap_uint<32>) ReturnCode;
+  Action_Output->Reserved =  0; 
+
+  Action_Output->Data.action_version =  RELEASE_VERSION; 
+
+  // Registers unchanged
+  Action_Output->Data.input_adjtable_address = Action_Input->Data.input_adjtable_address;
+  Action_Output->Data.input_type             = Action_Input->Data.input_type;
+  Action_Output->Data.input_flags            = Action_Input->Data.input_flags;
+  Action_Output->Data.input_vex_num          = Action_Input->Data.input_vex_num;
+  Action_Output->Data.output_address         = Action_Input->Data.output_address;
+  Action_Output->Data.output_type            = Action_Input->Data.output_type;
+  Action_Output->Data.output_flags           = Action_Input->Data.output_flags;
+
+  Action_Output->Data.status_pos             = current_pos;
+  Action_Output->Data.status_vex             = current_vex;
+  
+  Action_Output->Data.unused = 0;
+ }
+
 
 //--------------------------------------------------------------------------------------------
 //--- MAIN PROGRAM ---------------------------------------------------------------------------
 //--------------------------------------------------------------------------------------------
-void action_wrapper(snap_membus_t  *din_gmem, snap_membus_t  *dout_gmem,
-	snap_membus_t  *d_ddrmem,
-        action_reg *Action_Register, action_RO_config_reg *Action_Config)
+void hls_action(ap_uint<MEMDW> *din_gmem, ap_uint<MEMDW> *dout_gmem, 
+	ap_uint<MEMDW> *d_ddrmem,
+        action_input_reg *Action_Input, action_output_reg *Action_Output)
 {
 
 // Host Memory AXI Interface
-#pragma HLS INTERFACE m_axi port=din_gmem bundle=host_mem offset=slave depth=512
-#pragma HLS INTERFACE m_axi port=dout_gmem bundle=host_mem offset=slave depth=512
-#pragma HLS INTERFACE s_axilite port=din_gmem bundle=ctrl_reg 		offset=0x030
-#pragma HLS INTERFACE s_axilite port=dout_gmem bundle=ctrl_reg 		offset=0x040
+#pragma HLS INTERFACE m_axi port=din_gmem bundle=host_mem
+#pragma HLS INTERFACE m_axi port=dout_gmem bundle=host_mem
+#pragma HLS INTERFACE s_axilite port=din_gmem bundle=ctrl_reg
+#pragma HLS INTERFACE s_axilite port=dout_gmem bundle=ctrl_reg
 
 //DDR memory Interface
-#pragma HLS INTERFACE m_axi port=d_ddrmem bundle=card_mem0 offset=slave depth=512
-#pragma HLS INTERFACE s_axilite port=d_ddrmem bundle=ctrl_reg 		offset=0x050
+#pragma HLS INTERFACE m_axi port=d_ddrmem    bundle=card_mem0 offset=slave
+#pragma HLS INTERFACE s_axilite port=d_ddrmem    bundle=ctrl_reg 
 
 // Host Memory AXI Lite Master Interface
-#pragma HLS DATA_PACK variable=Action_Config
-#pragma HLS INTERFACE s_axilite port=Action_Config bundle=ctrl_reg	offset=0x010 
-#pragma HLS DATA_PACK variable=Action_Register
-#pragma HLS INTERFACE s_axilite port=Action_Register bundle=ctrl_reg	offset=0x100 
+#pragma HLS DATA_PACK variable=Action_Input
+#pragma HLS INTERFACE s_axilite port=Action_Input offset=0x080 bundle=ctrl_reg
+#pragma HLS DATA_PACK variable=Action_Output
+#pragma HLS INTERFACE s_axilite port=Action_Output offset=0x104 bundle=ctrl_reg
 #pragma HLS INTERFACE s_axilite port=return bundle=ctrl_reg
 
-// Hardcoded numbers
-  Action_Config->action_type   = (snapu32_t) BFS_ACTION_TYPE;
-  Action_Config->release_level = (snapu32_t) RELEASE_LEVEL;
 
-
- 
   // VARIABLES
-  short rc=0;
-  snapu32_t ReturnCode;
+  short rc;
+  ap_uint<32> ReturnCode;
 
-  snapu64_t INPUT_ADDRESS;
-  snapu64_t OUTPUT_ADDRESS;
-  snapu64_t fetch_address;
-  snapu64_t commit_address;
+  ap_uint<64> INPUT_ADDRESS;
+  ap_uint<64> OUTPUT_ADDRESS;
+  ap_uint<64> fetch_address;
+  ap_uint<64> commit_address;
 
   ap_uint<1>         visited[MAX_VEX_NUM];
   ap_uint<VEX_WIDTH> i,j, root, current, vex_num;
 
   ap_uint<VEX_WIDTH> vnode_cnt;
-  snapu32_t vnode_place;
+  ap_uint<32> vnode_place;
 
-  snapu64_t edgelink_ptr;
+  ap_uint<64> edgelink_ptr;
   ap_uint<VEX_WIDTH> adjvex;
 
-  snap_membus_t buf_node[1];
-  snap_membus_t buf_out[1];
+  ap_uint<MEMDW> buf_node[1];
+  ap_uint<MEMDW> buf_out[1];
   ap_uint<4> idx;
   
-  if(Action_Register->Control.sat == 0x04)
-  {
   //== Parameters fetched in memory ==
   //==================================
 
   // byte address received need to be aligned with port width
-  INPUT_ADDRESS  = Action_Register->Data.input_adjtable_address;
-  OUTPUT_ADDRESS = Action_Register->Data.output_address;
+  INPUT_ADDRESS  = Action_Input->Data.input_adjtable_address;
+  OUTPUT_ADDRESS = Action_Input->Data.output_address;
   commit_address = OUTPUT_ADDRESS;
-  vex_num        = Action_Register->Data.input_vex_num;
+  vex_num        = Action_Input->Data.input_vex_num;
 
   ReturnCode = RET_CODE_OK;
 
+  if(Action_Input->Control.action == BFS_ACTION_TYPE) {
    
   //define a local BRAM to hold VNODE: 
   // MAX_VEX_NUM * VNODE_SIZE
 
-  snap_membus_t vnode_array[MAX_VEX_NUM/(BPERDW/VNODE_SIZE)]; 
+  ap_uint<MEMDW> vnode_array[MAX_VEX_NUM/(BPERDW/VNODE_SIZE)]; 
   //Initialize it with burst read
   //It will improve the performance a lot.
-  rc = read_burst_of_data_from_mem(din_gmem, d_ddrmem, Action_Register->Data.input_type, 
+  rc = read_burst_of_data_from_mem(din_gmem, d_ddrmem, Action_Input->Data.input_type, 
       (INPUT_ADDRESS>>ADDR_RIGHT_SHIFT), vnode_array, vex_num*VNODE_SIZE);
   // A 512*512bits only takes 15 BRAM_18K,  XCKU060 has 2160 in total. Less than 1%
   // But It may harm the timing when this array size increased.
  
 
   L0: for (root = 0; root < vex_num; root ++)
-  {
+      {
 #if defined(NO_SYNTH)
-      printf("***Handling root = %d ***\n", root);
+          printf("***Handling root = %d ***\n", root);
 #endif
-      
+          
 
-      hls::stream <Q_t> Q;
-      #pragma HLS stream depth=2048 variable=Q
-
-
-      for (i = 0; i < vex_num; i ++)
-      {
-          #pragma HLS UNROLL factor=128
-          visited[i] = 0;
-      }
-
-      buf_out[0] = 0;
-      vnode_cnt = 0;
-      vnode_place = 0;
-
-      buf_out[0](31,0) =  root;
-      vnode_cnt ++;
-      vnode_place += 32;
+          hls::stream <Q_t> Q;
+          #pragma HLS stream depth=2048 variable=Q
 
 
-      visited[root]=1;
-      Q.write(root);
-      while (!Q.empty())
-      {
-          current = Q.read();
-          idx = current (1,0);
-          edgelink_ptr = (snapu64_t)(vnode_array[current/4](128*idx+63, 128*idx+0));
-
-          while (edgelink_ptr != 0) //judge with NULL
+          for (i = 0; i < vex_num; i ++)
           {
-              //Update fetch address
-              fetch_address = edgelink_ptr;
-              idx = edgelink_ptr(5,4);
+              #pragma HLS UNROLL factor=128
+              visited[i] = 0;
+          }
 
-              //Read next edge
-              rc |= read_burst_of_data_from_mem(din_gmem, d_ddrmem, Action_Register->Data.input_type, 
-              (fetch_address>>ADDR_RIGHT_SHIFT), buf_node, BPERDW);
+          buf_out[0] = 0;
+          vnode_cnt = 0;
+          vnode_place = 0;
 
-              edgelink_ptr = (snapu64_t)(buf_node[0](128*idx + 63, 128*idx));
-              adjvex       = (snapu32_t)(buf_node[0](128*idx + 95, 128*idx + 64));
+          buf_out[0](31,0) =  root;
+          vnode_cnt ++;
+          vnode_place += 32;
 
-              if(!visited[adjvex])
+
+          visited[root]=1;
+          Q.write(root);
+          while (!Q.empty())
+          {
+              current = Q.read();
+              idx = current (1,0);
+              edgelink_ptr = (ap_uint<64>)(vnode_array[current/4](128*idx+63, 128*idx+0));
+
+              while (edgelink_ptr != 0) //judge with NULL
               {
-                  visited[adjvex] = 1;
-                  Q.write(adjvex);
+                  //Update fetch address
+                  fetch_address = edgelink_ptr;
+                  idx = edgelink_ptr(5,4);
 
-                  buf_out[0](vnode_place+31, vnode_place) = adjvex;
-                  vnode_cnt ++;
-                  vnode_place += 32;
+                  //Read next edge
+                  rc |= read_burst_of_data_from_mem(din_gmem, d_ddrmem, Action_Input->Data.input_type, 
+                  (fetch_address>>ADDR_RIGHT_SHIFT), buf_node, BPERDW);
 
-                  //Commit buf_out if MEMDW is fulfilled
-                  if(vnode_place >= MEMDW)
+                  edgelink_ptr = (ap_uint<64>)(buf_node[0](128*idx + 63, 128*idx));
+                  adjvex       = (ap_uint<32>)(buf_node[0](128*idx + 95, 128*idx + 64));
+
+                  if(!visited[adjvex])
                   {
-                      rc |= write_burst_of_data_to_mem(din_gmem, d_ddrmem, Action_Register->Data.output_type, 
-                         (commit_address>>ADDR_RIGHT_SHIFT), buf_out, BPERDW);
+                      visited[adjvex] = 1;
+                      Q.write(adjvex);
 
-                      buf_out[0] = 0;
-                      vnode_place = 0;
-                      commit_address += BPERDW;
+                      buf_out[0](vnode_place+31, vnode_place) = adjvex;
+                      vnode_cnt ++;
+                      vnode_place += 32;
+
+                      //Commit buf_out if MEMDW is fulfilled
+                      if(vnode_place >= MEMDW)
+                      {
+                          rc |= write_burst_of_data_to_mem(din_gmem, d_ddrmem, Action_Input->Data.output_type, 
+                             (commit_address>>ADDR_RIGHT_SHIFT), buf_out, BPERDW);
+
+                          buf_out[0] = 0;
+                          vnode_place = 0;
+                          commit_address += BPERDW;
+                      }
                   }
               }
           }
+
+          //Last node
+          buf_out[0](vnode_place+31, vnode_place) = 0xFF000000 + vnode_cnt;
+          rc |= write_burst_of_data_to_mem(din_gmem, d_ddrmem, Action_Input->Data.output_type, 
+                (commit_address>>ADDR_RIGHT_SHIFT), buf_out, BPERDW); 
+          
+          buf_out[0] = 0;
+          vnode_place = 0;
+          commit_address += BPERDW;
+          write_results_in_BFS_regs(Action_Output, Action_Input, ReturnCode, root, commit_address(31,0) ); 
       }
-
-      //Last node
-      buf_out[0](vnode_place+31, vnode_place) = 0xFF000000 + vnode_cnt;
-      rc |= write_burst_of_data_to_mem(din_gmem, d_ddrmem, Action_Register->Data.output_type, 
-            (commit_address>>ADDR_RIGHT_SHIFT), buf_out, BPERDW); 
-      
-      buf_out[0] = 0;
-      vnode_place = 0;
-      commit_address += BPERDW;
-      
-      //Update register
-      Action_Register->Data.status_pos             = commit_address(31,0);
-      Action_Register->Data.status_vex             = root;
-
-
   }
+  else  // unknown action
+    ReturnCode = RET_CODE_FAILURE;
  
   if(rc!=0)
       ReturnCode = RET_CODE_FAILURE;
   
-  //Update register
-  Action_Register->Data.status_pos             = commit_address(31,0);
-  Action_Register->Data.status_vex             = root;
-  Action_Register->Control.Retc = (snapu32_t) ReturnCode;
-  } 
+  
+  write_results_in_BFS_regs(Action_Output, Action_Input, ReturnCode, root, commit_address(31,0)); 
   return;
 }
 
