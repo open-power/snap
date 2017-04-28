7b153066ceb5ed01de73cf7069e00dc72b44db5b
Joerg-Stephan Vogt <jsvogt@de.ibm.com>
2017-04-28 16:32:30 +0200

    First generate report, then extract info. Implement Frank's review proposal
    Signed-off-by: Joerg-Stephan Vogt <jsvogt@de.ibm.com>

--- hardware/setup/noHDK_build.tcl
+++ hardware/setup/noHDK_build.tcl
@@ -35,8 +35,6 @@ puts [format "%-*s %-*s %-*s %-*s"  $widthCol1 "" $widthCol2 "start synthese" $w
 synth_design       $msg_level -mode default -flatten_hierarchy none -fanout_limit 400 -fsm_extraction one_hot -keep_equivalent_registers -resource_sharing off -no_lc -shreg_min_size 5 -no_iobuf -top psl_fpga
 write_checkpoint   $msg_level -force ./Checkpoint/framework_synth.dcp
 report_utilization $msg_level -file  ./Reports/framework_utilization_synth.rpt
-set TIMING_TNS [exec grep -A6 "Design Timing Summary" ./Reports/framework_timing_summary.rpt | tail -n 1 | sed {s/^ *\([0-9]*\).\([0-9]*\).*/\1\2/g}]
-puts [format "%-*s %-*s %-*s %-*s"  $widthCol1 "" $widthCol2 "Timing (TNS)" $widthCol3 "$TIMING_TNS ps" $widthCol4 "" ]
  
 puts [format "%-*s %-*s %-*s %-*s"  $widthCol1 "" $widthCol2 "start locking PSL" $widthCol3 "" $widthCol4 "[clock format [clock seconds] -format %H:%M:%S]"]
 lock_design $msg_level -level routing b
@@ -66,10 +64,12 @@ report_utilization    $msg_level -file  ./Reports/framework_utilization_route_de
 report_route_status   $msg_level -file  ./Reports/framework_route_status.rpt
 report_timing_summary $msg_level -max_paths 100 -file ./Reports/framework_timing_summary.rpt
 report_drc            $msg_level -ruledeck bitstream_checks -name psl_fpga -file ./Reports/framework_drc_bitstream_checks.rpt
+set TIMING_TNS [exec grep -A6 "Design Timing Summary" ./Reports/framework_timing_summary.rpt | tail -n 1 | sed {s/^ *\([0-9]*\).\([0-9]*\).*/\1\2/g}]
+puts [format "%-*s %-*s %-*s %-*s"  $widthCol1 "" $widthCol2 "Timing (TNS)" $widthCol3 "$TIMING_TNS ps" $widthCol4 "" ]
  
 puts [format "%-*s %-*s %-*s %-*s"  $widthCol1 "" $widthCol2 "generating bitstreams" $widthCol3 "" $widthCol4 "[clock format [clock seconds] -format %H:%M:%S]"]
-set STREAM_NAME [exec cat ../.bitstream_name.txt]
- 
+set IMAGE_NAME [exec cat ../.bitstream_name.txt]
+append IMAGE_NAME [expr {$nvme_used == "TRUE" ? "_NVME" : ""}]
 if { $bram_used == "TRUE" } {
     set RAM_TYPE BRAM
 } elseif { $sdram_used == "TRUE" } {
@@ -77,12 +77,7 @@ if { $bram_used == "TRUE" } {
 } else {
     set RAM_TYPE noSDRAM
 }
- 
-if { $nvme_used == "TRUE" } {
-  set IMAGE_NAME [format {%s_NVME_%s_%s_%s} $STREAM_NAME $RAM_TYPE $fpgacard $TIMING_TNS]
-} else {
-  set IMAGE_NAME [format {%s_%s_%s_%s} $STREAM_NAME $RAM_TYPE $fpgacard $TIMING_TNS]
-}
+append IMAGE_NAME [format {_%s_%s_%s} $RAM_TYPE $fpgacard $TIMING_TNS]
  
 write_bitstream $msg_level -force -file ./Images/$IMAGE_NAME
 write_cfgmem    $msg_level -format bin -loadbit "up 0x0 ./Images/$IMAGE_NAME.bit" -file ./Images/$IMAGE_NAME  -size 128 -interface  BPIx16 -force
