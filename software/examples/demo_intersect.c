d14a30ca4759eefc70427069e5d83cd89c7ebde3
Eberhard S. Amann <esa@de.ibm.com>
2017-05-10 13:38:10 +0200

    Add mising Interrupt enablement
    Signed-off-by: Eberhard S. Amann <esa@de.ibm.com>

--- software/examples/demo_intersect.c
+++ software/examples/demo_intersect.c
@@ -445,7 +445,7 @@ int main(int argc, char *argv[])
 		goto out_error;
 	}
 
-	action = snap_attach_action(card, HLS_INTERSECT_ID, 0, 60);
+	action = snap_attach_action(card, HLS_INTERSECT_ID, action_irq, 60);
 	if (action == NULL) {
 		fprintf(stderr, "err: failed to attach action %u: %s\n",
 			card_no, strerror(errno));
