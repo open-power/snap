adaae1055c2bc2356b680f2e2f3b3cb139a1b962
Frank Haverkamp <haver@linux.vnet.ibm.com>
2017-05-19 11:49:39 +0200

    HLS HashJoin: Adding more changes to remove duplication
    Signed-off-by: Frank Haverkamp <haver@linux.vnet.ibm.com>

--- software/examples/action_hashjoin.h
+++ software/examples/action_hashjoin.h
@@ -17,17 +17,20 @@
  * limitations under the License.
  */
 
-#include <stdint.h>
-#include <stdio.h>
-#include <libsnap.h>
+#include <snap_types.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 #define HASHJOIN_ACTION_TYPE 0x10141002
+#define RELEASE_LEVEL        0x00000020
 
 #define TABLE1_SIZE 32
 #define TABLE2_SIZE 32
 #define TABLE3_SIZE (TABLE1_SIZE * TABLE2_SIZE)
-#define HT_SIZE (TABLE1_SIZE * 16) /* size of hashtable */
-#define HT_MULTI TABLE1_SIZE /* multihash entries depends on table1 */
+#define HT_SIZE     (TABLE1_SIZE * 16) /* size of hashtable */
+#define HT_MULTI    (TABLE1_SIZE) /* multihash entries depends on table1 */
 
 typedef char hashkey_t[64];
 typedef char hashdata_t[256];
@@ -63,7 +66,7 @@ typedef struct hashtable_s {
 	entry_t table[HT_SIZE];	/* fixed size */
 } hashtable_t;
 
-struct hashjoin_job {
+typedef struct hashjoin_job {
 	struct snap_addr t1; /* IN: input table1 for multihash */
 	struct snap_addr t2; /* IN: 2nd table2 to do join with */
 	struct snap_addr t3; /* OUT: resulting table3 */
@@ -72,87 +75,14 @@ struct hashjoin_job {
 	uint64_t t1_processed; /* #entries cached, repeat if not all */
 	uint64_t t2_processed; /* #entries processed, repeat if not all */
 	uint64_t t3_produced;  /* #entries produced store them away */
-	uint32_t rc;
-	uint32_t action_version;
-};
+	uint64_t checkpoint;
 
-static inline void print_hex(void *buf, size_t len)
-{
-	unsigned int x;
-	char *d = buf;
+	uint8_t reserved[96/8]; /* mandatory to keep output register
+				   at the right address */
+} hashjoin_job_t; /* DATA = 108 Bytes / 864 bits (fixed length) */
 
-	fprintf(stderr, "{ ");
-	for (x = 0; x < len; x++)
-		fprintf(stderr, "%02x, ", d[x]);
-	fprintf(stderr, "}");
+#ifdef __cplusplus
 }
-
-static inline void ht_dump(hashtable_t *ht)
-{
-	unsigned int i, j;
-
-	fprintf(stderr, "hashtable = {\n");
-	for (i = 0; i < HT_SIZE; i++) {
-		entry_t *entry = &ht->table[i];
-
-		if (!entry->used)
-			continue;
-
-		fprintf(stderr, "  { .ht[%d].key = \"%s\", .used = %d, .multi = {\n",
-		       i, entry->key, entry->used);
-		for (j = 0; j < entry->used; j++) {
-			table1_t *multi = &entry->multi[j];
-
-			fprintf(stderr, "      { .val = ");
-			print_hex(multi, sizeof(*multi));
-			fprintf(stderr, " },\n");
-		}
-		fprintf(stderr, "    },\n"
-		       "  },\n");
-	}
-	fprintf(stderr, "};\n");
-}
-
-static inline void table1_dump(table1_t *table1, unsigned int table1_idx)
-{
-	unsigned int i;
-	table1_t *t1;
-
-	fprintf(stderr, "table1_t table1[] = {\n");
-	for (i = 0; i < table1_idx; i++) {
-		t1 = &table1[i];
-		fprintf(stderr, "  { .name = \"%s\", .age=%d } /* %d. */\n",
-		       t1->name, t1->age, i);
-	}
-	fprintf(stderr, "}; /* table1_idx=%d\n", table1_idx);
-}
-
-static inline void table2_dump(table2_t *table2, unsigned int table2_idx)
-{
-	unsigned int i;
-	table2_t *t2;
-
-	fprintf(stderr, "table2_t table2[] = {\n");
-	for (i = 0; i < table2_idx; i++) {
-		t2 = &table2[i];
-		fprintf(stderr, "  { .name = \"%s\", .animal = \"%s\" } "
-		       "/* %d. */\n", t2->name, t2->animal, i);
-	}
-	fprintf(stderr, "}; /* table2_idx=%d\n", table2_idx);
-}
-
-static inline void table3_dump(table3_t *table3, unsigned int table3_idx)
-{
-	unsigned int i;
-	table3_t *t3;
-
-	fprintf(stderr, "table3_t table3[] = {\n");
-	for (i = 0; i < table3_idx; i++) {
-		t3 = &table3[i];
-		fprintf(stderr, "  { .name = \"%s\", .animal = \"%s\", .age=%d } "
-		       "/* %d. */\n", t3->name, t3->animal, t3->age, i);
-	}
-	fprintf(stderr, "}; /* table3_idx=%d\n", table3_idx);
-}
+#endif
 
 #endif	/* __ACTION_HASHJOIN_H__ */
