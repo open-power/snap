581c467e0e00b0d4088000b96fb10294d7a890da
Kenneth Hill <khill@us.ibm.com>
2017-07-03 12:24:31 -0500

    Correct minor typo in snap_build.tcl
    Signed-off-by: Kenneth Hill <khill@us.ibm.com>

--- hardware/setup/snap_build.tcl
+++ hardware/setup/snap_build.tcl
@@ -125,10 +125,10 @@ set step      phys_opt_design
 set logfile   $log_dir/${step}.log
 set directive [get_property STEPS.PHYS_OPT_DESIGN.ARGS.DIRECTIVE [get_runs impl_1]]
 set command   "phys_opt_design  -directive $directive"
-puts [format "%-*s %-*s %-*s %-*s"  $widthCol1 "" $widthCol2 "start place_design" $widthCol3 "with directive: $directive" $widthCol4 "[clock format [clock seconds] -format %H:%M:%S]"]
+puts [format "%-*s %-*s %-*s %-*s"  $widthCol1 "" $widthCol2 "start phys_opt_design" $widthCol3 "with directive: $directive" $widthCol4 "[clock format [clock seconds] -format %H:%M:%S]"]
 
 if { [catch "$command > $logfile" errMsg] } {
-  puts [format "%-*s %-*s %-*s %-*s"  $widthCol1 "" $widthCol2 "" $widthCol3 "ERROR: place_design failed" $widthCol4 "" ]
+  puts [format "%-*s %-*s %-*s %-*s"  $widthCol1 "" $widthCol2 "" $widthCol3 "ERROR: phys_opt_design failed" $widthCol4 "" ]
   puts [format "%-*s %-*s %-*s %-*s"  $widthCol1 "" $widthCol2 "" $widthCol3 "       please check $logfile" $widthCol4 "" ]
 
   if { ![catch {current_instance}] } {
