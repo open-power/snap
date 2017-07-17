e07b10101a21deb00e50e9127951e393e7878b0a
Frank Haverkamp <haver@linux.vnet.ibm.com>
2017-07-17 16:50:28 +0200

    Actions: Restructure action integration - 1st step
    Signed-off-by: Frank Haverkamp <haver@linux.vnet.ibm.com>

---
+++ actions/hls.mk
@@ -0,0 +1,81 @@
+#
+# Copyright 2017 International Business Machines
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Examples:
+#   xcku060-ffva1156-2-e
+#   xc7vx690tffg1157-2
+#
+FPGACHIP    ?= xcku060-ffva1156-2-e
+PART_NUMBER ?= $(FPGACHIP)
+
+# The wrapper name must match a function in the HLS sources which is
+# taken as entry point for the HDL generation.
+WRAPPER ?= hls_action
+
+syn_dir=$(SOLUTION_DIR)_$(PART_NUMBER)/$(SOLUTION_NAME)/syn
+symlinks=vhdl report
+
+# gcc test-bench stuff
+objs = $(srcs:.cpp=.o)
+CXX = g++
+CXXFLAGS = -Wall -W -Wextra -Werror -O2 -DNO_SYNTH -Wno-unknown-pragmas -I../include
+
+all: $(syn_dir) $(symlinks) check
+
+$(syn_dir): $(srcs) run_hls_script.tcl
+	vivado_hls -f run_hls_script.tcl
+	$(RM) -rf $@/systemc $@/verilog
+
+# Create symlink for simpler access
+$(symlinks): $(syn_dir)
+	@ln -sf $(syn_dir)/$@ $@
+
+run_hls_script.tcl: ../../scripts/create_run_hls_script.sh
+	../../scripts/create_run_hls_script.sh	\
+		-n $(SOLUTION_NAME)		\
+		-d $(SOLUTION_DIR) 		\
+		-w $(WRAPPER)			\
+		-p $(PART_NUMBER)		\
+		-f "$(srcs)" > $@
+
+$(SOLUTION_NAME): $(objs)
+	$(CXX) -o $@ $^
+
+# FIXME That those things are not resulting in an error is problematic.
+#      If we get critical warnings we stay away from continuing now,
+#      since that will according to our experience with vivado_hls, lead
+#      to strange problems later on. So let us work on fixing the design
+#      if they occur. Rather than challenging our luck.
+#
+# Check for critical warnings and exit if those occur. Add more if needed.
+# Check for reserved HLS MMIO reg at offset 0x17c.
+# Check for register duplication (0x184/Action_Output_o).
+#
+check: $(symlinks)
+	@echo -n "Checking for critical warnings during HLS synthesis ... "
+	@grep -A8 critical $(SOLUTION_DIR)*/$(SOLUTION_NAME)/$(SOLUTION_NAME).log ; \
+		test $$? = 1
+	@echo "OK"
+	@echo -n "Checking for reserved MMIO area during HLS synthesis ... "
+	@grep -A8 0x17c vhdl/$(WRAPPER)_ctrl_reg_s_axi.vhd | grep reserved > \
+		/dev/null; test $$? = 0
+	@echo "OK"
+#	@grep -A8 0x184 vhdl/$(WRAPPER)_ctrl_reg_s_axi.vhd ; \
+#		test $$? = 1
+
+clean:
+	@$(RM) -r $(SOLUTION_DIR)* run_hls_script.tcl *~ *.log \
+		$(symlinks) $(objs) $(SOLUTION_NAME)
---
+++ actions/software.mk
@@ -0,0 +1,76 @@
+#
+# Copyright 2017 International Business Machines
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+ifndef PSLSE_ROOT
+# FIXME If we find a better way to do the following, let us know:
+#   Environment variable PSLSE_ROOT defined by hardware setup scripts.
+#   Use default path if PSLSE_ROOT is not defined.
+#
+PSLSE_ROOT=$(abspath ../../../../pslse)
+endif
+
+include ../../../software/config.mk
+
+CFLAGS += -std=c99 -I../../../software/include
+DESTDIR ?= /usr
+libs += ../../../software/lib/libsnap.a
+LDLIBS += $(libs) -lpthread
+
+# Link statically for PSLSE simulation and dynamically for real version
+ifdef BUILD_SIMCODE
+libs += $(PSLSE_ROOT)/libcxl/libcxl.a
+CFLAGS += -D_SIM_
+else
+LDLIBS += -lcxl
+endif
+
+# This rule should be the 1st one to find (default)
+all: all_build
+
+# Include sub-Makefile if there are any
+-include *.mk
+
+# This rule needs to be behind all the definitions above
+all_build: $(projs)
+
+$(projs): $(libs)
+
+### Deactivate existing implicit rule
+%: %.c
+%: %.sh
+
+### Generic rule to build a tool
+%: %.o
+	$(CC) $(LDFLAGS) $@.o $($(@)_objs) $($(@)_libs) $(LDLIBS) -o $@
+
+%.o: %.c $(libs)
+	$(CC) -c $(CPPFLAGS) $(CFLAGS) $< -o $@
+
+install: all
+	@mkdir -p $(DESTDIR)/bin
+	@for f in $(projs); do 					\
+		intall -D -m 755 $$f -T $(DESTDIR)/bin/$$f	\
+	done
+
+uninstall:
+	@for f in $(projs) ; do					\
+		echo "removing $(DESTDIR)/bin/$$f ...";		\
+		$(RM) $(DESTDIR)/bin/$$f;			\
+	done
+
+clean distclean:
+	$(RM) $(projs) *.o *.log *.out
+
