5e80f851594ac3e575a8f1ef791df9f908479136
Frank Haverkamp <haver@linux.vnet.ibm.com>
2017-07-18 16:31:41 +0200

    Test: Add example for testcases with the action type in the name
    Signed-off-by: Frank Haverkamp <haver@linux.vnet.ibm.com>

---
+++ actions/hls_hashjoin/tests/test_0x10141002.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+#
+# Copyright 2017 International Business Machines
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+verbose=0
+snap_card=0
+duration="NORMAL"
+
+function usage() {
+    echo "Usage:"
+    echo "  test_<action_type>.sh"
+    echo "    [-C <card>]        card to be used for the test"
+    echo "    [-t <trace_level>]"
+    echo "    [-duration SHORT/NORMAL/LONG] run tests"
+    echo
+}
+
+while getopts ":C:t:d:h" opt; do
+    case $opt in
+	C)
+	snap_card=$OPTARG;
+	;;
+	t)
+	export SNAP_TRACE=$OPTARG;
+	;;
+	d)
+	duration=$OPTARG;
+	;;
+	h)
+	usage;
+	exit 0;
+	;;
+	\?)
+	echo "Invalid option: -$OPTARG" >&2
+	;;
+    esac
+done
+
+export PATH=$PATH:../software/tools
+
+snap_peek --help > /dev/null || exit 1;
+snap_poke --help > /dev/null || exit 1;
+
+#### VERSION ##########################################################
+
+# [ -z "$STATE" ] && echo "Need to set STATE" && exit 1;
+
+if [ -z "$SNAP_CONFIG" ]; then
+	echo "CARD VERSION"
+	snap_peek -C ${snap_card} 0x0 || exit 1;
+	snap_peek -C ${snap_card} 0x8 || exit 1;
+	echo
+fi
+
+#### HASHJOIN #########################################################
+
+export PATH=$PATH:./hls_hashjoin/sw
+
+echo "Doing snap_hashjoin ... "
+rm -f snap_hashjoin.log
+touch snap_hashjoin.log
+for t2_entries in `seq 1 128` 512 666 888 999 1024 2048 2049 5015 7007 8088 123123 ; do
+    echo -n "  ${t2_entries} entries for T2 ... "
+    cmd="snap_hashjoin -C${snap_card} -T ${t2_entries} -v \
+			>> snap_hashjoin.log 2>&1"
+    echo "$cmd" >> snap_hashjoin.log
+    eval ${cmd}
+    if [ $? -ne 0 ]; then
+	cat snap_hashjoin.log
+	echo
+	echo "cmd: ${cmd}"
+	echo "failed"
+	exit 1
+    fi
+    echo "ok"
+done
+
+rm -f *.bin *.bin *.out
+echo "Test OK"
+exit 0
