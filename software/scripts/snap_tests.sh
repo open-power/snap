b7fa3fc2fe47a0555c12923fb3a6b6a21269338d
Frank Haverkamp <haver@linux.vnet.ibm.com>
2017-07-18 11:05:29 +0200

    Test: Adjust path
    Signed-off-by: Frank Haverkamp <haver@linux.vnet.ibm.com>

--- software/scripts/snap_tests.sh
+++ software/scripts/snap_tests.sh
@@ -90,192 +90,202 @@ fi
 #### SEARCH ###########################################################
 
 if [ $search -eq 1 ]; then
-	echo -n "Trying snap_search ... "
-	cmd="./examples/snap_search -C${snap_card} -E 98 	\
-		-i examples/snap_search.txt -p snap >		\
-		examples/snap_search.log 2>&1"
-	eval ${cmd}
-	if [ $? -ne 0 ]; then
-		cat examples/snap_search.log
-		echo "cmd: ${cmd}"
-		echo "failed"
-		exit 1
-	fi
-	echo "ok"
+    export PATH=$PATH:../actions/hls_search/sw
 
-	echo -n "Searching X in 1024 bytes ... "
-	python3 -c 'print("X" * 1024, end="")' > examples/1KiB_X.bin
-	cmd="./examples/snap_search -C${snap_card} -E 1024	\
-		-i examples/1KiB_X.bin -p X >			\
-		examples/snap_search2.log 2>&1"
-	eval ${cmd}
-	if [ $? -ne 0 ]; then
-		cat examples/snap_search2.log
-		echo "cmd: ${cmd}"
-		echo "failed"
-		exit 1
-	fi
-	echo "ok"
+    echo -n "Trying snap_search ... "
+    cmd="snap_search -C${snap_card} -E 98 	\
+		-i snap_search.txt -p snap >		\
+		snap_search.log 2>&1"
+    eval ${cmd}
+    if [ $? -ne 0 ]; then
+	cat snap_search.log
+	echo "cmd: ${cmd}"
+	echo "failed"
+	exit 1
+    fi
+    echo "ok"
 
-	echo -n "Check results ... "
-	grep '1024 patterns' ./examples/snap_search2.log 2>&1 > /dev/null || exit 1
-	if [ $? -ne 0 ]; then
-		echo "failed"
-		exit 1
-	fi
-	echo "ok"
+    echo -n "Searching X in 1024 bytes ... "
+    python3 -c 'print("X" * 1024, end="")' > 1KiB_X.bin
+    cmd="snap_search -C${snap_card} -E 1024	\
+		-i 1KiB_X.bin -p X >			\
+		snap_search2.log 2>&1"
+    eval ${cmd}
+    if [ $? -ne 0 ]; then
+	cat snap_search2.log
+	echo "cmd: ${cmd}"
+	echo "failed"
+	exit 1
+    fi
+    echo "ok"
 
-	echo -n "Searching more X ... "
-	python3 -c 'print("A" * 1024, end="")' > examples/1KiB_A.bin
-	cat examples/1KiB_X.bin examples/1KiB_A.bin examples/1KiB_X.bin > \
-		examples/3KiB.bin
+    echo -n "Check results ... "
+    grep '1024 patterns' snap_search2.log 2>&1 > /dev/null || exit 1
+    if [ $? -ne 0 ]; then
+	echo "failed"
+	exit 1
+    fi
+    echo "ok"
 
-	cmd="./examples/snap_search -C${snap_card} -E 2048	\
-		-i examples/3KiB.bin -p X > 			\
-		examples/snap_search3.log 2>&1"
-	eval ${cmd}
-	if [ $? -ne 0 ]; then
-		cat examples/snap_search3.log
-		echo "cmd: ${cmd}"
-		echo "failed"
-		exit 1
-	fi
+    echo -n "Searching more X ... "
+    python3 -c 'print("A" * 1024, end="")' > 1KiB_A.bin
+    cat 1KiB_X.bin 1KiB_A.bin 1KiB_X.bin > \
+	3KiB.bin
 
-	echo -n "Check results ... "
-	grep '2048 patterns' ./examples/snap_search3.log 2>&1 > /dev/null || exit 1
-	if [ $? -ne 0 ]; then
-		echo "failed"
-		exit 1
-	fi
-	echo "ok"
+    cmd="snap_search -C${snap_card} -E 2048	\
+		-i 3KiB.bin -p X > 			\
+		snap_search3.log 2>&1"
+    eval ${cmd}
+    if [ $? -ne 0 ]; then
+	cat snap_search3.log
+	echo "cmd: ${cmd}"
+	echo "failed"
+	exit 1
+    fi
+
+    echo -n "Check results ... "
+    grep '2048 patterns' snap_search3.log 2>&1 > /dev/null || exit 1
+    if [ $? -ne 0 ]; then
+	echo "failed"
+	exit 1
+    fi
+    echo "ok"
 fi
 
 #### MEMCOPY ##########################################################
 
 if [ $memcopy -eq 1 ]; then
-	python3 -c 'print("A" * 1024, end="")' > examples/1KiB_A.bin
+    export PATH=$PATH:../actions/hls_memcopy/sw
 
-	echo -n "Doing snap_memcopy (aligned)... "
-	cmd="./examples/snap_memcopy -C${snap_card} -X	\
-		-i examples/1KiB_A.bin			\
-		-o examples/1KiB_A.out >		\
-		examples/snap_memcopy.log 2>&1"
-	eval ${cmd}
-	if [ $? -ne 0 ]; then
-		cat examples/snap_memcopy.log
-		echo "cmd: ${cmd}"
-		echo "failed"
-		exit 1
-	fi
-	echo "ok"
+    python3 -c 'print("A" * 1024, end="")' > 1KiB_A.bin
 
-	echo -n "Check results ... "
-	diff examples/1KiB_A.bin examples/1KiB_A.out 2>&1 > /dev/null
-	if [ $? -ne 0 ]; then
-		echo "failed"
-		echo "  examples/1KiB_A.bin examples/1KiB_A.out are different!"
-		exit 1
-	fi
-	echo "ok"
+    echo -n "Doing snap_memcopy (aligned)... "
+    cmd="snap_memcopy -C${snap_card} -X	\
+		-i 1KiB_A.bin			\
+		-o 1KiB_A.out >		\
+		snap_memcopy.log 2>&1"
+    eval ${cmd}
+    if [ $? -ne 0 ]; then
+	cat snap_memcopy.log
+	echo "cmd: ${cmd}"
+	echo "failed"
+	exit 1
+    fi
+    echo "ok"
+
+    echo -n "Check results ... "
+    diff 1KiB_A.bin 1KiB_A.out 2>&1 > /dev/null
+    if [ $? -ne 0 ]; then
+	echo "failed"
+	echo "  1KiB_A.bin 1KiB_A.out are different!"
+	exit 1
+    fi
+    echo "ok"
 fi
 
 #### MEMCOPY CARD #####################################################
 
 if [ $memcopy -eq 1 -a $memcopy_cardram -eq 1 ]; then
-	### Trying DRAM on card ...
-	test_data=examples/LARGE_A.bin
-	python3 -c 'print("A" * (1 * 1024 * 1024), end="")' > $test_data
-	# examples/snap_search.txt
+    export PATH=$PATH:../actions/hls_memcopy/sw
 
-	size=`ls -l $test_data | cut -d' ' -f5`
+    ### Trying DRAM on card ...
+    test_data=LARGE_A.bin
+    python3 -c 'print("A" * (1 * 1024 * 1024), end="")' > $test_data
+    # snap_search.txt
 
-	echo -n "Doing snap_memcopy (CARD_DRAM) ${size} bytes to card ... "
-	cmd="./examples/snap_memcopy -C${snap_card}		\
+    size=`ls -l $test_data | cut -d' ' -f5`
+
+    echo -n "Doing snap_memcopy (CARD_DRAM) ${size} bytes to card ... "
+    cmd="snap_memcopy -C${snap_card}		\
 		-i $test_data -D CARD_DRAM -d 0x00000000 >	\
-		examples/snap_memcopy_card.log 2>&1"
-	eval ${cmd}
-	if [ $? -ne 0 ]; then
-		cat examples/snap_memcopy_card.log
-		echo "cmd: ${cmd}"
-		echo "failed"
-		exit 1
-	fi
-	echo "ok"
+		snap_memcopy_card.log 2>&1"
+    eval ${cmd}
+    if [ $? -ne 0 ]; then
+	cat snap_memcopy_card.log
+	echo "cmd: ${cmd}"
+	echo "failed"
+	exit 1
+    fi
+    echo "ok"
 
-	echo -n "Doing snap_memcopy (CARD_DRAM) ${size} bytes from card... "
-	cmd="./examples/snap_memcopy -C${snap_card}	\
+    echo -n "Doing snap_memcopy (CARD_DRAM) ${size} bytes from card... "
+    cmd="snap_memcopy -C${snap_card}	\
 		-A CARD_DRAM -a 0x00000000 -s ${size}	\
-		-o examples/snap_search.out >>		\
-		examples/snap_memcopy_card.log 2>&1"
-	eval ${cmd}
-	if [ $? -ne 0 ]; then
-		cat examples/snap_memcopy_card.log
-		echo "cmd: ${cmd}"
-		echo "failed"
-		exit 1
-	fi
-	echo "ok"
+		-o snap_search.out >>		\
+		snap_memcopy_card.log 2>&1"
+    eval ${cmd}
+    if [ $? -ne 0 ]; then
+	cat snap_memcopy_card.log
+	echo "cmd: ${cmd}"
+	echo "failed"
+	exit 1
+    fi
+    echo "ok"
 
-	echo -n "Check results ... "
-	diff $test_data examples/snap_search.out 2>&1 > /dev/null
-	if [ $? -ne 0 ]; then
-		echo "failed"
-		echo "  examples/snap_search.txt examples/snap_search.out are different!"
-		exit 1
-	fi
-	echo "ok"
+    echo -n "Check results ... "
+    diff $test_data snap_search.out 2>&1 > /dev/null
+    if [ $? -ne 0 ]; then
+	echo "failed"
+	echo "  snap_search.txt snap_search.out are different!"
+	exit 1
+    fi
+    echo "ok"
 fi
 
 #### MEMCOPY UNALIGNED ################################################
 
 if [ $memcopy -eq 1 -a $memcopy_unaligned -eq 1 ]; then
-	echo -n "Doing snap_memcopy (unaligned)... "
-	cmd="./examples/snap_memcopy -C${snap_card} -X	\
-		-i examples/snap_search.txt		\
-		-o examples/snap_search.out >		\
-		examples/snap_memcopy.log 2>&1"
-	eval ${cmd}
-	if [ $? -ne 0 ]; then
-		cat examples/snap_memcopy.log
-		echo "cmd: ${cmd}"
-		echo "failed"
-		exit 1
-	fi
-	echo "ok"
+    export PATH=$PATH:../actions/hls_memcopy/sw
 
-	echo -n "Check results ... "
-	diff examples/snap_search.txt examples/snap_search.out 2>&1 > /dev/null
-	if [ $? -ne 0 ]; then
-		echo "failed"
-		echo "  examples/snap_search.txt examples/snap_search.out are different!"
-		exit 1
-	fi
-	echo "ok"
+    echo -n "Doing snap_memcopy (unaligned)... "
+    cmd="snap_memcopy -C${snap_card} -X	\
+		-i snap_search.txt		\
+		-o snap_search.out >		\
+		snap_memcopy.log 2>&1"
+    eval ${cmd}
+    if [ $? -ne 0 ]; then
+	cat snap_memcopy.log
+	echo "cmd: ${cmd}"
+	echo "failed"
+	exit 1
+    fi
+    echo "ok"
+
+    echo -n "Check results ... "
+    diff snap_search.txt snap_search.out 2>&1 > /dev/null
+    if [ $? -ne 0 ]; then
+	echo "failed"
+	echo "  snap_search.txt snap_search.out are different!"
+	exit 1
+    fi
+    echo "ok"
 fi
 
 #### HASHJOIN #########################################################
 
 if [ $hashjoin -eq 1 ]; then
-	echo "Doing snap_hashjoin ... "
-	rm -f examples/snap_hashjoin.log
-	touch examples/snap_hashjoin.log
-	for t2_entries in `seq 1 128` 512 666 888 999 1024 2048 2049 5015 7007 8088 123123 ; do
-		echo -n "  ${t2_entries} entries for T2 ... "
-		cmd="./examples/snap_hashjoin -C${snap_card} -T ${t2_entries} -v \
-			>> examples/snap_hashjoin.log 2>&1"
-		echo "$cmd" >> examples/snap_hashjoin.log
-		eval ${cmd}
-		if [ $? -ne 0 ]; then
-			cat examples/snap_hashjoin.log
-			echo
-			echo "cmd: ${cmd}"
-			echo "failed"
-			exit 1
-		fi
-		echo "ok"
-	done
+    export PATH=$PATH:../actions/hls_hashjoin/sw
+
+    echo "Doing snap_hashjoin ... "
+    rm -f snap_hashjoin.log
+    touch snap_hashjoin.log
+    for t2_entries in `seq 1 128` 512 666 888 999 1024 2048 2049 5015 7007 8088 123123 ; do
+	echo -n "  ${t2_entries} entries for T2 ... "
+	cmd="snap_hashjoin -C${snap_card} -T ${t2_entries} -v \
+			>> snap_hashjoin.log 2>&1"
+	echo "$cmd" >> snap_hashjoin.log
+	eval ${cmd}
+	if [ $? -ne 0 ]; then
+	    cat snap_hashjoin.log
+	    echo
+	    echo "cmd: ${cmd}"
+	    echo "failed"
+	    exit 1
+	fi
+	echo "ok"
+    done
 fi
 
-rm -f *.bin examples/*.bin examples/*.out
+rm -f *.bin *.bin *.out
 echo "Test OK"
 exit 0
