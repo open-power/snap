12252ef7c06eed0f5e3c8592c6cbcd98ac451df8
Frank Haverkamp <haver@linux.vnet.ibm.com>
2017-08-17 10:49:14 +0200

    NVMe: Adding a test tool to try the new block lib
    Signed-off-by: Frank Haverkamp <haver@linux.vnet.ibm.com>

--- software/lib/snapblock.c
+++ software/lib/snapblock.c
@@ -48,8 +48,6 @@ pthread_mutex_t globalLock = PTHREAD_MUTEX_INITIALIZER;
 typedef struct {
 	struct snap_card *card;
 	struct snap_action *act;
-	uint8_t *dst;   /* we need 64 byte aligned buffers, grrr */
-	uint8_t *src;   /* we need 64 byte aligned buffers, grrr */
 	unsigned int drive;
 	size_t nblocks; /* total size of the device in blocks */
 	int timeout;
@@ -59,39 +57,39 @@ typedef struct {
 static snap_Chunk_t chunk;
 
 /* Header file for SNAP Framework example code */
-#define ACTION_TYPE_EXAMPLE     0x10140000      /* Action Type */
+#define ACTION_TYPE_EXAMPLE	0x10140000	/* Action Type */
 
-#define ACTION_CONFIG           0x30
-#define ACTION_CONFIG_COUNT     1       /* Count Mode */
-#define ACTION_CONFIG_COPY_HH   2       /* Memcopy Host to Host */
-#define ACTION_CONFIG_COPY_HD   3       /* Memcopy Host to DDR */
-#define ACTION_CONFIG_COPY_DH   4       /* Memcopy DDR to Host */
-#define ACTION_CONFIG_COPY_DD   5       /* Memcopy DDR to DDR */
-#define ACTION_CONFIG_COPY_HDH  6       /* Memcopy Host to DDR to Host */
-#define ACTION_CONFIG_MEMSET_H  8       /* Memset Host Memory */
-#define ACTION_CONFIG_MEMSET_F  9       /* Memset FPGA Memory */
-#define ACTION_CONFIG_COPY_DN   0x0a    /* Copy DDR to NVME drive 0 */
-#define ACTION_CONFIG_COPY_ND   0x0b    /* Copy NVME drive 0 to DDR */
-#define NVME_DRIVE1             0x10    /* Select Drive 1 for 0a and 0b */
+#define ACTION_CONFIG		0x30
+#define ACTION_CONFIG_COUNT	1	/* Count Mode */
+#define ACTION_CONFIG_COPY_HH	2	/* Memcopy Host to Host */
+#define ACTION_CONFIG_COPY_HD	3	/* Memcopy Host to DDR */
+#define ACTION_CONFIG_COPY_DH	4	/* Memcopy DDR to Host */
+#define ACTION_CONFIG_COPY_DD	5	/* Memcopy DDR to DDR */
+#define ACTION_CONFIG_COPY_HDH	6	/* Memcopy Host to DDR to Host */
+#define ACTION_CONFIG_MEMSET_H	8	/* Memset Host Memory */
+#define ACTION_CONFIG_MEMSET_F	9	/* Memset FPGA Memory */
+#define ACTION_CONFIG_COPY_DN	0x0a	/* Copy DDR to NVME drive 0 */
+#define ACTION_CONFIG_COPY_ND	0x0b	/* Copy NVME drive 0 to DDR */
+#define NVME_DRIVE1		0x10	/* Select Drive 1 for 0a and 0b */
 
-#define ACTION_SRC_LOW          0x34    /* LBA for 0A, 1A, 0B and 1B */
-#define ACTION_SRC_HIGH         0x38
-#define ACTION_DEST_LOW         0x3c    /* LBA for 0A, 1A, 0B and 1B */
-#define ACTION_DEST_HIGH        0x40
-#define ACTION_CNT              0x44    /* Count Register or # of 512 Byte Blocks for NVME */
+#define ACTION_SRC_LOW		0x34	/* LBA for 0A, 1A, 0B and 1B */
+#define ACTION_SRC_HIGH		0x38
+#define ACTION_DEST_LOW		0x3c	/* LBA for 0A, 1A, 0B and 1B */
+#define ACTION_DEST_HIGH	0x40
+#define ACTION_CNT		0x44	/* Count Register or # of 512 Byte Blocks for NVME */
 
 /* defaults */
-#define ACTION_WAIT_TIME        10      /* Default timeout in sec */
+#define ACTION_WAIT_TIME	10	/* Default timeout in sec */
 
-#define KILO_BYTE               (1024ull)
-#define MEGA_BYTE               (1024 * KILO_BYTE)
-#define GIGA_BYTE               (1024 * MEGA_BYTE)
-#define DDR_MEM_SIZE            (4 * GIGA_BYTE)   /* Default End of FPGA Ram */
-#define DDR_MEM_BASE_ADDR       0x00000000        /* Default Start of FPGA Ram */
-#define HOST_BUFFER_SIZE        (256 * KILO_BYTE) /* Default Size for Host Buffers */
-#define NVME_LB_SIZE            512               /* NVME Block Size */
-#define NVME_DRIVE_SIZE         (4 * GIGA_BYTE)	  /* NVME Drive Size */
-#define NVME_MAX_TRANSFER_SIZE  (32 * MEGA_BYTE) /* NVME limit to Transfer in one chunk */
+#define KILO_BYTE		(1024ull)
+#define MEGA_BYTE		(1024 * KILO_BYTE)
+#define GIGA_BYTE		(1024 * MEGA_BYTE)
+#define DDR_MEM_SIZE		(4 * GIGA_BYTE)	  /* Default End of FPGA Ram */
+#define DDR_MEM_BASE_ADDR	0x00000000	  /* Default Start of FPGA Ram */
+#define HOST_BUFFER_SIZE	(256 * KILO_BYTE) /* Default Size for Host Buffers */
+#define NVME_LB_SIZE		512		  /* NVME Block Size */
+#define NVME_DRIVE_SIZE		(4 * GIGA_BYTE)	  /* NVME Drive Size */
+#define NVME_MAX_TRANSFER_SIZE	(32 * MEGA_BYTE) /* NVME limit to Transfer in one chunk */
 
 static inline void __free(void *ptr)
 {
@@ -132,7 +130,7 @@ static inline int action_wait_idle(struct snap_card* h, int timeout,
 	return !rc;
 }
 
-static inline void action_memcpy(struct snap_card* h,
+static inline void action_memcpy(struct snap_card *h,
 				 uint32_t action,
 				 uint64_t dest,
 				 uint64_t src,
@@ -140,11 +138,11 @@ static inline void action_memcpy(struct snap_card* h,
 {
 	fprintf(stderr, " memcpy_%x(0x%llx, 0x%llx, 0x%lx) ",
 		action, (long long)dest, (long long)src, n);
-	action_write(h, ACTION_CONFIG,  action);
-	action_write(h, ACTION_DEST_LOW, (uint32_t)(dest & 0xffffffff));
+	action_write(h, ACTION_CONFIG,	  action);
+	action_write(h, ACTION_DEST_LOW,  (uint32_t)(dest & 0xffffffff));
 	action_write(h, ACTION_DEST_HIGH, (uint32_t)(dest >> 32));
-	action_write(h, ACTION_SRC_LOW, (uint32_t)(src & 0xffffffff));
-	action_write(h, ACTION_SRC_HIGH, (uint32_t)(src >> 32));
+	action_write(h, ACTION_SRC_LOW,	  (uint32_t)(src & 0xffffffff));
+	action_write(h, ACTION_SRC_HIGH,  (uint32_t)(src >> 32));
 	action_write(h, ACTION_CNT, n);
 }
 
@@ -166,7 +164,7 @@ chunk_id_t cblk_open(const char *path,
 		     int flags)
 {
 	int timeout = ACTION_WAIT_TIME;
-        unsigned long have_nvme = 0;
+	unsigned long have_nvme = 0;
 	snap_action_flag_t attach_flags =
 		(SNAP_ACTION_DONE_IRQ | SNAP_ATTACH_IRQ);
 
@@ -174,44 +172,35 @@ chunk_id_t cblk_open(const char *path,
 
 	pthread_mutex_lock(&globalLock);
 	if (flags & CBLK_OPN_VIRT_LUN) {
-		pthread_mutex_unlock(&globalLock);
 		fprintf(stderr, "virtual luns not supported in capi stub\n");
-		return (chunk_id_t)(-1);
+		goto out_err0;
 	}
 
 	if (mode != O_RDWR) {
 		fprintf(stderr, "err: Only O_RDWR file mode is supported in capi stub\n");
-		pthread_mutex_unlock(&globalLock);
-		return (chunk_id_t)(-1);
+		goto out_err0;
 	}
 
 	if (chunk.card != NULL) { /* already initialized */
-                pthread_mutex_unlock(&globalLock);
+		pthread_mutex_unlock(&globalLock);
 		return 0;
 	}
 
-	chunk.src = snap_malloc(__CBLK_BLOCK_MAX);
-	if (chunk.src == NULL)
-		goto out_err0;
-	chunk.dst = snap_malloc(__CBLK_BLOCK_MAX);
-	if (chunk.dst == NULL)
-		goto out_err0;
-
-        /* path must match the following scheme: "/dev/cxl/afu%d.0m" */
-        chunk.card = snap_card_alloc_dev(path, SNAP_VENDOR_ID_IBM,
-					   SNAP_DEVICE_ID_SNAP);
-        if (NULL == chunk.card) {
+	/* path must match the following scheme: "/dev/cxl/afu%d.0m" */
+	chunk.card = snap_card_alloc_dev(path, SNAP_VENDOR_ID_IBM,
+					 SNAP_DEVICE_ID_SNAP);
+	if (NULL == chunk.card) {
 		fprintf(stderr, "err: Cannot open SNAP device %s\n", path);
 		goto out_err0;
-        }
+	}
 
-        /* Check if i do have NVME on this card */
-        snap_card_ioctl(chunk.card, GET_NVME_ENABLED, (unsigned long)&have_nvme);
-        if (0 == have_nvme) {
-                fprintf(stderr, "err: SNAP NVMe is not enabled on card %s!\n",
+	/* Check if i do have NVME on this card */
+	snap_card_ioctl(chunk.card, GET_NVME_ENABLED, (unsigned long)&have_nvme);
+	if (0 == have_nvme) {
+		fprintf(stderr, "err: SNAP NVMe is not enabled on card %s!\n",
 			path);
 		goto out_err1;
-        }
+	}
 
 	chunk.act = snap_attach_action(chunk.card, ACTION_TYPE_EXAMPLE,
 				       attach_flags, timeout);
@@ -232,8 +221,6 @@ chunk_id_t cblk_open(const char *path,
  out_err1:
 	snap_card_free(chunk.card);
  out_err0:
-	__free(chunk.dst);
-	__free(chunk.src);
 	pthread_mutex_unlock(&globalLock);
 	return (chunk_id_t)(-1);
 }
@@ -248,11 +235,16 @@ int cblk_close(chunk_id_t id __attribute__((unused)),
 		return -1;
 	}
 
+	snap_detach_action(chunk.act);
 	snap_card_free(chunk.card);
+
+	chunk.act = NULL;
 	chunk.card = NULL;
 	chunk.nblocks = 0;
-	pthread_mutex_unlock(&globalLock);
+	chunk.timeout = 0;
+	chunk.drive = -1;
 
+	pthread_mutex_unlock(&globalLock);
 	return 0;
 }
 
@@ -296,6 +288,10 @@ int cblk_read(chunk_id_t id __attribute__((unused)),
 		errno = EFAULT;
 		return -1;
 	}
+	if ((uint64_t)buf % 64) {
+		errno = EINVAL;
+		return -1;
+	}
 
 	pthread_mutex_lock(&globalLock);
 
@@ -309,13 +305,12 @@ int cblk_read(chunk_id_t id __attribute__((unused)),
 		goto __exit1;
 
 	/* HOST <- DDR */
-	action_memcpy(chunk.card, ACTION_CONFIG_COPY_DH, (uint64_t)chunk.dst, ddr_dest, mem_size);
+	action_memcpy(chunk.card, ACTION_CONFIG_COPY_DH, (uint64_t)buf, ddr_dest, mem_size);
 	rc = action_wait_idle(chunk.card, chunk.timeout, mem_size);
 	if (rc)
 		goto __exit1;
 
-	memcpy(buf, chunk.dst, mem_size);
- 	pthread_mutex_unlock(&globalLock);
+	pthread_mutex_unlock(&globalLock);
 	return nblocks;
 
  __exit1:
@@ -339,12 +334,15 @@ int cblk_write(chunk_id_t id __attribute__((unused)),
 		errno = EFAULT;
 		return -1;
 	}
+	if ((uint64_t)buf % 64) {
+		errno = EINVAL;
+		return -1;
+	}
 
 	pthread_mutex_lock(&globalLock);
-	memcpy(chunk.src, buf, __CBLK_BLOCK_SIZE * nblocks);
 
 	/* DDR <- HOST */
-	action_memcpy(chunk.card, ACTION_CONFIG_COPY_HD, ddr_src, (uint64_t)chunk.src, mem_size);
+	action_memcpy(chunk.card, ACTION_CONFIG_COPY_HD, ddr_src, (uint64_t)buf, mem_size);
 	rc = action_wait_idle(chunk.card, chunk.timeout, mem_size);
 	if (rc)
 		goto __exit1;
