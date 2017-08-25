313ee09cd81b29c2096c01cf1f5b4a35c06c00c2
luyong6 <luyong@cn.ibm.com>
2017-08-25 10:59:52 +0200

    HLS add new action hls_nvme_memcopy

---
+++ actions/hls_nvme_memcopy/sw/snap_nvme_memcopy.c
@@ -0,0 +1,388 @@
+/*
+ * Copyright 2016, 2017 International Business Machines
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <getopt.h>
+#include <malloc.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <assert.h>
+
+#include <snap_tools.h>
+#include <action_nvme_memcopy.h>
+#include <libsnap.h>
+#include <snap_hls_if.h>
+
+int verbose_flag = 0;
+
+static const char *version = GIT_VERSION;
+
+static const char *mem_tab[] = { "HOST_DRAM", "CARD_DRAM", "NVME_SSD" };
+
+/**
+ * @brief	prints valid command line options
+ *
+ * @param prog	current program's name
+ */
+static void usage(const char *prog)
+{
+	printf("Usage: %s [-h] [-v, --verbose] [-V, --version]\n"
+	       "  -C, --card <cardno> can be (0...3)\n"
+	       "  -i, --input  <file.bin>   input file  (HOST).\n"
+	       "  -o, --output <file.bin>   output file (HOST).\n"
+	       "  -A, --type-in  <NVME_SSD, HOST_DRAM, CARD_DRAM>.\n"
+	       "  -a, --addr-in  <addr>     byte address in CARD_DRAM or NVME_SSD.\n"
+	       "  -D, --type-out <NVME_SSD, HOST_DRAM, CARD_DRAM>.\n"
+	       "  -d, --addr-out <addr>     byte address in CARD_DRAM or NVME_SSD.\n"
+	       "  -n, --drv-id   <0/1>      drive_id if NVME_SSD is used (default: 0)\n"
+	       "  -s, --size <size>         size of data (in bytes).\n"
+	       "  -m, --mode <mode>         mode flags.\n"
+	       "  -t, --timeout             Timeout in sec to wait for done. (10 sec default)\n"
+	       "  -X, --verify              verify result if possible\n"
+	       "  -I, --irq                 Enable Interrupts\n"
+	       "\n"
+	       "Example:\n"
+           "  snap_nvme_memcopy -A HOST_DRAM -D HOST_DRAM -i in.bin -o out.bin ...\n"
+           "  snap_nvme_memcopy -A HOST_DRAM -D CARD_DRAM -i in.bin -d 0xD000 ...\n"
+           "  snap_nvme_memcopy -A HOST_DRAM -D NVME_SSD  -i in.bin -d 0xE000 ...\n"
+           "\n"
+           "  snap_nvme_memcopy -A CARD_DRAM -D HOST_DRAM -a 0xD000 -o out.bin -s 0x200 ...\n"
+           "  snap_nvme_memcopy -A CARD_DRAM -D NVME_SSD  -a 0xD000 -d 0xE000 -s 0x200 ...\n"
+           "  snap_nvme_memcopy -A CARD_DRAM -D CARD_DRAM -a 0xD000 -d 0xD200 -s 0x200 ...\n"
+           "\n"
+           "  snap_nvme_memcopy -A NVME_SSD -D CARD_DRAM -a 0xE000 -d 0xD000 -s 0x200 ...\n"
+           "  snap_nvme_memcopy -A NVME_SSD -D HOST_DRAM -a 0xE000 -o out.bin -s 0x200 ...\n"
+           "\n"
+           " 1) In Above examples, all addresses are byte address. \n"
+           "    CARD_DRAM address limit is 0x1_0000_0000. \n"
+           "    NVME_SSD  address limit is 0xDF_9035_6000 for one drive.\n"
+           "    If Source or Destination is NVME_SSD, size must be multiples of 512 (0x200)\n"
+           " 2) NVME to NVME is not directly supported,\n"
+           "    but can be done by calling snap_nvme_memcopy twice.\n"
+	       "\n",
+	       prog);
+}
+
+static void snap_prepare_nvme_memcopy(struct snap_job *cjob,
+				 struct nvme_memcopy_job *mjob,
+				 void *addr_in,
+				 uint32_t size_in,
+				 uint8_t type_in,
+				 void *addr_out,
+				 uint32_t size_out,
+				 uint8_t type_out,
+                 uint64_t drive_id)
+{
+	fprintf(stderr, "  prepare nvme_memcopy job of %ld bytes size\n", sizeof(*mjob));
+
+	assert(sizeof(*mjob) <= SNAP_JOBSIZE);
+	memset(mjob, 0, sizeof(*mjob));
+
+	snap_addr_set(&mjob->in, addr_in, size_in, type_in,
+		      SNAP_ADDRFLAG_ADDR | SNAP_ADDRFLAG_SRC);
+	snap_addr_set(&mjob->out, addr_out, size_out, type_out,
+		      SNAP_ADDRFLAG_ADDR | SNAP_ADDRFLAG_DST |
+		      SNAP_ADDRFLAG_END);
+    mjob->drive_id = drive_id;
+
+	snap_job_set(cjob, mjob, sizeof(*mjob), NULL, 0);
+}
+
+/**
+ * Read accelerator specific registers. Must be called as root!
+ */
+int main(int argc, char *argv[])
+{
+	int ch, rc = 0;
+	int card_no = 0;
+	struct snap_card *card = NULL;
+	struct snap_action *action = NULL;
+	char device[128];
+	struct snap_job cjob;
+	struct nvme_memcopy_job mjob;
+	const char *input = NULL;
+	const char *output = NULL;
+	unsigned long timeout = 10;
+	unsigned int mode = 0x0;
+	const char *space = "CARD_RAM";
+	struct timeval etime, stime;
+	ssize_t size = 1024 * 1024;
+	uint8_t *ibuff = NULL, *obuff = NULL;
+	uint8_t type_in = SNAP_ADDRTYPE_HOST_DRAM;
+	uint64_t addr_in = 0x0ull;
+	uint8_t type_out = SNAP_ADDRTYPE_HOST_DRAM;
+	uint64_t addr_out = 0x0ull;
+	int verify = 0;
+    uint64_t drive_id = 0;
+	int exit_code = EXIT_SUCCESS;
+	uint8_t trailing_zeros[1024] = { 0, };
+	snap_action_flag_t action_irq = 0;
+
+	while (1) {
+		int option_index = 0;
+		static struct option long_options[] = {
+			{ "card",	 required_argument, NULL, 'C' },
+			{ "input",	 required_argument, NULL, 'i' },
+			{ "output",	 required_argument, NULL, 'o' },
+			{ "src-type",	 required_argument, NULL, 'A' },
+			{ "src-addr",	 required_argument, NULL, 'a' },
+			{ "dst-type",	 required_argument, NULL, 'D' },
+			{ "dst-addr",	 required_argument, NULL, 'd' },
+			{ "drv-id",	 required_argument, NULL, 'n' },
+			{ "size",	 required_argument, NULL, 's' },
+			{ "mode",	 required_argument, NULL, 'm' },
+			{ "timeout",	 required_argument, NULL, 't' },
+			{ "verify",	 no_argument,	    NULL, 'X' },
+			{ "version",	 no_argument,	    NULL, 'V' },
+			{ "verbose",	 no_argument,	    NULL, 'v' },
+			{ "help",	 no_argument,	    NULL, 'h' },
+			{ "irq",	 no_argument,	    NULL, 'I' },
+			{ 0,		 no_argument,	    NULL, 0   },
+		};
+
+		ch = getopt_long(argc, argv,
+				 "A:C:i:o:a:S:D:d:n:x:s:t:XVqvhI",
+				 long_options, &option_index);
+		if (ch == -1)
+			break;
+
+		switch (ch) {
+		case 'C':
+			card_no = strtol(optarg, (char **)NULL, 0);
+			break;
+		case 'i':
+			input = optarg;
+			break;
+		case 'o':
+			output = optarg;
+			break;
+		case 's':
+			size = __str_to_num(optarg);
+			break;
+		case 't':
+			timeout = strtol(optarg, (char **)NULL, 0);
+			break;
+		case 'm':
+			mode = strtol(optarg, (char **)NULL, 0);
+			break;
+			/* input data */
+		case 'A':
+			space = optarg;
+			if (strcmp(space, "NVME_SSD") == 0)
+				type_in = SNAP_ADDRTYPE_NVME;
+			else if (strcmp(space, "HOST_DRAM") == 0)
+				type_in = SNAP_ADDRTYPE_HOST_DRAM;
+			else if (strcmp(space, "CARD_DRAM") == 0)
+				type_in = SNAP_ADDRTYPE_CARD_DRAM;
+			else {
+				usage(argv[0]);
+				exit(EXIT_FAILURE);
+			}
+			break;
+		case 'a':
+			addr_in = strtol(optarg, (char **)NULL, 0);
+			break;
+			/* output data */
+		case 'D':
+			space = optarg;
+			if (strcmp(space, "NVME_SSD") == 0)
+				type_out = SNAP_ADDRTYPE_NVME;
+			else if (strcmp(space, "HOST_DRAM") == 0)
+				type_out = SNAP_ADDRTYPE_HOST_DRAM;
+			else if (strcmp(space, "CARD_DRAM") == 0)
+				type_out = SNAP_ADDRTYPE_CARD_DRAM;
+			else {
+				usage(argv[0]);
+				exit(EXIT_FAILURE);
+			}
+			break;
+		case 'd':
+			addr_out = strtol(optarg, (char **)NULL, 0);
+			break;
+		case 'n':
+			drive_id = strtol(optarg, (char **)NULL, 0);
+			break;
+		case 'X':
+			verify++;
+			break;
+			/* service */
+		case 'V':
+			printf("%s\n", version);
+			exit(EXIT_SUCCESS);
+		case 'v':
+			verbose_flag = 1;
+			break;
+		case 'h':
+			usage(argv[0]);
+			exit(EXIT_SUCCESS);
+			break;
+		case 'I':
+			action_irq = (SNAP_ACTION_DONE_IRQ | SNAP_ATTACH_IRQ);
+			break;
+		default:
+			usage(argv[0]);
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	if (optind != argc) {
+		usage(argv[0]);
+		exit(EXIT_FAILURE);
+	}
+
+	/* if input file is defined, use that as input */
+	if (input != NULL) {
+		size = __file_size(input);
+		if (size < 0)
+			goto out_error;
+
+		/* source buffer */
+		ibuff = snap_malloc(size);
+		if (ibuff == NULL)
+			goto out_error;
+		memset(ibuff, 0, size);
+
+		fprintf(stdout, "reading input data %d bytes from %s\n",
+			(int)size, input);
+
+		rc = __file_read(input, ibuff, size);
+		if (rc < 0)
+			goto out_error;
+
+		type_in = SNAP_ADDRTYPE_HOST_DRAM;
+		addr_in = (unsigned long)ibuff;
+	}
+
+	/* if output file is defined, use that as output */
+	if (output != NULL) {
+		size_t set_size = size + (verify ? sizeof(trailing_zeros) : 0);
+
+		obuff = snap_malloc(set_size);
+		if (obuff == NULL)
+			goto out_error;
+		memset(obuff, 0x0, set_size);
+		type_out = SNAP_ADDRTYPE_HOST_DRAM;
+		addr_out = (unsigned long)obuff;
+	}
+
+	printf("PARAMETERS:\n"
+	       "  input:       %s\n"
+	       "  output:      %s\n"
+	       "  type_in:     %x %s\n"
+	       "  addr_in:     %016llx\n"
+	       "  type_out:    %x %s\n"
+	       "  addr_out:    %016llx\n"
+           "  drive_id:    %ld\n"
+	       "  size_in/out: %08lx\n"
+	       "  mode:        %08x\n",
+	       input  ? input  : "unknown",
+	       output ? output : "unknown",
+	       type_in,  mem_tab[type_in],  (long long)addr_in,
+	       type_out, mem_tab[type_out], (long long)addr_out,
+           (long) drive_id,
+	       size, mode);
+
+	snprintf(device, sizeof(device)-1, "/dev/cxl/afu%d.0s", card_no);
+	card = snap_card_alloc_dev(device, SNAP_VENDOR_ID_IBM,
+				   SNAP_DEVICE_ID_SNAP);
+	if (card == NULL) {
+		fprintf(stderr, "err: failed to open card %u: %s\n",
+			card_no, strerror(errno));
+		goto out_error;
+	}
+
+	action = snap_attach_action(card, NVME_MEMCOPY_ACTION_TYPE, action_irq, 60);
+	if (action == NULL) {
+		fprintf(stderr, "err: failed to attach action %u: %s\n",
+			card_no, strerror(errno));
+		goto out_error1;
+	}
+
+	snap_prepare_nvme_memcopy(&cjob, &mjob,
+			     (void *)addr_in,  size, type_in,
+			     (void *)addr_out, size, type_out, drive_id);
+
+	__hexdump(stderr, &mjob, sizeof(mjob));
+
+	gettimeofday(&stime, NULL);
+	rc = snap_action_sync_execute_job(action, &cjob, timeout);
+	gettimeofday(&etime, NULL);
+	if (rc != 0) {
+		fprintf(stderr, "err: job execution %d: %s!\n", rc,
+			strerror(errno));
+		goto out_error2;
+	}
+
+	/* If the output buffer is in host DRAM we can write it to a file */
+	if (output != NULL) {
+		fprintf(stdout, "writing output data %p %d bytes to %s\n",
+			obuff, (int)size, output);
+
+		rc = __file_write(output, obuff, size);
+		if (rc < 0)
+			goto out_error2;
+	}
+
+	/* obuff[size] = 0xff; */
+	fprintf(stdout, "RETC=%x\n", cjob.retc);
+	if (cjob.retc != SNAP_RETC_SUCCESS) {
+		fprintf(stderr, "err: Unexpected RETC=%x!\n", cjob.retc);
+		goto out_error2;
+	}
+
+	if (verify) {
+		if ((type_in  == SNAP_ADDRTYPE_HOST_DRAM) &&
+		    (type_out == SNAP_ADDRTYPE_HOST_DRAM)) {
+			rc = memcmp(ibuff, obuff, size);
+			if (rc != 0)
+				exit_code = EX_ERR_VERIFY;
+
+			rc = memcmp(obuff + size, trailing_zeros, 1024);
+			if (rc != 0) {
+				fprintf(stderr, "err: trailing zero "
+					"verification failed!\n");
+				__hexdump(stderr, obuff + size, 1024);
+				exit_code = EX_ERR_VERIFY;
+			}
+
+		} else
+			fprintf(stderr, "warn: Verification works currently "
+				"only with HOST_DRAM\n");
+	}
+	fprintf(stdout, "nvme_memcopy took %lld usec\n",
+		(long long)timediff_usec(&etime, &stime));
+
+	snap_detach_action(action);
+	snap_card_free(card);
+
+	__free(obuff);
+	__free(ibuff);
+	exit(exit_code);
+
+ out_error2:
+	snap_detach_action(action);
+ out_error1:
+	snap_card_free(card);
+ out_error:
+	__free(obuff);
+	__free(ibuff);
+	exit(EXIT_FAILURE);
+}
