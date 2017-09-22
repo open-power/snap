2349da08f4a892b7e18a5b3e28d4fd179d781eab
Frank Haverkamp <haver@linux.vnet.ibm.com>
2017-09-22 10:42:28 +0200

    CBLK: Adding data structures for cache, replace global lock by device lock
    Signed-off-by: Frank Haverkamp <haver@linux.vnet.ibm.com>

--- software/lib/snapblock.c
+++ software/lib/snapblock.c
@@ -49,8 +49,6 @@
 #define SNAP_FLASHGT_NVME_SIZE (1ull * 1024 * 1024 * 1024) /* FIXME n TiB */
 #define __CBLK_BLOCK_SIZE 4096
 
-pthread_mutex_t globalLock = PTHREAD_MUTEX_INITIALIZER;
-
 enum cblk_status {
 	CBLK_IDLE = 0,
 	CBLK_READING = 1,
@@ -72,6 +70,8 @@ struct cblk_req {
 struct cblk_dev {
 	struct snap_card *card;
 	struct snap_action *act;
+	pthread_mutex_t dev_lock;
+
 	unsigned int drive;
 	size_t nblocks; /* total size of the device in blocks */
 	int timeout;
@@ -84,10 +84,12 @@ struct cblk_dev {
 	pthread_t done_tid;
 };
 
-/* Use just one ... */
-static struct cblk_dev chunk;
+/* Use just one for now ... */
+static struct cblk_dev chunk = {
+	.dev_lock = PTHREAD_MUTEX_INITIALIZER,
+};
 
-/* Header file for SNAP Framework example code */
+/* Action related definitions. Used to access the hardware */
 #define ACTION_TYPE_NVME_EXAMPLE	0x10140001	/* Action Type */
 
 #define ACTION_CONFIG		0x30
@@ -126,6 +128,32 @@ static const char *action_name[] = {
 #define NVME_LB_SIZE		512		  /* NVME Block Size */
 #define NVME_DRIVE_SIZE		(4 * GIGA_BYTE)	  /* NVME Drive Size */
 #define NVME_MAX_TRANSFER_SIZE	(32 * MEGA_BYTE)  /* NVME limit to Transfer in one chunk */
+
+/* NVME lba cache */
+ #define CBLK_MASK	0x000000ff
+#define CBLK_WAYS	4
+#define CBLK_ENTRIES	(CBLK_MASK + 1)
+
+enum cblk_block_status {
+	CBLK_BLOCK_UNUSED = 0,	/* not in use yset */
+	CLBK_BLOCK_VALID = 1,	/* data is valid  for specified lba */
+	CBLK_BLOCK_DIRTY = 2,	/* data was written */
+};
+
+struct cblk_way {
+	enum cblk_block_status status;
+	off_t lba;		/* lba this cache entry is for */
+	size_t nblocks;		/* use 1 to keep things simple */
+	unsigned int used;	/* # times this block was used */
+	void *buf;		/* data if status is CBLK_BLOCK_VALID */
+};
+
+struct cblk_cache {
+	pthread_mutex_t way_lock;
+	struct cblk_way way[CBLK_WAYS];
+};
+
+struct cblk_cache cache[CBLK_ENTRIES];
 
 static inline void __free(void *ptr)
 {
@@ -273,15 +301,16 @@ chunk_id_t cblk_open(const char *path,
 	unsigned long have_nvme = 0;
 	snap_action_flag_t attach_flags =
 		(SNAP_ACTION_DONE_IRQ | SNAP_ATTACH_IRQ);
+	struct cblk_dev *c = &chunk;
 
 	block_trace("%s: opening (%s) ...\n", __func__, path);
 
-	pthread_mutex_lock(&globalLock);
+	pthread_mutex_lock(&c->dev_lock);
 
-	for (i = 0; i < ARRAY_SIZE(chunk.req); i++) {
-		chunk.req[i].num = 0;
-		chunk.req[i].status = CBLK_IDLE;
-		sem_init(&chunk.req[i].wait_sem, 0, i);
+	for (i = 0; i < ARRAY_SIZE(c->req); i++) {
+		c->req[i].num = 0;
+		c->req[i].status = CBLK_IDLE;
+		sem_init(&c->req[i].wait_sem, 0, i);
 	}
 
 	if (flags & CBLK_OPN_VIRT_LUN) {
@@ -294,60 +323,60 @@ chunk_id_t cblk_open(const char *path,
 		goto out_err0;
 	}
 
-	if (chunk.card != NULL) { /* already initialized */
-		pthread_mutex_unlock(&globalLock);
+	if (c->card != NULL) { /* already initialized */
+		pthread_mutex_unlock(&c->dev_lock);
 		return 0;
 	}
 
 	/* path must match the following scheme: "/dev/cxl/afu%d.0m" */
-	chunk.card = snap_card_alloc_dev(path, SNAP_VENDOR_ID_IBM,
+	c->card = snap_card_alloc_dev(path, SNAP_VENDOR_ID_IBM,
 					 SNAP_DEVICE_ID_SNAP);
-	if (NULL == chunk.card) {
+	if (NULL == c->card) {
 		fprintf(stderr, "err: Cannot open SNAP device %s\n", path);
 		goto out_err0;
 	}
 
 	/* Check if i do have NVME on this card */
-	snap_card_ioctl(chunk.card, GET_NVME_ENABLED, (unsigned long)&have_nvme);
+	snap_card_ioctl(c->card, GET_NVME_ENABLED, (unsigned long)&have_nvme);
 	if (0 == have_nvme) {
 		fprintf(stderr, "err: SNAP NVMe is not enabled on card %s!\n",
 			path);
 		goto out_err1;
 	}
 
-	chunk.act = snap_attach_action(chunk.card, ACTION_TYPE_NVME_EXAMPLE,
+	c->act = snap_attach_action(c->card, ACTION_TYPE_NVME_EXAMPLE,
 					attach_flags, timeout);
-	if (NULL == chunk.act) {
+	if (NULL == c->act) {
 		fprintf(stderr, "err: Cannot Attach Action: %x\n",
 			ACTION_TYPE_NVME_EXAMPLE);
 		goto out_err1;
 	}
 
-	chunk.buf = snap_malloc(__CBLK_BLOCK_SIZE * 2);
-	chunk.drive = 0;
-	chunk.nblocks = SNAP_FLASHGT_NVME_SIZE / __CBLK_BLOCK_SIZE;
-	chunk.timeout = timeout;
-	chunk.done_tid = 0;
-	chunk.widx = 0;
-	chunk.ridx = 0;
+	c->buf = snap_malloc(__CBLK_BLOCK_SIZE * 2);
+	c->drive = 0;
+	c->nblocks = SNAP_FLASHGT_NVME_SIZE / __CBLK_BLOCK_SIZE;
+	c->timeout = timeout;
+	c->done_tid = 0;
+	c->widx = 0;
+	c->ridx = 0;
 
-	rc = pthread_create(&chunk.done_tid, NULL, &done_thread, &chunk);
+	rc = pthread_create(&c->done_tid, NULL, &done_thread, &chunk);
 	if (rc != 0)
 		goto out_err2;
 
-	pthread_mutex_unlock(&globalLock);
+	pthread_mutex_unlock(&c->dev_lock);
 	return 0;
 
  out_err2:
-	snap_detach_action(chunk.act);
+	snap_detach_action(c->act);
  out_err1:
-	snap_card_free(chunk.card);
+	snap_card_free(c->card);
  out_err0:
- 	for (i = 0; i < ARRAY_SIZE(chunk.req); i++) {
-		chunk.req[i].status = CBLK_IDLE;
-		sem_destroy(&chunk.req[i].wait_sem);
+ 	for (i = 0; i < ARRAY_SIZE(c->req); i++) {
+		c->req[i].status = CBLK_IDLE;
+		sem_destroy(&c->req[i].wait_sem);
 	}
-	pthread_mutex_unlock(&globalLock);
+	pthread_mutex_unlock(&c->dev_lock);
 	return (chunk_id_t)(-1);
 }
 
@@ -355,41 +384,42 @@ int cblk_close(chunk_id_t id __attribute__((unused)),
 	       int flags __attribute__((unused)))
 {
 	unsigned int i;
+	struct cblk_dev *c = &chunk;
 
-	pthread_mutex_lock(&globalLock);
-	if (chunk.card == NULL) {
-		pthread_mutex_unlock(&globalLock);
+	pthread_mutex_lock(&c->dev_lock);
+	if (c->card == NULL) {
+		pthread_mutex_unlock(&c->dev_lock);
 		errno = EINVAL;
 		return -1;
 	}
 
-	if (chunk.done_tid) {
-		pthread_cancel(chunk.done_tid);
-		pthread_join(chunk.done_tid, NULL);
-		chunk.done_tid = 0;
+	if (c->done_tid) {
+		pthread_cancel(c->done_tid);
+		pthread_join(c->done_tid, NULL);
+		c->done_tid = 0;
 	}
 
 	block_trace("%s: id=%d ...\n", __func__, (int)id);
 
-	for (i = 0; i < ARRAY_SIZE(chunk.req); i++) {
+	for (i = 0; i < ARRAY_SIZE(c->req); i++) {
 		block_trace("  req[%2d]: %s %d\n", i,
-			status_str[chunk.req[i].status],
-			chunk.req[i].num);
-		chunk.req[i].status = CBLK_IDLE;
-		sem_destroy(&chunk.req[i].wait_sem);
+			status_str[c->req[i].status],
+			c->req[i].num);
+		c->req[i].status = CBLK_IDLE;
+		sem_destroy(&c->req[i].wait_sem);
 	}
 
-	snap_detach_action(chunk.act);
-	snap_card_free(chunk.card);
-	__free(chunk.buf);
+	snap_detach_action(c->act);
+	snap_card_free(c->card);
+	__free(c->buf);
 
-	chunk.act = NULL;
-	chunk.card = NULL;
-	chunk.nblocks = 0;
-	chunk.timeout = 0;
-	chunk.drive = -1;
+	c->act = NULL;
+	c->card = NULL;
+	c->nblocks = 0;
+	c->timeout = 0;
+	c->drive = -1;
 
-	pthread_mutex_unlock(&globalLock);
+	pthread_mutex_unlock(&c->dev_lock);
 	return 0;
 }
 
@@ -397,10 +427,12 @@ int cblk_get_lun_size(chunk_id_t id __attribute__((unused)),
 		      size_t *size __attribute__((unused)),
 		      int flags __attribute__((unused)))
 {
+	struct cblk_dev *c = &chunk;
+
 	block_trace("%s: lun_size=%zu block of %d bytes ...\n",
-		__func__, chunk.nblocks, __CBLK_BLOCK_SIZE);
+		__func__, c->nblocks, __CBLK_BLOCK_SIZE);
 	if (size)
-		*size = chunk.nblocks;
+		*size = c->nblocks;
 	return 0;
 }
 
@@ -418,9 +450,8 @@ int cblk_set_size(chunk_id_t id __attribute__((unused)),
 	return -1;
 }
 
-int cblk_read(chunk_id_t id __attribute__((unused)),
-	      void *buf, off_t lba, size_t nblocks,
-	      int flags  __attribute__((unused)))
+static int block_read(struct cblk_dev *c, void *buf, off_t lba,
+		size_t nblocks)
 {
 	int rc;
 	uint32_t mem_size = __CBLK_BLOCK_SIZE * nblocks;
@@ -436,48 +467,53 @@ int cblk_read(chunk_id_t id __attribute__((unused)),
 			errno = EFAULT;
 			return -1;
 		}
-		_buf = chunk.buf;
+		_buf = c->buf;
 	}
+	
+	pthread_mutex_lock(&c->dev_lock);
 
-	pthread_mutex_lock(&globalLock);
-
-	chunk.req[CBLK_WIDX_MAX + chunk.ridx].status = CBLK_READING;
-	chunk.req[CBLK_WIDX_MAX + chunk.ridx].num++;
-	chunk_memcpy(&chunk,
-		ACTION_CONFIG_COPY_NH | ((CBLK_WIDX_MAX + chunk.ridx) << 8), /* NVMe to Host DDR */
+	c->req[CBLK_WIDX_MAX + c->ridx].status = CBLK_READING;
+	c->req[CBLK_WIDX_MAX + c->ridx].num++;
+	chunk_memcpy(c,
+		ACTION_CONFIG_COPY_NH | ((CBLK_WIDX_MAX + c->ridx) << 8), /* NVMe to Host DDR */
 		(uint64_t)buf,					/* dst */
 		lba * __CBLK_BLOCK_SIZE/NVME_LB_SIZE,		/* src */
 		mem_size);					/* size */
 
 	/* FIXME Kick the done thread */
-	rc = chunk_wait_idle(&chunk, chunk.timeout, mem_size);
+	rc = chunk_wait_idle(c, c->timeout, mem_size);
 	if (rc)
 		goto __exit1;
 
 	if ((uint64_t)buf % 64)
 		memcpy(buf, _buf, nblocks * __CBLK_BLOCK_SIZE);
 
-	chunk.req[CBLK_WIDX_MAX + chunk.ridx].status = CBLK_IDLE;
-	chunk.ridx = (chunk.ridx + 1) % CBLK_RIDX_MAX;
+	c->req[CBLK_WIDX_MAX + c->ridx].status = CBLK_IDLE;
+	c->ridx = (c->ridx + 1) % CBLK_RIDX_MAX;
 	
-	pthread_mutex_unlock(&globalLock);
+	pthread_mutex_unlock(&c->dev_lock);
 	block_trace("%s: exit lba=%zu nblocks=%zu\n",
 		__func__, lba, nblocks);
 	return nblocks;
 
  __exit1:
-	chunk.req[CBLK_WIDX_MAX + chunk.ridx].status = CBLK_ERROR;
-	pthread_mutex_unlock(&globalLock);
+	c->req[CBLK_WIDX_MAX + c->ridx].status = CBLK_ERROR;
+	pthread_mutex_unlock(&c->dev_lock);
 	return -1;
 }
 
-int cblk_write(chunk_id_t id __attribute__((unused)),
-	       void *buf, off_t lba, size_t nblocks,
-	       int flags __attribute__((unused)))
+int cblk_read(chunk_id_t id __attribute__((unused)),
+	      void *buf, off_t lba, size_t nblocks,
+	      int flags  __attribute__((unused)))
+{
+	return block_read(&chunk, buf, lba, nblocks);
+}
+
+static int block_write(struct cblk_dev *c, void *buf, off_t lba,
+		size_t nblocks)
 {
 	int rc;
 	uint32_t mem_size = __CBLK_BLOCK_SIZE * nblocks;
-
 	block_trace("%s: writing (%p lba=%zu nblocks=%zu) ...\n",
 		__func__, buf, lba, nblocks);
 	if ((uint64_t)buf % 64) {
@@ -488,36 +524,43 @@ int cblk_write(chunk_id_t id __attribute__((unused)),
 			errno = EFAULT;
 			return -1;
 		}
-		memcpy(chunk.buf, buf, nblocks * __CBLK_BLOCK_SIZE);
-		buf = chunk.buf;
+		memcpy(c->buf, buf, nblocks * __CBLK_BLOCK_SIZE);
+		buf = c->buf;
 	}
 
-	pthread_mutex_lock(&globalLock);
+	pthread_mutex_lock(&c->dev_lock);
 
-	chunk.req[chunk.widx].status = CBLK_WRITING;
-	chunk.req[chunk.widx].num++;
-	chunk_memcpy(&chunk,
-		ACTION_CONFIG_COPY_HN | (chunk.widx << 8),	/* Host DDR to NVMe */
+	c->req[c->widx].status = CBLK_WRITING;
+	c->req[c->widx].num++;
+	chunk_memcpy(c,
+		ACTION_CONFIG_COPY_HN | (c->widx << 8),	/* Host DDR to NVMe */
 		lba * __CBLK_BLOCK_SIZE/NVME_LB_SIZE,		/* dst */
 		(uint64_t)buf,					/* src */
 		mem_size);					/* size */
 
 	/* FIXME Kick the done thread */
-	rc = chunk_wait_idle(&chunk, chunk.timeout, mem_size);
+	rc = chunk_wait_idle(c, c->timeout, mem_size);
 	if (rc)
 		goto __exit1;
 
-	chunk.req[chunk.widx].status = CBLK_IDLE;
-	chunk.widx = (chunk.widx + 1) % CBLK_WIDX_MAX;
+	c->req[c->widx].status = CBLK_IDLE;
+	c->widx = (c->widx + 1) % CBLK_WIDX_MAX;
 
-	pthread_mutex_unlock(&globalLock);
+	pthread_mutex_unlock(&c->dev_lock);
 	block_trace("%s: exit lba=%zu nblocks=%zu\n", __func__, lba, nblocks);
 	return nblocks;
 
  __exit1:
-	chunk.req[chunk.widx].status = CBLK_ERROR;
-	pthread_mutex_unlock(&globalLock);
+	c->req[c->widx].status = CBLK_ERROR;
+	pthread_mutex_unlock(&c->dev_lock);
 	return -1;
+}
+
+int cblk_write(chunk_id_t id __attribute__((unused)),
+	       void *buf, off_t lba, size_t nblocks,
+	       int flags __attribute__((unused)))
+{
+	return block_write(&chunk, buf, lba, nblocks);
 }
 
 static void _init(void) __attribute__((constructor));
