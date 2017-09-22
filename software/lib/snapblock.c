b0fc592e9fcf7d658bbdb34599067c86a74c5cf4
Frank Haverkamp <haver@linux.vnet.ibm.com>
2017-09-22 16:58:45 +0200

    CBLK: Adding a simple caching layer which is not active yet
    Signed-off-by: Frank Haverkamp <haver@linux.vnet.ibm.com>

--- software/lib/snapblock.c
+++ software/lib/snapblock.c
@@ -46,6 +46,33 @@
  * improve the performance.
  */
 
+static inline void __free(void *ptr)
+{
+	if (ptr == NULL)
+		return;
+	free(ptr);
+}
+
+typedef struct atomic_t {
+	pthread_mutex_t lock;
+	unsigned long count;
+} atomic_t;
+
+static inline void atomic_init(atomic_t *a, unsigned long v)
+{
+	pthread_mutex_init(&a->lock, NULL);
+	a->count = v;
+}
+
+static inline unsigned long atomic_inc(atomic_t *a)
+{
+	unsigned long v;
+	pthread_mutex_lock(&a->lock);
+	v = a->count++;
+	pthread_mutex_unlock(&a->lock);
+	return v;
+}
+
 #define SNAP_FLASHGT_NVME_SIZE (1ull * 1024 * 1024 * 1024) /* FIXME n TiB */
 #define __CBLK_BLOCK_SIZE 4096
 
@@ -62,10 +89,12 @@ struct cblk_req {
 	unsigned int num;
 	enum cblk_status status;
 	sem_t wait_sem;
+	uint8_t *buf;
 };
 
 #define CBLK_WIDX_MAX	1	/* Just one for now */
 #define CBLK_RIDX_MAX	15	/* 15 read slots */
+#define CBLK_IDX_MAX	(CBLK_WIDX_MAX + CBLK_RIDX_MAX)
 
 struct cblk_dev {
 	struct snap_card *card;
@@ -79,8 +108,8 @@ struct cblk_dev {
 
 	unsigned int widx;
 	unsigned int ridx;
-	struct cblk_req req[CBLK_WIDX_MAX + CBLK_RIDX_MAX];
 
+	struct cblk_req req[CBLK_IDX_MAX];
 	pthread_t done_tid;
 };
 
@@ -90,6 +119,30 @@ static struct cblk_dev chunk = {
 };
 
 /* Action related definitions. Used to access the hardware */
+
+/*
+* Die neue NVMe Action ist im Branch copy4k8k und stellt zwei 
+* Funktionen zur Verfügung. Host Memory nach NVMe (action reg 0x30=3)
+* und NVMe nach Host Memory ( reg 0x30=4).
+* 
+* Source, desination und size Register- Zuordnung haben sich nicht 
+* geändert. Size ist entweder 4k oder 8k. Ein Read or Write Transfer
+* wird mit action start getriggert. Unterstützt werden bis zu 15 Reads
+* und 1 Write zur einer Zeit. Alle Transfers müssen eine unterschiedliche
+* ID haben. ID 0 ist für den Write Transfer reserviert. ID1 bis 15 für
+* die Reads. Die ID muss in Register 0x30 bit 8 bis 11 mit angegeben
+* werden.
+* 
+* Über Register 0x4c kann der Transfer- Status abgefragt
+* werden. Bit 4 zeigt an, ob ein Transfer abgeschlossen wurde.
+* Bit 0 bis 3 gibt die zugehörige ID an.
+* 
+* Beispiel:
+*  reg 0x4c = 0x10 -> write transfer completed
+*  reg 0x4c = 0x1f -> read transfer with ID 15 completed
+*  reg 0x4c = 0x00 -> no completion
+*/ 
+
 #define ACTION_TYPE_NVME_EXAMPLE	0x10140001	/* Action Type */
 
 #define ACTION_CONFIG		0x30
@@ -130,36 +183,150 @@ static const char *action_name[] = {
 #define NVME_MAX_TRANSFER_SIZE	(32 * MEGA_BYTE)  /* NVME limit to Transfer in one chunk */
 
 /* NVME lba cache */
- #define CBLK_MASK	0x000000ff
-#define CBLK_WAYS	4
-#define CBLK_ENTRIES	(CBLK_MASK + 1)
+#define CACHE_MASK	0x0000000f
+#define CACHE_WAYS	4
+#define CACHE_ENTRIES	(CACHE_MASK + 1)
 
-enum cblk_block_status {
-	CBLK_BLOCK_UNUSED = 0,	/* not in use yset */
-	CLBK_BLOCK_VALID = 1,	/* data is valid  for specified lba */
-	CBLK_BLOCK_DIRTY = 2,	/* data was written */
+enum cache_block_status {
+	CACHE_BLOCK_UNUSED = 0,	/* not in use yset */
+	CACHE_BLOCK_VALID = 1,	/* data is valid  for specified lba */
+	CACHE_BLOCK_DIRTY = 2,	/* data was written */
 };
 
-struct cblk_way {
-	enum cblk_block_status status;
+static const char *block_status_str[] = { "UNUSED", "VALID", "DIRTY"};
+
+struct cache_way {
+	enum cache_block_status status;
 	off_t lba;		/* lba this cache entry is for */
 	size_t nblocks;		/* use 1 to keep things simple */
 	unsigned int used;	/* # times this block was used */
+	unsigned int count;	/* eviction counter */
 	void *buf;		/* data if status is CBLK_BLOCK_VALID */
 };
 
-struct cblk_cache {
+struct cache_entry {
 	pthread_mutex_t way_lock;
-	struct cblk_way way[CBLK_WAYS];
+	unsigned int count;
+	struct cache_way way[CACHE_WAYS];
 };
 
-struct cblk_cache cache[CBLK_ENTRIES];
+typedef uint8_t cache_block_t[__CBLK_BLOCK_SIZE];
 
-static inline void __free(void *ptr)
+static struct cache_entry cache_entries[CACHE_ENTRIES];
+static cache_block_t *cache_blocks = NULL;
+
+static int cache_init(void)
 {
-	if (ptr == NULL)
-		return;
-	free(ptr);
+	int rc;
+	unsigned int i, j;
+
+	rc = posix_memalign((void **)&cache_blocks, __CBLK_BLOCK_SIZE,
+		CACHE_ENTRIES * CACHE_WAYS * __CBLK_BLOCK_SIZE);
+	if (rc != 0) {
+		perror("err: posix_memalign");
+		return rc;
+	}
+
+	for (i = 0; i < CACHE_ENTRIES; i++) {
+		struct cache_entry *entry = &cache_entries[i];
+		struct cache_way *way = entry->way;
+
+		pthread_mutex_init(&entry->way_lock, NULL);
+		for (j = 0; j < CACHE_WAYS; j++) {
+			way[j].status = CACHE_BLOCK_UNUSED;
+			way[i].count = 0;
+			way[j].buf = &cache_blocks[i * CACHE_WAYS + j];
+		}
+	}
+	return 0;
+}
+
+static void cache_done(void)
+{
+	unsigned int i, j;
+
+	block_trace("CACHE\n");
+	for (i = 0; i < CACHE_ENTRIES; i++) {
+		struct cache_entry *entry = &cache_entries[i];
+		struct cache_way *way = entry->way;
+
+		block_trace("  entry[%d]\n", i);
+		for (j = 0; j < CACHE_WAYS; j++) {
+			block_trace("    way[%d].status=%s buf=%p lba=%lld\n", j,
+				block_status_str[way[j].status],
+				way[j].buf, (long long)way[j].lba);
+		}
+	}
+
+	__free(cache_blocks);
+	cache_blocks = NULL;
+}
+
+static inline int cache_read(off_t lba, void *buf)
+{
+	unsigned int i = lba & CACHE_MASK, j;
+	struct cache_entry *entry = &cache_entries[i];
+	struct cache_way *way = entry->way;
+
+	block_trace("[%s]\n", __func__);
+	block_trace("  searching read entry[%d] lba=%lld\n", i, (long long)lba);
+
+	pthread_mutex_lock(&entry->way_lock);
+	for (j = 0; j < CACHE_WAYS; j++) {
+		if (lba == way[j].lba) {
+			way[j].count = entry->count++;
+			memcpy(buf, way[j].buf, __CBLK_BLOCK_SIZE);
+			pthread_mutex_unlock(&entry->way_lock);
+			return 0;
+		}
+		block_trace("    way[%d].status=%s buf=%p lba=%lld\n",
+			j, block_status_str[way[j].status],
+			way[j].buf, (long long)way[j].lba);
+	}
+	pthread_mutex_unlock(&entry->way_lock);
+
+	block_trace("  not found lba=%lld\n", (long long)lba);
+	return -1; /* not found */
+}
+
+static inline int cache_write(off_t lba, const void *buf)
+{
+	unsigned int i = lba & CACHE_MASK, j;
+	struct cache_entry *entry = &cache_entries[i];
+	struct cache_way *way = entry->way;
+	uint8_t evict_idx;
+	unsigned int min_count;
+
+	block_trace("[%s]\n", __func__);
+	block_trace("  searching write entry[%d] lba=%lld\n", i, (long long)lba);
+
+	pthread_mutex_lock(&entry->way_lock);
+	evict_idx = 0;
+	min_count = way[0].count;
+
+	for (j = 0; j < CACHE_WAYS; j++) {
+		if (way[j].lba == lba) { /* entry is already in cache */
+			evict_idx = j;
+			break;
+		} else if (way[j].status == CACHE_BLOCK_UNUSED) {
+			evict_idx = j;
+			break;		/* assume fill from 0 to n */
+		} else if (way[j].count < min_count)
+			evict_idx = j;	/* replace entry with smallest count */
+	}
+
+	j = evict_idx;
+	memcpy(way[j].buf, buf, __CBLK_BLOCK_SIZE);
+	way[j].lba = lba;
+	way[j].status = CACHE_BLOCK_VALID;
+	way[j].count = entry->count++;
+
+	block_trace("    way[%d].status=%s buf=%p lba=%lld replaced\n",
+		j, block_status_str[way[j].status],
+		way[j].buf, (long long)way[j].lba);
+
+	pthread_mutex_unlock(&entry->way_lock);
+	return 0; /* always write */
 }
 
 /* Action or Kernel Write and Read are 32 bit MMIO */
@@ -303,15 +470,9 @@ chunk_id_t cblk_open(const char *path,
 		(SNAP_ACTION_DONE_IRQ | SNAP_ATTACH_IRQ);
 	struct cblk_dev *c = &chunk;
 
-	block_trace("%s: opening (%s) ...\n", __func__, path);
+	block_trace("[%s] opening (%s) ...\n", __func__, path);
 
 	pthread_mutex_lock(&c->dev_lock);
-
-	for (i = 0; i < ARRAY_SIZE(c->req); i++) {
-		c->req[i].num = 0;
-		c->req[i].status = CBLK_IDLE;
-		sem_init(&c->req[i].wait_sem, 0, i);
-	}
 
 	if (flags & CBLK_OPN_VIRT_LUN) {
 		fprintf(stderr, "virtual luns not supported in capi stub\n");
@@ -352,21 +513,44 @@ chunk_id_t cblk_open(const char *path,
 		goto out_err1;
 	}
 
-	c->buf = snap_malloc(__CBLK_BLOCK_SIZE * 2);
+	c->buf = snap_malloc(CBLK_IDX_MAX * __CBLK_BLOCK_SIZE * 2);
+	if (c->buf == NULL) {
+		fprintf(stderr, "err: Cannot alloc temporary buffer\n");
+		goto out_err2;
+	}
+	
 	c->drive = 0;
 	c->nblocks = SNAP_FLASHGT_NVME_SIZE / __CBLK_BLOCK_SIZE;
 	c->timeout = timeout;
 	c->done_tid = 0;
 	c->widx = 0;
 	c->ridx = 0;
+	
+	for (i = 0; i < ARRAY_SIZE(c->req); i++) {
+		c->req[i].num = 0;
+		c->req[i].buf = c->buf + i * 2 * __CBLK_BLOCK_SIZE;
+		c->req[i].status = CBLK_IDLE;
+		sem_init(&c->req[i].wait_sem, 0, i);
+	}
 
 	rc = pthread_create(&c->done_tid, NULL, &done_thread, &chunk);
 	if (rc != 0)
-		goto out_err2;
+		goto out_err3;
 
+	rc = cache_init();
+	if (rc != 0)
+		goto out_err4;
+	
 	pthread_mutex_unlock(&c->dev_lock);
 	return 0;
 
+ out_err4:
+	pthread_cancel(c->done_tid);
+	pthread_join(c->done_tid, NULL);
+	c->done_tid = 0;
+ out_err3:
+	__free(c->buf);
+	c->buf = NULL;
  out_err2:
 	snap_detach_action(c->act);
  out_err1:
@@ -399,7 +583,7 @@ int cblk_close(chunk_id_t id __attribute__((unused)),
 		c->done_tid = 0;
 	}
 
-	block_trace("%s: id=%d ...\n", __func__, (int)id);
+	block_trace("[%s] id=%d ...\n", __func__, (int)id);
 
 	for (i = 0; i < ARRAY_SIZE(c->req); i++) {
 		block_trace("  req[%2d]: %s %d\n", i,
@@ -419,6 +603,7 @@ int cblk_close(chunk_id_t id __attribute__((unused)),
 	c->timeout = 0;
 	c->drive = -1;
 
+	cache_done();
 	pthread_mutex_unlock(&c->dev_lock);
 	return 0;
 }
@@ -429,7 +614,7 @@ int cblk_get_lun_size(chunk_id_t id __attribute__((unused)),
 {
 	struct cblk_dev *c = &chunk;
 
-	block_trace("%s: lun_size=%zu block of %d bytes ...\n",
+	block_trace("[%s] lun_size=%zu block of %d bytes ...\n",
 		__func__, c->nblocks, __CBLK_BLOCK_SIZE);
 	if (size)
 		*size = c->nblocks;
@@ -454,26 +639,30 @@ static int block_read(struct cblk_dev *c, void *buf, off_t lba,
 		size_t nblocks)
 {
 	int rc;
+	struct cblk_req *req;
 	uint32_t mem_size = __CBLK_BLOCK_SIZE * nblocks;
 	uint8_t *_buf = buf;
 
-	block_trace("%s: reading (%p lba=%zu nblocks=%zu) ...\n",
+	block_trace("[%s] reading (%p lba=%zu nblocks=%zu) ...\n",
 		__func__, buf, lba, nblocks);
+	
+	pthread_mutex_lock(&c->dev_lock);
+
+	req = &c->req[CBLK_WIDX_MAX + c->ridx];
 	if ((uint64_t)buf % 64) {
 		fprintf(stderr, "warn: buffer address not aligned! %p\n",
 			buf);
 		if (nblocks > 2) {
 			fprintf(stderr, "warn: temp buffer too small!\n");
 			errno = EFAULT;
+			pthread_mutex_unlock(&c->dev_lock);
 			return -1;
 		}
-		_buf = c->buf;
+		_buf = req->buf;
 	}
-	
-	pthread_mutex_lock(&c->dev_lock);
 
-	c->req[CBLK_WIDX_MAX + c->ridx].status = CBLK_READING;
-	c->req[CBLK_WIDX_MAX + c->ridx].num++;
+	req->status = CBLK_READING;
+	req->num++;
 	chunk_memcpy(c,
 		ACTION_CONFIG_COPY_NH | ((CBLK_WIDX_MAX + c->ridx) << 8), /* NVMe to Host DDR */
 		(uint64_t)buf,					/* dst */
@@ -488,16 +677,16 @@ static int block_read(struct cblk_dev *c, void *buf, off_t lba,
 	if ((uint64_t)buf % 64)
 		memcpy(buf, _buf, nblocks * __CBLK_BLOCK_SIZE);
 
-	c->req[CBLK_WIDX_MAX + c->ridx].status = CBLK_IDLE;
+	req->status = CBLK_IDLE;
 	c->ridx = (c->ridx + 1) % CBLK_RIDX_MAX;
 	
 	pthread_mutex_unlock(&c->dev_lock);
-	block_trace("%s: exit lba=%zu nblocks=%zu\n",
+	block_trace("[%s] exit lba=%zu nblocks=%zu\n",
 		__func__, lba, nblocks);
 	return nblocks;
 
  __exit1:
-	c->req[CBLK_WIDX_MAX + c->ridx].status = CBLK_ERROR;
+	req->status = CBLK_ERROR;
 	pthread_mutex_unlock(&c->dev_lock);
 	return -1;
 }
@@ -506,7 +695,18 @@ int cblk_read(chunk_id_t id __attribute__((unused)),
 	      void *buf, off_t lba, size_t nblocks,
 	      int flags  __attribute__((unused)))
 {
-	return block_read(&chunk, buf, lba, nblocks);
+	int rc;
+	size_t i;
+
+	rc = block_read(&chunk, buf, lba, nblocks);
+	if (rc <= 0)
+		return rc;
+
+	for (i = 0; i < nblocks; i++) {
+		cache_write(lba + i, buf + i * __CBLK_BLOCK_SIZE);
+		cache_read(lba + i, buf + i * __CBLK_BLOCK_SIZE);
+	}
+	return rc;
 }
 
 static int block_write(struct cblk_dev *c, void *buf, off_t lba,
@@ -514,24 +714,29 @@ static int block_write(struct cblk_dev *c, void *buf, off_t lba,
 {
 	int rc;
 	uint32_t mem_size = __CBLK_BLOCK_SIZE * nblocks;
-	block_trace("%s: writing (%p lba=%zu nblocks=%zu) ...\n",
+	struct cblk_req *req;
+
+	block_trace("[%s] writing (%p lba=%zu nblocks=%zu) ...\n",
 		__func__, buf, lba, nblocks);
+	
+	pthread_mutex_lock(&c->dev_lock);
+
+	req = &c->req[c->widx];
 	if ((uint64_t)buf % 64) {
 		fprintf(stderr, "warn: buffer address not aligned! %p\n",
 			buf);
 		if (nblocks > 2) {
 			fprintf(stderr, "warn: temp buffer too small!\n");
 			errno = EFAULT;
+			pthread_mutex_unlock(&c->dev_lock);
 			return -1;
 		}
-		memcpy(c->buf, buf, nblocks * __CBLK_BLOCK_SIZE);
-		buf = c->buf;
+		memcpy(req->buf, buf, nblocks * __CBLK_BLOCK_SIZE);
+		buf = req->buf;
 	}
 
-	pthread_mutex_lock(&c->dev_lock);
-
-	c->req[c->widx].status = CBLK_WRITING;
-	c->req[c->widx].num++;
+	req->status = CBLK_WRITING;
+	req->num++;
 	chunk_memcpy(c,
 		ACTION_CONFIG_COPY_HN | (c->widx << 8),	/* Host DDR to NVMe */
 		lba * __CBLK_BLOCK_SIZE/NVME_LB_SIZE,		/* dst */
@@ -543,15 +748,15 @@ static int block_write(struct cblk_dev *c, void *buf, off_t lba,
 	if (rc)
 		goto __exit1;
 
-	c->req[c->widx].status = CBLK_IDLE;
+	req->status = CBLK_IDLE;
 	c->widx = (c->widx + 1) % CBLK_WIDX_MAX;
 
 	pthread_mutex_unlock(&c->dev_lock);
-	block_trace("%s: exit lba=%zu nblocks=%zu\n", __func__, lba, nblocks);
+	block_trace("[%s] exit lba=%zu nblocks=%zu\n", __func__, lba, nblocks);
 	return nblocks;
 
  __exit1:
-	c->req[c->widx].status = CBLK_ERROR;
+	req->status = CBLK_ERROR;
 	pthread_mutex_unlock(&c->dev_lock);
 	return -1;
 }
@@ -567,7 +772,7 @@ static void _init(void) __attribute__((constructor));
 
 static void _init(void)
 {
-	block_trace("%s: init\n", __func__);
+	block_trace("[%s] init\n", __func__);
 
 }
 
@@ -575,6 +780,6 @@ static void _done(void) __attribute__((destructor));
 
 static void _done(void)
 {
-	block_trace("%s: exit\n", __func__);
+	block_trace("[%s] exit\n", __func__);
 	cblk_close(0, 0);
 }
