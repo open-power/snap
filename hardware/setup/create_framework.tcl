fee8615df145362c97aa87ae1360c6b411c65d31
Sven Boekholt <boekholt@de.ibm.com>
2017-09-26 16:08:05 +0200

    HW: Creating pblock for DDR4 SDRAM only in PR flow
    Signed-off-by: Sven Boekholt <boekholt@de.ibm.com>

--- hardware/setup/create_framework.tcl
+++ hardware/setup/create_framework.tcl
@@ -293,9 +293,9 @@ if { $fpga_card == "KU3" } {
   if { $bram_used == "TRUE" } {
     add_files -fileset constrs_1 -norecurse  $root_dir/setup/FGT/snap_refclk266.xdc
   } elseif { $sdram_used == "TRUE" } {
-    add_files -fileset constrs_1 -norecurse $root_dir/setup/FGT/snap_ddr4_pblock.xdc
-    set_property used_in_synthesis false [get_files $root_dir/setup/FGT/snap_ddr4_pblock.xdc]
     if { $use_prflow == "TRUE" } {
+      add_files -fileset constrs_1 -norecurse $root_dir/setup/FGT/snap_ddr4_pblock.xdc
+      set_property used_in_synthesis false [get_files $root_dir/setup/FGT/snap_ddr4_pblock.xdc]
       add_files -fileset constrs_1 -norecurse $root_dir/setup/FGT/action_pblock.xdc
       set_property used_in_synthesis false [get_files  $root_dir/setup/FGT/action_pblock.xdc]
       add_files -fileset constrs_1 -norecurse $root_dir/setup/FGT/snap_pblock.xdc
