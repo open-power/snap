5741c800f83800c7e3e828778f8b7d4de95b4f3c
Frank Haverkamp <haver@linux.vnet.ibm.com>
2017-10-16 14:29:04 +0200

    CBLK: Add minor fixups
    Signed-off-by: Frank Haverkamp <haver@linux.vnet.ibm.com>

--- actions/hdl_nvme_example/sw/force_cpu.c
+++ actions/hdl_nvme_example/sw/force_cpu.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016, International Business Machines
+ * Copyright 2017 International Business Machines
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
