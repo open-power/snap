6eaee5e20780caa3fc115f6746b38a91e631f37a
Thomas Fuchs <thomas.fuchs@de.ibm.com>
2017-11-24 07:11:14 +0100

    PSL9: using C preprocessing to configure NVME and a FACTORY_IMAGE
    Signed-off-by: Thomas Fuchs <thomas.fuchs@de.ibm.com>

--- hardware/setup/snap_config.sh
+++ hardware/setup/snap_config.sh
@@ -21,22 +21,16 @@
 
 SDRAM_SIZE="x\"0000\""
 if [ "$FPGACARD" == "ADKU3" ]; then
-  FPGA_FILTER1="\-\- only for FPGACARD=N250S"
-  FPGA_FILTER2="\-\- only for FPGACARD=S121B"
   CARD_TYPE="x\"00\""
   if [ "${SDRAM_USED^^}" == "TRUE" ]; then
     SDRAM_SIZE="x\"2000\""
   fi
 elif [ "$FPGACARD" == "N250S" ]; then
-  FPGA_FILTER1="\-\- only for FPGACARD=ADKU3"
-  FPGA_FILTER2="\-\- only for FPGACARD=S121B"
   CARD_TYPE="x\"01\""
   if [ "${SDRAM_USED^^}" == "TRUE" ]; then
     SDRAM_SIZE="x\"1000\""
   fi
 else
-  FPGA_FILTER1="\-\- only for FPGACARD=ADKU3"
-  FPGA_FILTER2="\-\- only for FPGACARD=N250S"
   CARD_TYPE="x\"02\""
   if [ "${SDRAM_USED^^}" == "TRUE" ]; then
     SDRAM_SIZE="x\"2000\""
@@ -49,58 +43,10 @@ if [ "${BRAM_USED^^}" == "TRUE" ]; then
   SDRAM_SIZE="x\"0001\""
 fi
 
-if [ "${DDRI_USED^^}" == "TRUE" ]; then
-  DDRI_FILTER="\-\- only for DDRI_USED!=TRUE"
-else
-  DDRI_FILTER="\-\- only for DDRI_USED=TRUE"
-fi
+NAME=`basename $1`
 
-if [ "${DDR3_USED^^}" == "TRUE" ]; then
-  DDR3_FILTER="\-\- only for DDR3_USED!=TRUE"
-else
-  DDR3_FILTER="\-\- only for DDR3_USED=TRUE"
-fi
+echo -e "                        configuring $NAME"
 
-if [ "${DDR4_USED^^}" == "TRUE" ]; then
-  DDR4_FILTER="\-\- only for DDR4_USED!=TRUE"
-else
-  DDR4_FILTER="\-\- only for DDR4_USED=TRUE"
-fi
-
-if [ "${BRAM_USED^^}" == "TRUE" ]; then
-  BRAM_FILTER="\-\- only for BRAM_USED!=TRUE"
-else
-  BRAM_FILTER="\-\- only for BRAM_USED=TRUE"
-fi
-
-if [ "${NVME_USED^^}" == "TRUE" ]; then
-  NVME_FILTER="\-\- only for NVME_USED!=TRUE"
-  NVME_ENABLED="\'1\'"
-else
-  NVME_FILTER="\-\- only for NVME_USED=TRUE"
-  NVME_ENABLED="\'0\'"
-fi
-
-NAME=`basename $2`
-
-echo -e "                        generating $NAME"
-
-grep -v "$FPGA_FILTER1" $1 | grep -v "$FPGA_FILTER2" | grep -v "$DDRI_FILTER" |  grep -v "$DDR3_FILTER" | grep -v "$DDR4_FILTER" | grep -v "$BRAM_FILTER" | grep -v "$NVME_FILTER" > $2
-
-if ([ "$NAME" == "snap_core_types.vhd" ]); then
-  sed -i 's/CONSTANT[ ^I]*NUM_OF_ACTIONS[ ^I]*:[ ^I]*integer.*;/CONSTANT NUM_OF_ACTIONS                  : integer RANGE 0 TO 16         := '$NUM_OF_ACTIONS';             /' $2
-  sed -i 's/CONSTANT[ ^I]*SDRAM_SIZE[ ^I]*:[ ^I]std_logic_vector(15 DOWNTO 0).*;/CONSTANT SDRAM_SIZE                      : std_logic_vector(15 DOWNTO 0) := '$SDRAM_SIZE';               /' $2
-  sed -i 's/CONSTANT[ ^I]*CARD_TYPE[ ^I]*:[ ^I]std_logic_vector(7 DOWNTO 0).*;/CONSTANT CARD_TYPE                       : std_logic_vector(7 DOWNTO 0)  := '$CARD_TYPE';                /' $2
-  sed -i 's/CONSTANT[ ^I]*NVME_ENABLED[ ^I]*:[ ^I]std_logic.*; /CONSTANT NVME_ENABLED                    : std_logic                     := '$NVME_ENABLED';/' $2
-  if [ "$FPGACARD" == "N250SP" ]; then
-    sed -i 's/CONSTANT[ ^I]*CAPI_VER[ ^I]*:[ ^I]integer.*; /CONSTANT CAPI_VER                        : integer RANGE 1 TO 2 := 2;/' $2
-  fi
-fi
-
-if [ "$NAME" == "psl_fpga.vhd" ]; then
-  if [ "${FACTORY_IMAGE^^}" == "TRUE" ]; then
-    sed -i "s/\(gold_factory <= \)'[0-1]'/\1'0'/" $2
-  else
-    sed -i "s/\(gold_factory <= \)'[0-1]'/\1'1'/" $2
-  fi
-fi
+sed -i 's/CONSTANT[ ^I]*NUM_OF_ACTIONS[ ^I]*:[ ^I]*integer.*;/CONSTANT NUM_OF_ACTIONS                  : integer RANGE 0 TO 16         := '$NUM_OF_ACTIONS';             /' $1
+sed -i 's/CONSTANT[ ^I]*SDRAM_SIZE[ ^I]*:[ ^I]std_logic_vector(15 DOWNTO 0).*;/CONSTANT SDRAM_SIZE                      : std_logic_vector(15 DOWNTO 0) := '$SDRAM_SIZE';               /' $1
+sed -i 's/CONSTANT[ ^I]*CARD_TYPE[ ^I]*:[ ^I]std_logic_vector(7 DOWNTO 0).*;/CONSTANT CARD_TYPE                       : std_logic_vector(7 DOWNTO 0)  := '$CARD_TYPE';                /' $1
