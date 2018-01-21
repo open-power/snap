aa3d5d552d656bb5a2eaca4adaefcc49d4e5eea2
Sven Boekholt <boekholt@de.ibm.com>
2018-01-21 19:15:32 +0100

    HW abort: keep action alive until DMA is quiesced
    Signed-off-by: Sven Boekholt <boekholt@de.ibm.com>

--- hardware/hdl/core/job_manager.vhd
+++ hardware/hdl/core/job_manager.vhd
@@ -439,7 +439,7 @@ BEGIN
             ctx_completed_fifo_busy_q     <= '1';
           END IF;
           IF (ctx_completed_fifo_busy_q AND NOT (ctx_completed_fifo_re(sat_id) OR action_completed_v)) = '1' THEN
-            IF mmj_c_i.action_reset(to_integer(unsigned(ctx_completed_fifo_dout(sat_id)))) = '0' THEN
+            IF mmj_c_i.action_reset_vector(to_integer(unsigned(ctx_completed_fifo_dout(sat_id)))) = '0' THEN
               action_completed_fifo_we(sat_id)  <= '1';
               action_completed_fifo_din(sat_id) <= ctx_completed_fifo_dout(sat_id);
               ctx_completed_fifo_busy_q         <= '0';
