2aee2804ba757d3f926b4e4a2906fa238923cf2c
Bruno Mesnet <bruno.mesnet@fr.ibm.com>
2018-01-31 19:25:08 +0100

    HLS memcopy full update
    Signed-off-by: Bruno Mesnet <bruno.mesnet@fr.ibm.com>

--- actions/hls_memcopy/hw/hw_action_memcopy.cpp
+++ actions/hls_memcopy/hw/hw_action_memcopy.cpp
@@ -18,14 +18,8 @@
 
 #include <string.h>
 #include "ap_int.h"
-#include "action_memcopy.H"
+#include "hw_action_memcopy.H"
 
-/* ----------------------------------------------------------------------------
- * Known Limitations => Issue #39 & #45
- * => Transfers must be 64 byte aligned and a size of multiples of 64 bytes
- * Issue#320 - memcopy doesn't handle 4Kbytes xfer => use patch
- * ----------------------------------------------------------------------------
- */
 
 // WRITE DATA TO MEMORY
 short write_burst_of_data_to_mem(snap_membus_t *dout_gmem,
@@ -36,32 +30,16 @@ short write_burst_of_data_to_mem(snap_membus_t *dout_gmem,
 				 snapu64_t size_in_bytes_to_transfer)
 {
 	short rc;
-	// Patch to Issue#320 - memcopy doesn't handle 4Kbytes xfer
-	int size_in_words;
-	if(size_in_bytes_to_transfer %BPERDW == 0)
-		size_in_words = size_in_bytes_to_transfer/BPERDW;
-	else
-		size_in_words = (size_in_bytes_to_transfer/BPERDW) + 1;
 
 	switch (memory_type) {
 	case SNAP_ADDRTYPE_HOST_DRAM:
-		// Patch to Issue#320 - memcopy doesn't handle 4Kbytes xfer
-		//memcpy((snap_membus_t  *) (dout_gmem + output_address),
-		//       buffer, size_in_bytes_to_transfer);
-		wb_dout_loop: for (int k=0; k<size_in_words; k++)
-#pragma HLS PIPELINE
-                    (dout_gmem + output_address)[k] = buffer[k];
-
+		memcpy((snap_membus_t  *) (dout_gmem + output_address),
+		       buffer, size_in_bytes_to_transfer);
        		rc =  0;
 		break;
 	case SNAP_ADDRTYPE_CARD_DRAM:
-		// Patch to Issue#320 - memcopy doesn't handle 4Kbytes xfer
-		//memcpy((snap_membus_t  *) (d_ddrmem + output_address),
-		//       buffer, size_in_bytes_to_transfer);
-		wb_ddr_loop: for (int k=0; k<size_in_words; k++)
-#pragma HLS PIPELINE
-                    (d_ddrmem + output_address)[k] = buffer[k];
-
+		memcpy((snap_membus_t  *) (d_ddrmem + output_address),
+		       buffer, size_in_bytes_to_transfer);
        		rc =  0;
 		break;
 	case SNAP_ADDRTYPE_UNUSED: /* no copy but with rc =0 */
@@ -85,33 +63,16 @@ short read_burst_of_data_from_mem(snap_membus_t *din_gmem,
 	short rc;
         int i;
 
-	// Patch to Issue#320 - memcopy doesn't handle 4Kbytes xfer
-	int size_in_words;
-	if(size_in_bytes_to_transfer %BPERDW == 0)
-		size_in_words = size_in_bytes_to_transfer/BPERDW;
-	else
-		size_in_words = (size_in_bytes_to_transfer/BPERDW) + 1;
-
 	switch (memory_type) {
 
 	case SNAP_ADDRTYPE_HOST_DRAM:
-		// Patch to Issue#320 - memcopy doesn't handle 4Kbytes xfer
-		//memcpy(buffer, (snap_membus_t  *) (din_gmem + input_address),
-		//       size_in_bytes_to_transfer);
-		rb_din_loop: for (int k=0; k<size_in_words; k++)
-#pragma HLS PIPELINE
-                    buffer[k] = (din_gmem + input_address)[k];
-
+		memcpy(buffer, (snap_membus_t  *) (din_gmem + input_address),
+		       size_in_bytes_to_transfer);
        		rc =  0;
 		break;
 	case SNAP_ADDRTYPE_CARD_DRAM:
-		// Patch to Issue#320 - memcopy doesn't handle 4Kbytes xfer
-		//memcpy(buffer, (snap_membus_t  *) (d_ddrmem + input_address),
-		//       size_in_bytes_to_transfer);
-		rb_ddr_loop: for (int k=0; k<size_in_words; k++)
-#pragma HLS PIPELINE
-                    buffer[k] = (d_ddrmem + input_address)[k];
-
+		memcpy(buffer, (snap_membus_t  *) (d_ddrmem + input_address),
+		       size_in_bytes_to_transfer);
        		rc =  0;
 		break;
 	case SNAP_ADDRTYPE_UNUSED: /* no copy but with rc =0 */
@@ -205,7 +166,7 @@ void hls_action(snap_membus_t *din_gmem,
 		action_RO_config_reg *Action_Config)
 {
 	// Host Memory AXI Interface
-#pragma HLS INTERFACE m_axi port=din_gmem bundle=host_mem offset=slave depth=512 \
+#pragma HLS INTERFACE m_axi port=din_gmem bundle=host_mem offset=slave depth=512  \
   max_read_burst_length=64  max_write_burst_length=64 
 #pragma HLS INTERFACE s_axilite port=din_gmem bundle=ctrl_reg offset=0x030
 
