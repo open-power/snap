05d574ff7eb220cc67d96c2724df0cce1a4575dc
Thomas Fuchs <thomas.fuchs@de.ibm.com>
2018-01-31 15:36:17 +0100

    HW: clean up port map
    Signed-off-by: Thomas Fuchs <thomas.fuchs@de.ibm.com>

--- hardware/setup/create_ip.tcl
+++ hardware/setup/create_ip.tcl
@@ -264,7 +264,7 @@ if { $create_interconect == "TRUE" } {
   create_ip -name axi_interconnect -vendor xilinx.com -library ip -version 1.7 -module_name axi_interconnect -dir $ip_dir  >> $log_file
   set_property -dict [list                                  \
                       CONFIG.NUM_SLAVE_PORTS {2} 	    \
-                      CONFIG.THREAD_ID_WIDTH {1} 	    \
+                      CONFIG.THREAD_ID_WIDTH {0} 	    \
                       CONFIG.INTERCONNECT_DATA_WIDTH {512}  \
                       CONFIG.S00_AXI_DATA_WIDTH {512}       \
                       CONFIG.S01_AXI_DATA_WIDTH {128}       \
