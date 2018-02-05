a06f26a30b51e3b14fef5feb0ce91dc5bf25bd5d
luyong6 <luyong@cn.ibm.com>
2018-02-05 04:02:30 +0100

    not needed

--- actions/hdl_stringmatch/hw/tcl/for_synth.tcl
+++
@@ -1,6 +0,0 @@
-# Remove sim-only files for synthesis
-puts "Removing sim_netlist files from synthesis"
-puts [get_files -regexp .*_sim_netlist.(v\|vhdl)]
-set_property used_in_synthesis false [get_files -regexp .*_sim_netlist.(v\|vhdl)]
-puts "Done removing sim_netlist files from synthesis"
-
