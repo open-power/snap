439126d53b10d10e0d8bc6f1292db96a4e946739
Thomas Fuchs <thomas.fuchs@de.ibm.com>
2018-02-26 17:14:17 +0100

    simulation with SDRAM works find, added refclk to top_capi10, removed ddr4_s_axi_ctrl from psl_accel and added AXI interface to DDR4 MIG
    Signed-off-by: Thomas Fuchs <thomas.fuchs@de.ibm.com>

--- hardware/setup/create_ip.tcl
+++ hardware/setup/create_ip.tcl
@@ -429,10 +429,14 @@ if { $create_ddr4_ad8k5 == "TRUE" } {
                       CONFIG.C0.DDR4_InputClockPeriod {3332}                                   \
                       CONFIG.C0.DDR4_MemoryPart {CUSTOM_DBI_MT40A1G8PM-083E}                   \
                       CONFIG.C0.DDR4_DataWidth {64}                                            \
-                      CONFIG.C0.DDR4_DataMask {NO_DM_DBI_WR_RD}                                \
                       CONFIG.C0.DDR4_CasLatency {20}                                           \
                       CONFIG.C0.DDR4_CustomParts $root_dir/setup/AD8K5/DBI_MT40A1G8PM-083E.csv \
                       CONFIG.C0.DDR4_isCustom {true}                                           \
+                      CONFIG.C0.DDR4_AxiSelection {true} 				       \
+                      CONFIG.Simulation_Mode {Unisim} 				               \
+                      CONFIG.C0.DDR4_AxiDataWidth {512} 				       \
+                      CONFIG.C0.DDR4_AxiAddressWidth {33} 				       \
+                      CONFIG.C0.DDR4_AxiIDWidth {4} 					       \
                      ] [get_ips ddr4sdram] >> $log_file
   set_property generate_synth_checkpoint false [get_files $ip_dir/ddr4sdram/ddr4sdram.xci]                    >> $log_file
   generate_target {instantiation_template}     [get_files $ip_dir/ddr4sdram/ddr4sdram.xci]                    >> $log_file
