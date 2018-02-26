6110a41d2d3a0454d4a05cc0ddab7300258c93d9
Thomas Fuchs <thomas.fuchs@de.ibm.com>
2018-02-26 12:26:07 +0100

    configured DDR4 in create_ip.tcl
    Signed-off-by: Thomas Fuchs <thomas.fuchs@de.ibm.com>

--- hardware/setup/create_ip.tcl
+++ hardware/setup/create_ip.tcl
@@ -393,7 +393,7 @@ if { $create_ddr4 == "TRUE" } {
 }
 #DDR4 create ddr4sdramm with ECC (S121B)
 if { $create_ddr4_s121b == "TRUE" } {
-  puts "	                        generating IP ddr4sdram"
+  puts "	                generating IP ddr4sdram"
   create_ip -name ddr4 -vendor xilinx.com -library ip -version 2.* -module_name ddr4sdram -dir $ip_dir >> $log_file
   set_property -dict [list                                                                   \
                       CONFIG.C0.DDR4_MemoryPart {MT40A1G8PM-075E} 			     \
@@ -418,38 +418,30 @@ if { $create_ddr4_s121b == "TRUE" } {
   export_simulation -of_objects [get_files $ip_dir/ddr4sdram/ddr4sdram.xci] -directory $ip_dir/ip_user_files/sim_scripts -force >> $log_file
 
   #DDR4 create ddr4sdramm example design
-  puts "	                        generating ddr4sdram example design"
+  puts "	                generating ddr4sdram example design"
   open_example_project -in_process -force -dir $ip_dir     [get_ips ddr4sdram] >> $log_file
 }
 #DDR4 create ddr4sdramm with ECC (AD8K5)
 if { $create_ddr4_ad8k5 == "TRUE" } {
-  puts "	                        generating IP ddr4sdram"
+  puts "	                generating IP ddr4sdram"
   create_ip -name ddr4 -vendor xilinx.com -library ip -version 2.* -module_name ddr4sdram -dir $ip_dir >> $log_file
-#  MT40A1G8PM-083E?
-#  set_property -dict [list                                                                   \
-#                      CONFIG.C0.DDR4_MemoryPart {MT40A1G8PM-075E} 			     \
-#                      CONFIG.C0.DDR4_TimePeriod {833} 				             \
-#                      CONFIG.C0.DDR4_InputClockPeriod {2499} 				     \
-#                      CONFIG.C0.DDR4_CasLatency {17} 					     \
-#                      CONFIG.C0.DDR4_CasWriteLatency {12} 				     \
-#                      CONFIG.C0.DDR4_DataWidth {72} 					     \
-#                      CONFIG.C0.DDR4_AxiSelection {true} 				     \
-#                      CONFIG.Simulation_Mode {Unisim} 				             \
-#                      CONFIG.C0.DDR4_DataMask {NO_DM_NO_DBI} 				     \
-#                      CONFIG.C0.DDR4_Ecc {true} 					     \
-#                      CONFIG.C0.DDR4_AxiDataWidth {512} 				     \
-#                      CONFIG.C0.DDR4_AxiAddressWidth {33} 				     \
-#                      CONFIG.C0.DDR4_AxiIDWidth {4} 					     \
-#                      CONFIG.C0.BANK_GROUP_WIDTH {2}					     \
-#                     ] [get_ips ddr4sdram] >> $log_file
-  set_property generate_synth_checkpoint false [get_files $ip_dir/ddr4sdram/ddr4sdram.xci]
-  generate_target {instantiation_template}     [get_files $ip_dir/ddr4sdram/ddr4sdram.xci] >> $log_file
-  generate_target all                          [get_files $ip_dir/ddr4sdram/ddr4sdram.xci] >> $log_file
+  set_property -dict [list                                                                     \
+                      CONFIG.C0.DDR4_InputClockPeriod {3332}                                   \
+                      CONFIG.C0.DDR4_MemoryPart {CUSTOM_DBI_MT40A1G8PM-083E}                   \
+                      CONFIG.C0.DDR4_DataWidth {64}                                            \
+                      CONFIG.C0.DDR4_DataMask {NO_DM_DBI_WR_RD}                                \
+                      CONFIG.C0.DDR4_CasLatency {20}                                           \
+                      CONFIG.C0.DDR4_CustomParts $root_dir/setup/AD8K5/DBI_MT40A1G8PM-083E.csv \
+                      CONFIG.C0.DDR4_isCustom {true}                                           \
+                     ] [get_ips ddr4sdram] >> $log_file
+  set_property generate_synth_checkpoint false [get_files $ip_dir/ddr4sdram/ddr4sdram.xci]                    >> $log_file
+  generate_target {instantiation_template}     [get_files $ip_dir/ddr4sdram/ddr4sdram.xci]                    >> $log_file
+  generate_target all                          [get_files $ip_dir/ddr4sdram/ddr4sdram.xci]                    >> $log_file
   export_ip_user_files -of_objects             [get_files $ip_dir/ddr4sdram/ddr4sdram.xci] -no_script -force  >> $log_file
   export_simulation -of_objects [get_files $ip_dir/ddr4sdram/ddr4sdram.xci] -directory $ip_dir/ip_user_files/sim_scripts -force >> $log_file
 
   #DDR4 create ddr4sdramm example design
-  puts "	                        generating ddr4sdram example design"
+  puts "	                generating ddr4sdram example design"
   open_example_project -in_process -force -dir $ip_dir     [get_ips ddr4sdram] >> $log_file
 }
 
