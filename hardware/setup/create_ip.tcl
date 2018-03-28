530d0eaea094957e99df27bc982ead06a2f96d5b
Alexandre CASTELLANE <31407482+acastellane@users.noreply.github.com>
2018-03-28 14:10:17 +0200

    Updated create_ip.tcl
    typo : generating IP axi_interconnect

--- hardware/setup/create_ip.tcl
+++ hardware/setup/create_ip.tcl
@@ -280,7 +280,7 @@ if { $create_clock_conv == "TRUE" } {
 
 #create axi interconect
 if { $create_interconect == "TRUE" } {
-  puts "                        generating IP axi_interconect"
+  puts "                        generating IP axi_interconnect"
   create_ip -name axi_interconnect -vendor xilinx.com -library ip -version 1.7 -module_name axi_interconnect -dir $ip_dir  >> $log_file
   set_property -dict [list                                  \
                       CONFIG.NUM_SLAVE_PORTS {2}            \
