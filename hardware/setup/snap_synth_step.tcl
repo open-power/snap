e3d37cdac65f721b3525a6798b402d8a8e3cd9c9
Thomas Fuchs <thomas.fuchs@de.ibm.com>
2018-04-05 14:02:46 +0200

    starting synth_design with the same parameter as framework.xpr
    Signed-off-by: Thomas Fuchs <thomas.fuchs@de.ibm.com>

--- hardware/setup/snap_synth_step.tcl
+++ hardware/setup/snap_synth_step.tcl
@@ -33,7 +33,22 @@ set widthCol4 $::env(WIDTHCOL4)
 set step      synth_design
 set logfile   $logs_dir/${step}.log
 set directive [get_property STEPS.SYNTH_DESIGN.ARGS.DIRECTIVE [get_runs synth_1]]
-set command   "synth_design -mode default -directive $directive"
+
+set    command $step
+append command " -mode default"
+append command " -directive          $directive"
+append command " -fanout_limit      [get_property STEPS.SYNTH_DESIGN.ARGS.FANOUT_LIMIT      [get_runs synth_1]]"
+append command " -fsm_extraction    [get_property STEPS.SYNTH_DESIGN.ARGS.FSM_EXTRACTION    [get_runs synth_1]]"
+append command " -resource_sharing  [get_property STEPS.SYNTH_DESIGN.ARGS.RESOURCE_SHARING  [get_runs synth_1]]"
+append command " -shreg_min_size    [get_property STEPS.SYNTH_DESIGN.ARGS.SHREG_MIN_SIZE    [get_runs synth_1]]"
+append command " -flatten_hierarchy [get_property STEPS.SYNTH_DESIGN.ARGS.FLATTEN_HIERARCHY [get_runs synth_1]]"
+if { [get_property STEPS.SYNTH_DESIGN.ARGS.KEEP_EQUIVALENT_REGISTERS [get_runs synth_1]] == 1 } {
+  append command " -keep_equivalent_registers"
+}
+if { [get_property STEPS.SYNTH_DESIGN.ARGS.NO_LC [get_runs synth_1]] == 1 } {
+  append command " -no_lc"
+}
+
 puts [format "%-*s%-*s%-*s%-*s"  $widthCol1 "" $widthCol2 "start synthesis" $widthCol3 "with directive: $directive" $widthCol4 "[clock format [clock seconds] -format {%T %a %b %d %Y}]"]
 
 if { [catch "$command > $logfile" errMsg] } {
