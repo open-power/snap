3511d8f68d742625e78df47ec1047e7004764cc6
Thomas Fuchs <thomas.fuchs@de.ibm.com>
2018-05-14 14:11:10 +0200

    first version that runs with 512B write, removed hdx_clp_laddr interface
    Signed-off-by: Thomas Fuchs <thomas.fuchs@de.ibm.com>

--- hardware/hdl/core/dma_rams.vhd
+++ hardware/hdl/core/dma_rams.vhd
@@ -205,7 +205,7 @@ END ARCHITECTURE;
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
 -- ******************************************************
--- *****  DUAL PORT 1152x32, 512 Input 1152 Output  *****
+-- *****  DUAL PORT 512x256, 512 Input 1024 Output   *****
 -- ******************************************************
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
@@ -215,24 +215,24 @@ USE ieee.std_logic_misc.all;
 USE ieee.std_logic_unsigned.all;
 USE ieee.numeric_std.all;
 
-ENTITY ram_1152x32_2p IS
+ENTITY ram_512x256_2p IS
   PORT (
     clk        : IN  std_logic;
 
     wea       : IN  std_logic;
-    addra     : IN  std_logic_vector(     5 DOWNTO 0);
-    dina      : IN  std_logic_vector( 576-1 DOWNTO 0);
+    addra     : IN  std_logic_vector(     7 DOWNTO 0);
+    dina      : IN  std_logic_vector( 512-1 DOWNTO 0);
 
-    addrb     : IN  std_logic_vector(     4 DOWNTO 0);
-    doutb     : OUT std_logic_vector(1152-1 DOWNTO 0)
+    addrb     : IN  std_logic_vector(     6 DOWNTO 0);
+    doutb     : OUT std_logic_vector(1024-1 DOWNTO 0)
   );
-END ram_1152x32_2p;
+END ram_512x256_2p;
 
-ARCHITECTURE ram_1152x32_2p OF ram_1152x32_2p IS
-  TYPE ram_t IS ARRAY (32-1 DOWNTO 0) OF std_logic_vector(1152-1 DOWNTO 0);
+ARCHITECTURE ram_512x256_2p OF ram_512x256_2p IS
+  TYPE ram_t IS ARRAY (256-1 DOWNTO 0) OF std_logic_vector(512-1 DOWNTO 0);
 
   SHARED VARIABLE ram_v    : ram_t;
-  SIGNAL          dout_int : std_logic_vector(1152-1 DOWNTO 0);
+  SIGNAL          dout_int : std_logic_vector(1024-1 DOWNTO 0);
 
 BEGIN
   --
@@ -242,11 +242,7 @@ BEGIN
   BEGIN 
     IF (rising_edge(clk)) THEN
       IF (wea = '1') THEN
-        IF addra(0) = '1' THEN
-          ram_v(to_integer(unsigned(addra(5 DOWNTO 1))))(575 DOWNTO 0) := dina;
-        ELSE
-          ram_v(to_integer(unsigned(addra(5 DOWNTO 1))))(1151 DOWNTO 576) := dina;
-        END IF;
+        ram_v(to_integer(unsigned(addra))) := dina;
       END IF;
     END IF;
   END PROCESS port_a;
@@ -257,7 +253,8 @@ BEGIN
   port_b: PROCESS (clk)
   BEGIN
     IF (rising_edge(clk)) THEN
-      dout_int <= ram_v(to_integer(unsigned(addrb(4 DOWNTO 0))));
+      dout_int(1023 DOWNTO 512) <= ram_v((to_integer(unsigned(addrb & '0')) + 0));
+      dout_int( 511 DOWNTO   0) <= ram_v((to_integer(unsigned(addrb & '0')) + 1));
 
       -- output latch
       doutb <= dout_int;
