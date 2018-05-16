4a74d10849a9fc82bb57b741ed4fedd663982ae4
Frank Haverkamp <haver@linux.vnet.ibm.com>
2018-05-16 10:51:40 +0200

    NVMe: Intro more blocks to try out
    Signed-off-by: Frank Haverkamp <haver@linux.vnet.ibm.com>

--- actions/hdl_nvme_example/tests/demo_0x10140001.sh
+++ actions/hdl_nvme_example/tests/demo_0x10140001.sh
@@ -2,18 +2,31 @@
 # Example for Iteration Closing during NVMe simulator development
 #
 
-SNAP_TRACE=0xff snap_maint -vvv
-dd if=/dev/urandom of=data_4KiB.bin count=1 bs=4096
+# n: Number of blocks
+n=4
 
-SNAP_TRACE=0xfff CBLK_PREFETCH=0 CBLK_REQTIMEOUT=1000 snap_cblk -C0 -w -b1 -n1 data_4KiB.bin
+snap_maint -v
 
-SNAP_TRACE=0xfff CBLK_PREFETCH=0 CBLK_REQTIMEOUT=1000 snap_cblk -C0 -r -b1 -n1 data_4KiBr.bin
+dd if=/dev/urandom of=data_${n}_4KiB.bin count=${n} bs=4096
 
-diff data_4KiB.bin data_4KiBr.bin
+time (SNAP_TRACE=0x0 CBLK_PREFETCH=0 CBLK_REQTIMEOUT=1000 \
+    snap_cblk -C0 -w -b1 -n4 data_${n}_4KiB.bin)
+
+time (SNAP_TRACE=0x0 CBLK_PREFETCH=0 CBLK_REQTIMEOUT=1000 \
+    snap_cblk -C0 -r -b1 -n4 data_${n}_4KiBr.bin)
+
+diff data_${n}_4KiB.bin data_${n}_4KiBr.bin
 if [ $? -ne 0 ]; then
     echo "ERROR: Data differs!"
     exit 1
 fi
 
 echo "OK"
+
+echo "Resulting LBAs on disk:"
+ls -l SNAP*.bin
+
+echo "Paste LBA 0 as an example:"
+cat SNAP_LBA_0000000000000000.bin
+
 exit 0
