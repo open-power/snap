642ecd6bf58a2cbd51ac0354b520deea05df963d
luyong6 <luyong@cn.ibm.com>
2018-06-19 04:32:06 +0200

    Change Copyright to 2016-2018
    Signed-off-by: luyong6 <luyong@cn.ibm.com>

--- hardware/setup/export_modelsim.tcl
+++ hardware/setup/export_modelsim.tcl
@@ -1,6 +1,6 @@
 #-----------------------------------------------------------
 #
-# Copyright 2016, International Business Machines
+# Copyright 2016-2018, International Business Machines
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
