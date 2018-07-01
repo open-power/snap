4641f7dabf2d9c74945fa64280bfb78da80dd3d8
Robert Schmid <robert.schmid@student.hpi.uni-potsdam.de>
2018-07-01 16:02:50 +0200

    Fix hls.mk
    Signed-off-by: Robert Schmid <robert.schmid@student.hpi.uni-potsdam.de>

--- actions/hls.mk
+++ actions/hls.mk
@@ -21,7 +21,7 @@
 #FPGACHIP    ?= xcku060-ffva1156-2-e
 CONFIG_FILE = $(SNAP_ROOT)/.snap_config
 ifneq ("$(wildcard $(CONFIG_FILE))","")
-  FPGACHIP = $(shell grep FPGACHIP $(CONFIG_FILE) | cut -d = -f 2)
+  FPGACHIP = $(shell grep FPGACHIP $(CONFIG_FILE) | cut -d = -f 2 | tr -d '"')
 #  $(info FPGACHIP is set to $(FPGACHIP).)
 endif
 
