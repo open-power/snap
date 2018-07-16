d35bd05fc82d567416b5da8dc975d2afbd806412
Bruno Mesnet <bruno.mesnet@fr.ibm.com>
2018-07-16 16:04:46 +0200

    aligning format for factory generate bitstream on user generate bitstream
    Signed-off-by: Bruno Mesnet <bruno.mesnet@fr.ibm.com>

--- hardware/setup/snap_bitstream_step.tcl
+++ hardware/setup/snap_bitstream_step.tcl
@@ -76,7 +76,7 @@ if { [catch "$command > $logfile" errMsg] } {
 
 # Also write the factory bitstream if it was selected
 if { $factory_image == "TRUE" } {
-  puts [format "%-*s %-*s %-*s %-*s"  $widthCol1 "" $widthCol2 "generating bitstreams" $widthCol3 "type: factory image" $widthCol4 "[clock format [clock seconds] -format {%T %a %b %d %Y}]"]
+  puts [format "%-*s%-*s%-*s%-*s"  $widthCol1 "" $widthCol2 "generating bitstreams" $widthCol3 "type: factory image" $widthCol4 "[clock format [clock seconds] -format {%T %a %b %d %Y}]"]
   # The factory bitstream has the properties from snap_bitstream_pre.tcl plus:
   #xapp1246/xapp1296: These settings are not needed for SNAP.
   #FIXME remove when testing was successful
