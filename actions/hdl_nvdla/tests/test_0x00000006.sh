cb3cd5e4e693113900aa71e375d6eef94e4e9854
Peng Fei Gou <shgoupf@cn.ibm.com>
2018-08-23 09:45:36 -0500

    NVDLA -- update the test with log outputing.
    Signed-off-by: Peng Fei Gou <shgoupf@cn.ibm.com>

--- actions/hdl_nvdla/tests/test_0x00000006.sh
+++ actions/hdl_nvdla/tests/test_0x00000006.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 snap_maint -vv
-snap_nvdla --loadable $ACTION_ROOT/sw/nvdla-sw/regression/flatbufs/kmd/CDP/CDP_L0_0_small_fbuf --rawdump -vv
+unbuffer snap_nvdla --loadable $ACTION_ROOT/sw/nvdla-sw/regression/flatbufs/kmd/CDP/CDP_L0_0_small_fbuf --rawdump -vv | tee snap_nvdla.log
