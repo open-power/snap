ccf1c1f38fec8369bb6ef5ed2cfdb88851f74c15
Peng Fei Gou <shgoupf@cn.ibm.com>
2018-08-26 00:08:05 -0500

    NVDLA -- apply DMA fix from branch "add_again_helloworld".
    Signed-off-by: Peng Fei Gou <shgoupf@cn.ibm.com>

--- actions/hdl_nvdla/hw/tcl/design.tcl
+++ actions/hdl_nvdla/hw/tcl/design.tcl
@@ -3,6 +3,8 @@ add_files -scan_for_includes $action_dir/vlibs/RANDFUNC.vlib -verbose
 add_files -scan_for_includes $action_dir/vlibs/nv_assert_no_x.vlib -verbose
 set_property file_type {Verilog} [get_files *.vlib]
 
+add_files -scan_for_includes $action_dir/include/NV_NVDLA_global_include.vh -verbose
+
 # Use fifo in fifo directory
 foreach fifo_file [glob -nocomplain -dir $action_dir/fifos *.v] {
     set fifo_file_name [exec basename $fifo_file]
