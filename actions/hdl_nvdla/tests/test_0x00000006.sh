1de8f982a51484e3013aebeb07554523c52e17a7
Peng Fei Gou <shgoupf@cn.ibm.com>
2018-08-28 11:01:40 -0500

    NVDLA -- nv_small first pass CDP test.
    The test itself passed with respect to UMD/KMD,
    but the output.dimg differs from the regression/gold,
    need further investigation.
    
    Signed-off-by: Peng Fei Gou <shgoupf@cn.ibm.com>

--- actions/hdl_nvdla/tests/test_0x00000006.sh
+++ actions/hdl_nvdla/tests/test_0x00000006.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 snap_maint -vv
-unbuffer snap_nvdla --loadable $ACTION_ROOT/sw/nvdla-sw/regression/flatbufs/kmd/CDP/CDP_L0_0_small_fbuf --rawdump -vv | tee snap_nvdla.log
+#unbuffer snap_nvdla --loadable $ACTION_ROOT/sw/nvdla-sw/regression/flatbufs/kmd/CDP/CDP_L0_0_small_fbuf --rawdump -vv | tee snap_nvdla.log
+unbuffer snap_nvdla --loadable $ACTION_ROOT/sw/nvdla-sw/regression/flatbufs/kmd/CDP/CDP_L0_0_small_fbuf -vv | tee snap_nvdla.log
