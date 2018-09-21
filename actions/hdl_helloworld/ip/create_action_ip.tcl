b6254c4f506d6faf86be5be09b1b1fff6db37313
luyong6 <luyong@cn.ibm.com>
2018-09-21 10:59:58 +0200

    Better to add close_project
    Signed-off-by: luyong6 <luyong@cn.ibm.com>

--- actions/hdl_helloworld/ip/create_action_ip.tcl
+++ actions/hdl_helloworld/ip/create_action_ip.tcl
@@ -9,7 +9,6 @@ puts "FPGACHIP = $fpga_part"
 puts "ACTION_ROOT = $action_root"
 
 set aip_dir 	$action_root/ip
-set coe_dir 	$action_root/hw/hdl/accdnn/coe
 set log_dir     $action_root/../../hardware/logs
 set log_file    $log_dir/create_action_ip.log
 set src_dir 	$aip_dir/action_ip_prj/action_ip_prj.srcs/sources_1/ip
@@ -25,4 +24,5 @@ create_ip -name fifo_generator -vendor xilinx.com -library ip -version 13.2 -mod
 set_property -dict [list  CONFIG.Fifo_Implementation {Common_Clock_Block_RAM} CONFIG.Input_Data_Width {512} CONFIG.Input_Depth {32} CONFIG.Output_Data_Width {512} CONFIG.Output_Depth {32} CONFIG.Use_Embedded_Registers {false} CONFIG.Reset_Type {Asynchronous_Reset} CONFIG.Full_Flags_Reset_Value {1} CONFIG.Valid_Flag {true} CONFIG.Data_Count {true} CONFIG.Data_Count_Width {5} CONFIG.Write_Data_Count_Width {5} CONFIG.Read_Data_Count_Width {5} CONFIG.Full_Threshold_Assert_Value {30} CONFIG.Full_Threshold_Negate_Value {29} CONFIG.Enable_Safety_Circuit {true}] [get_ips fifo_sync_32_512i512o]
 generate_target all [get_files $src_dir/fifo_sync_32_512i512o/fifo_sync_32_512i512o.xci] >> $log_file
 
-
+puts "\[CREATE_ACTION_IPs..........\] done  [clock format [clock seconds] -format {%T %a %b %d %Y}]"
+close_project
