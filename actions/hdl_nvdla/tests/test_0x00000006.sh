8ec57a0840e46d93f00a4813a2bb659da18ebb59
Peng Fei GOU <shgoupf@cn.ibm.com>
2018-10-08 13:42:35 +0800

    NVDLA -- add command line options to control test_0x00000006.sh
    Signed-off-by: Peng Fei GOU <shgoupf@cn.ibm.com>

--- actions/hdl_nvdla/tests/test_0x00000006.sh
+++ actions/hdl_nvdla/tests/test_0x00000006.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
 
+OPTIND=1         # Reset in case getopts has been used previously in the shell.
+CARD_NO=0
+TESTCASE=-1
+#if [ ! -z $1 ]; then
+#    CARD_NO=$1
+#fi
+while getopts "h?c:t:" opt; do
+    case "$opt" in
+        h|\?)
+            show_help
+            exit 0
+            ;;
+        c)  CARD_NO=$OPTARG
+            ;;
+        t)  TESTCASE=$OPTARG
+            ;;
+    esac
+done
+
+shift $((OPTIND-1))
+
+echo "CARD_NO=$CARD_NO TESTCASE=$TESTCASE"
+
+[ "${1:-}" = "--" ] && shift
 if [ -z $SNAP_ROOT ]; then
     SNAP_ROOT=../../../
     echo "WARNING! SNAP_ROOT not specified, seting to $SNAP_ROOT"
@@ -32,11 +56,6 @@ else
     DLA_CONFIG=nv_small
 fi
 
-CARD_NO=0
-if [ ! -z $1 ]; then
-    CARD_NO=$1
-fi
-
 SW_REGRESSION=$ACTION_ROOT/sw/nvdla-sw/regression
 if [ ! -d $SW_REGRESSION ]; then
     echo "Cannot find directory $SW_REGRESSION!"
@@ -64,39 +83,37 @@ $ACTION_ROOT/../../software/tools/snap_maint -C $CARD_NO -vv
 
 if [ "$DLA_CONFIG" == "nv_small" ]; then
     declare -a flatbuf_tests=(
-                              "$TEST_ROOT/CDP/CDP_L0_0_small_fbuf" \
-                              "$TEST_ROOT/SDP/SDP_X1_L0_0_small_fbuf" \
-                              "$TEST_ROOT/PDP/PDP_L0_0_small_fbuf" \
-                              "$TEST_ROOT/CONV/CONV_D_L0_0_small_fbuf"
-                              )
-    
-    #declare -a flatbuf_tests=("$TEST_ROOT/CDP/CDP_L0_0_small_fbuf")
-    
+    "$TEST_ROOT/CDP/CDP_L0_0_small_fbuf" \
+        "$TEST_ROOT/SDP/SDP_X1_L0_0_small_fbuf" \
+        "$TEST_ROOT/PDP/PDP_L0_0_small_fbuf" \
+        "$TEST_ROOT/CONV/CONV_D_L0_0_small_fbuf"
+    )
+
     declare -a golden_md5s=(
-                           "$GOLDEN_ROOT/CDP_L0_0_small_4531af/dla/lead.md5" \
-                           "$GOLDEN_ROOT/SDP_X1_L0_0_small_c9894d/dla/lead.md5" \
-                           "$GOLDEN_ROOT/PDP_L0_0_small_fbdf76/dla/lead.md5" \
-                           "$GOLDEN_ROOT/CONV_D_L0_0_small_3c77f6/dla/lead.md5"
-                           )
+    "$GOLDEN_ROOT/CDP_L0_0_small_4531af/dla/lead.md5" \
+        "$GOLDEN_ROOT/SDP_X1_L0_0_small_c9894d/dla/lead.md5" \
+        "$GOLDEN_ROOT/PDP_L0_0_small_fbdf76/dla/lead.md5" \
+        "$GOLDEN_ROOT/CONV_D_L0_0_small_3c77f6/dla/lead.md5"
+    )
 elif [ "$DLA_CONFIG" == "nv_large" ]; then
-    
+
     declare -a flatbuf_tests=(
-                              "$TEST_ROOT/CDP/CDP_L0_0_large_fbuf" \
-                              "$TEST_ROOT/SDP/SDP_X1_L0_0_large_fbuf" \
-                              "$TEST_ROOT/PDP/PDP_L0_0_large_fbuf" \
-                              "$TEST_ROOT/CONV/CONV_D_L0_0_large_fbuf" \
-                              #"$TEST_ROOT/NN/NN_L0_1_large_fbuf" \
-                              "$TEST_ROOT/NN/NN_L0_1_large_random_fbuf"
-                              )
-    
+    "$TEST_ROOT/CDP/CDP_L0_0_large_fbuf" \
+        "$TEST_ROOT/SDP/SDP_X1_L0_0_large_fbuf" \
+        "$TEST_ROOT/PDP/PDP_L0_0_large_fbuf" \
+        "$TEST_ROOT/CONV/CONV_D_L0_0_large_fbuf" \
+        "$TEST_ROOT/NN/NN_L0_1_large_fbuf" \
+        "$TEST_ROOT/NN/NN_L0_1_large_random_fbuf"
+    )
+
     declare -a golden_md5s=(
-                           "$GOLDEN_ROOT/CDP_L0_0_large_fe3167/dla/lead.md5" \
-                           "$GOLDEN_ROOT/SDP_X1_L0_0_large_4486c3/dla/lead.md5" \
-                           "$GOLDEN_ROOT/PDP_L0_0_large_a57d22/dla/lead.md5" \
-                           "$GOLDEN_ROOT/CONV_D_L0_0_large_f394b6/dla/lead.md5" \
-                           #"$GOLDEN_ROOT/NN_L0_1_large_b059a8/dla/lead.md5" \
-                           "$GOLDEN_ROOT/NN_L0_1_large_random_2e0fa0/dla/lead.md5"
-                           )
+    "$GOLDEN_ROOT/CDP_L0_0_large_fe3167/dla/lead.md5" \
+        "$GOLDEN_ROOT/SDP_X1_L0_0_large_4486c3/dla/lead.md5" \
+        "$GOLDEN_ROOT/PDP_L0_0_large_a57d22/dla/lead.md5" \
+        "$GOLDEN_ROOT/CONV_D_L0_0_large_f394b6/dla/lead.md5" \
+        "$GOLDEN_ROOT/NN_L0_1_large_b059a8/dla/lead.md5" \
+        "$GOLDEN_ROOT/NN_L0_1_large_random_2e0fa0/dla/lead.md5"
+    )
 else
     echo "Unsupported NVDLA_CONFIG: $DLA_CONFIG"
     exit -1
@@ -104,36 +121,43 @@ fi
 
 for i in "${!flatbuf_tests[@]}"
 do
-   # Run simulation with xrun
-   if [ ! -f ${!flatbuf_tests[i]} ]; then
-       echo "Cannot find ${flatbuf_tests[i]} "
-       exit -1
-   fi
-   test=${flatbuf_tests[i]}
-   testname=`basename $test`
-   cmd="unbuffer $ACTION_ROOT/sw/snap_nvdla -C ${CARD_NO} --loadable ${flatbuf_tests[i]} -vv | tee snap_${testname}.log"
-   eval $cmd
-   if [ $? -eq 0 ]; then
-       echo "FINISHED RUNNING TEST ${testname}" | tee -a snap_${testname}.log
-   else
-       echo "ERROR RUNNING TEST ${testname}" | tee -a snap_${testname}.log
-       exit -1
-   fi
+    if [ $TESTCASE -ne -1 ]; then
+        if [ $i -ne $TESTCASE ]; then
+            continue;
+        fi
+    fi
 
-   test_md5=`md5sum output.dimg | awk '{ print $1 }'`
-   
-   if [ ! -f ${golden_md5s[i]} ]; then
-       echo "Cannot find ${golden_md5s[i]} " | tee -a snap_${testname}.log
-       exit -1
-   fi
+    # Run simulation with xrun
+    if [ ! -f ${!flatbuf_tests[i]} ]; then
+        echo "Cannot find ${flatbuf_tests[i]} "
+        exit -1
+    fi
+    test=${flatbuf_tests[i]}
+    testname=`basename $test`
+    #cmd="unbuffer $ACTION_ROOT/sw/snap_nvdla -C ${CARD_NO} --loadable ${flatbuf_tests[i]} -vv | tee snap_${testname}.log"
+    cmd="$ACTION_ROOT/sw/snap_nvdla -C ${CARD_NO} --loadable ${flatbuf_tests[i]} | tee snap_${testname}.log"
+    eval $cmd
+    if [ $? -eq 0 ]; then
+        echo "FINISHED RUNNING TEST ${testname}" | tee -a snap_${testname}.log
+    else
+        echo "ERROR RUNNING TEST ${testname}" | tee -a snap_${testname}.log
+        exit -1
+    fi
 
-   golden_md5=`cat ${golden_md5s[i]} | awk '{ print $1 }'`
+    test_md5=`md5sum output.dimg | awk '{ print $1 }'`
 
-   if [ "$test_md5" == "$golden_md5" ]; then
-       echo "TEST $testname PASSED" | tee -a snap_${testname}.log
-   else
-       echo "TEST $testname MD5 MISCOMPARE" | tee -a snap_${testname}.log
-       exit -1
-   fi
+    if [ ! -f ${golden_md5s[i]} ]; then
+        echo "Cannot find ${golden_md5s[i]} " | tee -a snap_${testname}.log
+        exit -1
+    fi
+
+    golden_md5=`cat ${golden_md5s[i]} | awk '{ print $1 }'`
+
+    if [ "$test_md5" == "$golden_md5" ]; then
+        echo "TEST $testname PASSED" | tee -a snap_${testname}.log
+    else
+        echo "TEST $testname MD5 MISCOMPARE" | tee -a snap_${testname}.log
+        exit -1
+    fi
 done
 
