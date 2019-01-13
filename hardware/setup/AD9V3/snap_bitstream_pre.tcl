1aa83b9e561c535c7b11b4e040068e50853a234b
Bruno Mesnet <bruno.mesnet@fr.ibm.com>
2019-01-13 17:10:18 +0100

    re-add the constraints for AD9V3
    Signed-off-by: Bruno Mesnet <bruno.mesnet@fr.ibm.com>

---
+++ hardware/setup/AD9V3/snap_bitstream_pre.tcl
@@ -0,0 +1,32 @@
+############################################################################
+############################################################################
+##
+## Copyright 2018 International Business Machines
+##
+## Licensed under the Apache License, Version 2.0 (the "License");
+## you may not use this file except in compliance with the License.
+## You may obtain a copy of the License at
+##
+##     http://www.apache.org/licenses/LICENSE-2.0
+##
+## Unless required by applicable law or agreed to in writing, software
+## distributed under the License is distributed on an "AS IS" BASIS,
+## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+## See the License for the specific language governing permissions and
+## limitations under the License.
+##
+############################################################################
+############################################################################
+
+#Alpha Data 9V3 configuration constraints
+
+set_property BITSTREAM.GENERAL.COMPRESS TRUE [ current_design ]
+set_property BITSTREAM.CONFIG.EXTMASTERCCLK_EN {DIV-1} [current_design]
+set_property BITSTREAM.CONFIG.SPI_32BIT_ADDR YES [current_design]
+set_property BITSTREAM.CONFIG.SPI_BUSWIDTH 8 [current_design]
+set_property BITSTREAM.CONFIG.SPI_FALL_EDGE YES [current_design]
+set_property BITSTREAM.CONFIG.UNUSEDPIN {Pullnone} [current_design]
+set_property CONFIG_MODE SPIx8 [current_design]
+set_property CFGBVS GND [ current_design ]
+set_property CONFIG_VOLTAGE 1.8 [ current_design ]
+set_property BITSTREAM.CONFIG.OVERTEMPSHUTDOWN Enable [current_design]
