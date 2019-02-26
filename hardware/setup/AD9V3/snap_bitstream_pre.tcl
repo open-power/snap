8ea75fb89437252154553f402959ea5304becf61
Bruno Mesnet <bruno.mesnet@fr.ibm.com>
2019-02-26 17:38:58 +0100

    move constraints from setup to BSP
    Signed-off-by: Bruno Mesnet <bruno.mesnet@fr.ibm.com>

--- hardware/setup/AD9V3/snap_bitstream_pre.tcl
+++ hardware/setup/AD9V3/snap_bitstream_pre.tcl
@@ -18,15 +18,4 @@
 ############################################################################
 ############################################################################
 
-#Alpha Data 9V3 configuration constraints
-
-set_property BITSTREAM.GENERAL.COMPRESS TRUE [ current_design ]
-set_property BITSTREAM.CONFIG.EXTMASTERCCLK_EN {DIV-1} [current_design]
-set_property BITSTREAM.CONFIG.SPI_32BIT_ADDR YES [current_design]
-set_property BITSTREAM.CONFIG.SPI_BUSWIDTH 8 [current_design]
-set_property BITSTREAM.CONFIG.SPI_FALL_EDGE YES [current_design]
-set_property BITSTREAM.CONFIG.UNUSEDPIN {Pullnone} [current_design]
-set_property CONFIG_MODE SPIx8 [current_design]
-set_property CFGBVS GND [ current_design ]
-set_property CONFIG_VOLTAGE 1.8 [ current_design ]
-set_property BITSTREAM.CONFIG.OVERTEMPSHUTDOWN Enable [current_design]
+#all constraints have been moved to BSP as for all CAPI2.0 cards to avoid duplication
