476bed22241f909972f4c59d742ecdc2fddc235b
Bruno Mesnet <bruno.mesnet@fr.ibm.com>
2020-04-09 19:51:34 +0200

    add areset + apbcomplet pins
    Signed-off-by: Bruno Mesnet <bruno.mesnet@fr.ibm.com>

--- hardware/setup/create_hbm_for_simu.tcl
+++ hardware/setup/create_hbm_for_simu.tcl
@@ -20,10 +20,12 @@ set vivadoVer    [version -short]
 set root_dir    $::env(SNAP_HARDWARE_ROOT)
 set denali_used $::env(DENALI_USED)
 set fpga_part   $::env(FPGACHIP)
+set fpga_card   $::env(FPGACARD)
 set log_dir     $::env(LOGS_DIR)
 set log_file    $log_dir/create_hbm_host.log
 
 # user can set a specific value for the Action clock lower than the 250MHz nominal clock
+# as of now, only 3 clock frequencies are enabled in this file: 200MHz, 225MHz and 250MHz
 set action_clock_freq "250MHz"
 #overide default value if variable exist
 set action_clock_freq $::env(FPGA_ACTION_CLK)
@@ -35,7 +37,7 @@ set bd_name  hbm_top
 # _______________________________________________________________________________
 # In this file, we define all the logic to have independent 256MB/2Gb memories
 # each with an independent AXI interfaces which will be connected to the action
-# Default is HBM_MEM_NUM = 2 interfaces
+# Default is HBM_MEM_NUM = 8 interfaces
 # TO increase/decrease the number of memory needed, just look to #CHANGE_HBM_INTERFACES_NUMBER
 # param and 1) change HBM_MEM_NUM value
 # with a value between 1 and 16. If you need more memories, you need to add the 2nd stack
@@ -53,6 +55,10 @@ set  HBM_MEM_NUM 8
 # Create HBM project
 create_project   $prj_name $root_dir/ip/hbm -part $fpga_part -force >> $log_file
 set_property target_language VHDL [current_project]
+if { ($fpga_card == "U200" ) || ($fpga_card == "U50") } {
+  set fpga_board  $::env(FPGABOARD)
+  set_property board_part $fpga_board [current_project]
+}
 
 #Create block design
 create_bd_design $bd_name  >> $log_file
@@ -72,10 +78,21 @@ if {[string equal [get_filesets -quiet sources_1] ""]} {
   set_property "generic" "" $source_set
 
 
+#====================
+#ARESETN is used for HBM reset
 set port [create_bd_port -dir I ARESETN]
+#CRESETN is used for converters reset
+set port [create_bd_port -dir I CRESETN]
+
+create_bd_cell -type ip -vlnv xilinx.com:ip:xlconstant:1.1 constant_1_one
+set_property -dict [list CONFIG.CONST_WIDTH {1} CONFIG.CONST_VAL {1}] [get_bd_cells constant_1_one]
+
+# Send 1 to output port 
+set port [create_bd_port -dir O apb_complete]
+connect_bd_net [get_bd_ports apb_complete] [get_bd_pins constant_1_one/dout]
+
 
 #====================
-#
 #-- Set the upper bound of the loop to the number of memory you use --
 
 #--------------------- start loop ------------------
@@ -110,7 +127,9 @@ for {set i 0} {$i < $HBM_MEM_NUM} {incr i} {
       CONFIG.DATA_WIDTH {256}                \
   ] [get_bd_intf_ports S_AXI_p$i\_HBM]
   
-  if { $action_clock_freq == "225MHZ" } {
+  if { $action_clock_freq == "200MHZ" } {
+    set_property -dict [list CONFIG.FREQ_HZ {200000000} ] [get_bd_intf_ports S_AXI_p$i\_HBM]
+  } elseif { $action_clock_freq == "225MHZ" } {
     set_property -dict [list CONFIG.FREQ_HZ {225000000} ] [get_bd_intf_ports S_AXI_p$i\_HBM]
   } else {
     set_property -dict [list CONFIG.FREQ_HZ {250000000} ] [get_bd_intf_ports S_AXI_p$i\_HBM]
@@ -119,14 +138,18 @@ for {set i 0} {$i < $HBM_MEM_NUM} {incr i} {
   connect_bd_intf_net [get_bd_intf_ports S_AXI_p$i\_HBM] [get_bd_intf_pins axi_bram_ctrl_$i/S_AXI]
 
   if { ($vivadoVer >= "2019.2")} {
-    if { $action_clock_freq == "225MHZ" } {
+    if { $action_clock_freq == "200MHZ" } {
+      set port [create_bd_port -dir I -type clk -freq_hz 200000000 S_AXI_p$i\_HBM_ACLK]
+    } elseif { $action_clock_freq == "225MHZ" } {
       set port [create_bd_port -dir I -type clk -freq_hz 225000000 S_AXI_p$i\_HBM_ACLK]
     } else {
       set port [create_bd_port -dir I -type clk -freq_hz 250000000 S_AXI_p$i\_HBM_ACLK]
     }
   } else {
     set port [create_bd_port -dir I -type clk S_AXI_p$i\_HBM_ACLK]
-    if { $action_clock_freq == "225MHZ" } {
+    if { $action_clock_freq == "200MHZ" } {
+      set_property {CONFIG.FREQ_HZ} {200000000} $port
+    } elseif { $action_clock_freq == "225MHZ" } {
       set_property {CONFIG.FREQ_HZ} {225000000} $port
     } else {
       set_property {CONFIG.FREQ_HZ} {250000000} $port
@@ -141,13 +164,11 @@ for {set i 0} {$i < $HBM_MEM_NUM} {incr i} {
 }
 #--------------------- end loop ------------------
 
-# In Vivado 2018.3, there are 32 segments of 256 MiB each in the HBM.
 assign_bd_address  >> $log_file
 
 regenerate_bd_layout
 validate_bd_design >> $log_file
 save_bd_design >> $log_file
-#return $bd
 
 #====================
 # Generate the Output products of the HBM block design.
